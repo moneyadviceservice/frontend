9535066df4f9f9340888d18809a63347d0a28830
Andrew Garner <andrew@andrewgarner.com>
2013-12-20 17:33:00 +0000

    Add ActiveModel Railtie as per Rails 4.1

--- config/application.rb
+++ config/application.rb
@@ -2,6 +2,7 @@ require_relative 'boot'
 
 require 'action_controller/railtie'
 require 'action_view/railtie'
+require 'active_model/railtie'
 require 'sprockets/railtie'
 
 Bundler.require(*Rails.groups)

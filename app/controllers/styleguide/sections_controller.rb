8a8c4bd0631a8fdbb2a7d0fd418d6c2e77dfd581
Andrew Garner <andrew@andrewgarner.com>
2013-12-20 09:51:06 +0000

    Introduce styleguide namespace

---
+++ app/controllers/styleguide/sections_controller.rb
@@ -0,0 +1,5 @@
+module Styleguide
+  class SectionsController < ApplicationController
+
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
-
+  namespace :styleguide do
+    resources :sections
+  end
 end

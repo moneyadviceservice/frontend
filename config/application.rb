1ff8d64bbd503b6499e7634bb279f51d6218f0d3
Andrew Garner <andrew@andrewgarner.com>
2013-12-20 17:32:26 +0000

    Switch to Rails 4.1 secrets convention

--- config/application.rb
+++ config/application.rb
@@ -12,9 +12,6 @@ module Frontend
     # Configure sensitive parameters which will be filtered from the log file.
     config.filter_parameters += [:password]
 
-    # Configure secret key used to verify the integrity of signed cookies.
-    config.secret_key_base   = 'abc123'
-
     # Configure cookie session store.
     config.session_store :cookie_store, key: '_mas_session'
 

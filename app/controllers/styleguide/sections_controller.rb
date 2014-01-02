02e5d03e05e9715449e3fac30638203456164c67
Andrew Garner <andrew@andrewgarner.com>
2014-01-02 17:16:18 +0000

    Introduce specific styleguide layout

--- app/controllers/styleguide/sections_controller.rb
+++ app/controllers/styleguide/sections_controller.rb
@@ -1,5 +1,7 @@
 module Styleguide
   class SectionsController < ApplicationController
+    layout 'styleguide'
+
     def index
       @sections ||= Definition.new.sections.inject({}) do |h, (k, v)|
         h[k] = SectionDecorator.new(v); h

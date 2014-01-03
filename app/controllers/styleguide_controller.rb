7ba8d4fe43f2c2739c632258be76399865cdb7c4
Andrew Garner <andrew@andrewgarner.com>
2014-01-03 14:48:13 +0000

    Remove empty styleguide controller action

--- app/controllers/styleguide_controller.rb
+++ app/controllers/styleguide_controller.rb
@@ -1,7 +1,4 @@
 class StyleguideController < ApplicationController
-  def show
-  end
-
   def css
     @sections ||= Styleguide.new.sections.inject({}) do |h, (k, v)|
       h[k] = StyleguideSectionDecorator.new(v); h

074a38e65aceb82818a71b0f88e943a70ebd4133
Pablo Vicente <PabloVicente.Carrera@moneyadviceservice.org.uk>
2014-01-03 15:50:23 +0000

    Change inject by each_with_object.
    It avoids return h.

--- app/controllers/styleguide_controller.rb
+++ app/controllers/styleguide_controller.rb
@@ -1,7 +1,7 @@
 class StyleguideController < ApplicationController
   def css
-    @sections ||= Styleguide.new.sections.inject({}) do |h, (k, v)|
-      h[k] = StyleguideSectionDecorator.new(v); h
+    @sections ||= Styleguide.new.sections.each_with_object({}) do |(k, v), h|
+      h[k] = StyleguideSectionDecorator.new(v)
     end
   end
 end

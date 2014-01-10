16dca37c30877540d617a4e06df7babad248052f
alexwllms <alex.williams@moneyadviceservice.org.uk>
2014-01-10 14:22:51 +0000

    Add navigation items.

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,14 @@ Rails.application.routes.draw do
   resource :styleguide, controller: 'styleguide', only: 'show' do
     member do
       get 'css'
+      get 'overview'
+      get 'base'
+      get 'components'
+      get 'form_demo'
+      get 'article_demo'
       get 'html'
       get 'sass'
+      get 'lib'
       get 'javascript'
       get 'ruby'
     end

c449e813dc8c5ee88a42e731a8905767024fcad8
alexwllms <alex.williams@moneyadviceservice.org.uk>
2014-01-13 14:56:13 +0000

    Fix the mobile drowp down menu js issue.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,4 +1,26 @@
 //= require jquery
 //= require jquery_ujs
-//= require turbolinks
 //= require_tree .
+
+$(document).ready(function() {
+
+
+  $('body').addClass('js');
+
+  $(".navToggle").on("click", function(e){
+    $('.primaryNav-list').slideToggle();
+  });
+
+
+  $(".has-subMenu").on("click", function(e){
+    $(this).find('.primaryNav-subMenu', this).slideToggle();
+    e.preventDefault();
+  });
+
+
+  $(".has-subMenu li a").on("click", function(e){
+    console.log(this);
+    e.stopPropagation();
+  });
+
+});
--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,26 @@
 Rails.application.routes.draw do
   resource :styleguide, controller: 'styleguide', only: 'show' do
     member do
-      get 'css'
-      get 'overview'
+      get 'sass'
       get 'base'
       get 'components'
-      get 'form_demo'
+      get 'forms'
       get 'article_demo'
       get 'html'
-      get 'sass'
-      get 'lib'
+      get 'variables'
       get 'javascript'
       get 'ruby'
+
+      scope 'css' do
+        get 'css_overview', path: '/css'
+        get 'css_base', path: '/base'
+        get 'css_article_demo', path: '/article-demo'
+      end
+
+      scope 'sass' do
+        get 'sass_overview', path: '/sass'
+        get 'sass_variables', path: '/variables'
+      end
     end
   end
 end

97296957d83aad348c3525b0928bfbfd02233763
Andrew Garner <andrew@andrewgarner.com>
2014-01-13 12:36:34 +0000

    Add localisation switcher to application layout

--- app/controllers/concerns/localisation.rb
+++ app/controllers/concerns/localisation.rb
@@ -2,6 +2,7 @@ module Localisation
   extend ActiveSupport::Concern
 
   included do
+    helper_method :alternative_locales
     before_action :set_locale
 
     unless Rails.env.development?
@@ -13,6 +14,10 @@ module Localisation
 
   def set_locale
     I18n.locale = params[:locale] || I18n.default_locale
+  end
+
+  def alternative_locales
+    I18n.available_locales - Array(I18n.locale)
   end
 
   def default_url_options(options={})

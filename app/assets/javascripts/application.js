2e1bcbd9d75a1d3092d2c92ced50c3f8128d8891
alexwllms <alex.williams@moneyadviceservice.org.uk>
2014-01-13 17:40:00 +0000

    Add styleguide.js.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,25 +1,6 @@
 //= require jquery
 //= require jquery_ujs
-//= require_tree .
 
-$(document).ready(function() {
-
+$(document).ready(function () {
   $('body').addClass('js');
-
-  $('.primaryNav').prepend('<button class="navToggle">Menu <span class="navIcon">&#9660;</span></button>');
-
-  $(".navToggle").on("click", function(e){
-    $('.primaryNav-list').slideToggle();
-  });
-
-  $(".has-subMenu").on("click", function(e){
-    $('.primaryNav-subMenu', this).slideToggle();
-    e.preventDefault();
-  });
-
-
-  $(".has-subMenu li a").on("click", function(e){
-    e.stopPropagation();
-  });
-
 });

4fa7b1915ebec75d95c8bc7760b32deeb7f32acf
Andrew Garner <andrew@andrewgarner.com>
2014-01-14 14:17:57 +0000

    Use MAS spec helper defaults

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,4 +1,4 @@
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 
-require 'mas/development_dependencies/rspec'
+require 'mas/development_dependencies/rspec/spec_helper'

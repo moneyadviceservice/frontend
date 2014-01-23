4e0d12917fcba14f2f228be6c038a7f667f3ae37
alexwllms <alex.williams@moneyadviceservice.org.uk>
2014-01-23 12:13:24 +0000

    Refactor naming convention, links and variables.

--- features/support/ui/pages/home.rb
+++ features/support/ui/pages/home.rb
@@ -9,10 +9,10 @@ module UI::Pages
 
     element :heading, 'h1'
     element :summary_list, 'ul.list-benefits'
-    element :introduction_text, '#introductionText'
+    element :introduction_text, '#introduction-text'
 
     section :header, UI::Sections::Header, '.header'
-    section :footer_site_links, UI::Sections::FooterSiteLinks, '.footerSiteLinks'
-    section :footer_social_links, UI::Sections::FooterSocialLinks, '.footerSocialLinks'
+    section :footer_site_links, UI::Sections::FooterSiteLinks, '.footer-site-links'
+    section :footer_social_links, UI::Sections::FooterSocialLinks, '.footer-social-links'
   end
 end

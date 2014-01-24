d95ca1040047267f99836ddda6d6d1ec6a095516
Rich Kang <rich@saekang.co.uk>
2014-01-24 15:46:32 +0000

    export BUNDLE_WITHOUT, otherwise rake attempts to load spring

--- test.sh
+++ test.sh
@@ -4,13 +4,14 @@ set -e -x
 
 export PATH=./bin:$PATH
 export RAILS_ENV=test
+export BUNDLE_WITHOUT=development
 
 CI_PIPELINE_COUNTER=${GO_PIPELINE_COUNTER-0}
 CI_EXECUTOR_NUMBER=${EXECUTOR_NUMBER-0}
 
 gem uninstall -aIx spring
 
-BUNDLE_WITHOUT=development bundle install
+bundle install
 
 rake spec
 rake cucumber

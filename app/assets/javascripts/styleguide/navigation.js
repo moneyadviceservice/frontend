b0b8c91a3211170a7d507b059a76e8257aa0f329
alexwllms <alex.williams@moneyadviceservice.org.uk>
2014-01-27 21:10:24 +0000

    Move navigation js to separate file.

---
+++ app/assets/javascripts/styleguide/navigation.js
@@ -0,0 +1,19 @@
+$(document).ready(function() {
+
+  $('.primaryNav').prepend('<button class="navToggle">Menu <span class="navIcon">&#9660;</span></button>');
+
+  $(".navToggle").on("click", function(e){
+    $('.primaryNav-list').slideToggle();
+  });
+
+  $(".has-subMenu").on("click", function(e){
+    $('.primaryNav-subMenu', this).slideToggle();
+    e.preventDefault();
+  });
+
+
+  $(".has-subMenu li a").on("click", function(e){
+    e.stopPropagation();
+  });
+
+});

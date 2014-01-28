c217e9858aa823d26b80693be7005acb1cc89809
Pablo Vicente <PabloVicente.Carrera@moneyadviceservice.org.uk>
2014-01-28 11:45:21 +0000

    Remove 'should' from spec description

--- spec/config/initializers/active_resource_extension_spec.rb
+++ spec/config/initializers/active_resource_extension_spec.rb
@@ -12,14 +12,14 @@ describe 'ActiveResource::Base' do
   before(:each) { Thread.current['request-id'] = nil }
   after(:each) { FakeWeb.last_request = nil }
 
-  it 'should add a request Id header only if Thread.Current has it' do
+  it 'adds a request Id header only if Thread.Current has it' do
     FakeWeb.register_uri(:get, 'http://somethingelse/other_dummies/id.json', status: 200, body: '{}')
     OtherDummy.find('id')
     request = FakeWeb.last_request
     expect(request['X-Request-Id']).to eq(nil)
   end
 
-  it 'should add a request Id header' do
+  it 'adds a request Id header' do
     Thread.current['request-id'] = 'current-request-id'
     FakeWeb.register_uri(:get, 'http://something/dummies/id.json', status: 200, body: '{}')
     Dummy.find('id')

af0658e68a3953be975628fe9a4c7c1fe93d7ffb
Andrew Garner <andrew@andrewgarner.com>
2014-01-28 18:34:45 +0000

    Simplify serialisation of ActiveResource object

--- lib/core/repositories/articles/active_resource.rb
+++ lib/core/repositories/articles/active_resource.rb
@@ -12,24 +12,18 @@ module Core
         end
 
         def find(id)
-          Model.find(id, params: { locale: I18n.locale }).value
+          Model.find(id, params: { locale: I18n.locale }).attributes
         rescue ::ActiveResource::ResourceNotFound
           nil
         end
 
         private
 
-        Model = Class.new(::ActiveResource::Base) do
-          def value
-            self.attributes
-          end
-        end
-
+        Model              = Class.new(::ActiveResource::Base)
         Model.element_name = 'article'
 
         def_delegator Model, :site, :url
         def_delegator Model, :site=, :url=
-
       end
     end
   end

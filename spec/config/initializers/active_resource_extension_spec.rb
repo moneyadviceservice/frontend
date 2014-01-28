63e6baa342f07778b9519c5bd40666abc0d3ad15
Pablo Vicente <PabloVicente.Carrera@moneyadviceservice.org.uk>
2014-01-28 13:07:20 +0000

    Refactor active_resource_extension_spec

--- spec/config/initializers/active_resource_extension_spec.rb
+++ spec/config/initializers/active_resource_extension_spec.rb
@@ -1,30 +1,38 @@
 require 'spec_helper'
 
-class Dummy < ActiveResource::Base
-  self.site = 'http://something'
-end
-
-class OtherDummy < ActiveResource::Base
-  self.site = 'http://somethingelse'
-end
-
-describe 'ActiveResource::Base' do
-  before(:each) { Thread.current['request-id'] = nil }
-  after(:each) { FakeWeb.last_request = nil }
-
-  it 'adds a request Id header only if Thread.Current has it' do
-    FakeWeb.register_uri(:get, 'http://somethingelse/other_dummies/id.json', status: 200, body: '{}')
-    OtherDummy.find('id')
-    request = FakeWeb.last_request
-    expect(request['X-Request-Id']).to eq(nil)
+describe ActiveResource::Base do
+  before(:all) do
+    Dummy = Class.new(ActiveResource::Base) { self.site = 'http://example.com' }
   end
 
-  it 'adds a request Id header' do
-    Thread.current['request-id'] = 'current-request-id'
-    FakeWeb.register_uri(:get, 'http://something/dummies/id.json', status: 200, body: '{}')
-    Dummy.find('id')
-    request = FakeWeb.last_request
-    expect(request['X-Request-Id']).to eq('current-request-id')
+  before(:each) do
+    FakeWeb.register_uri(:get, 'http://example.com/dummies/id.json',
+                         status: 200, body: '{}')
+
+    Thread.current['request-id'] = request_id
   end
 
+  after(:each) do
+    FakeWeb.last_request = nil
+  end
+
+  describe '.find' do
+    before { Dummy.find('id') }
+
+    context 'when there is no request-id in the current thread' do
+      let(:request_id) { nil }
+
+      it "does not set a `X-Request-Id' header the request" do
+        expect(FakeWeb.last_request['X-Request-Id']).to be_nil
+      end
+    end
+
+    context 'when there is a request-id in the current thread' do
+      let(:request_id) { 'abc123' }
+
+      it "adds a `X-Request-Id' header to the request" do
+        expect(FakeWeb.last_request['X-Request-Id']).to eq(request_id)
+      end
+    end
+  end
 end

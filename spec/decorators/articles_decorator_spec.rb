7a81bdfdc260ee066a1f2b555abc3f15f33f2647
Andrew Garner <andrew@andrewgarner.com>
2014-01-30 15:38:55 +0000

    Additional syntax tweaks

--- spec/decorators/articles_decorator_spec.rb
+++ spec/decorators/articles_decorator_spec.rb
@@ -2,40 +2,37 @@ require 'spec_helper'
 require 'core/entities/article'
 
 describe ArticleDecorator do
-
   let(:api_article) do
     MultiJson.load(File.read('spec/fixtures/pawnbrokers-how-they-work.json'))
   end
 
-  let(:decorator)   { ArticleDecorator }
+  let(:decorator) { ArticleDecorator }
 
-  let(:article)     do
+  let(:article) do
     decorator.decorate(
       double(Core::Article,
-        id:          'bob',
-        title:       'uncle-bob-is-richer-than-you',
-        description: 'uncle is rich',
-        body:        api_article['body']))
+             id:          'bob',
+             title:       'uncle-bob-is-richer-than-you',
+             description: 'uncle is rich',
+             body:        api_article['body']))
   end
 
   describe '#content' do
     describe 'sanitizes HTML' do
-      context 'With content than needs sanitizing' do
+      context 'with content than needs sanitizing' do
+        let(:html) { Nokogiri::HTML(decorator.decorate(article).content) }
+
         it 'strips images from intro paragraphs' do
-          filter_xml = Nokogiri::HTML(decorator.decorate(article).content).search(HTMLProcessor::INTRO_IMG)
-          expect(filter_xml).to be_empty
+          expect(html.search(HTMLProcessor::INTRO_IMG)).to be_empty
         end
 
         it 'strips action email links' do
-          filter_xml = Nokogiri::HTML(decorator.decorate(article).content).search(HTMLProcessor::ACTION_EMAIL)
-          expect(filter_xml).to be_empty
+          expect(html.search(HTMLProcessor::ACTION_EMAIL)).to be_empty
         end
 
         it 'strips action forms' do
-          filter_xml = Nokogiri::HTML(decorator.decorate(article).content).search(HTMLProcessor::ACTION_FORM)
-          expect(filter_xml).to be_empty
+          expect(html.search(HTMLProcessor::ACTION_FORM)).to be_empty
         end
-
       end
     end
   end

6cc2c096ded18a7308283672258425b23d216b66
Pablo Vicente <PabloVicente.Carrera@moneyadviceservice.org.uk>
2014-01-31 16:38:26 +0000

    ArticlesController raises not_found RoutingError on article not found

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,8 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   include Localisation
+
+  def not_found
+    raise ActionController::RoutingError.new('Not Found')
+  end
 end

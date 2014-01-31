0219eb143d7eb847c6f18a09c7d5e1aaa50c42c8
Andy Tennison <andy.tennison@moneyadviceservice.org.uk>
2014-01-31 21:10:34 +0000

    refactor of test

--- spec/javascripts/mas_analytics_spec.js
+++ spec/javascripts/mas_analytics_spec.js
@@ -1,101 +1,93 @@
 //= require spec_helper
-// require views
-
-// appendTo = function(applicationRoot) {
-//   return $(applicationRoot).append('<div class="hello-world">Hello, world!</div>');
-// };
-
 
 describe("mas_analytics#scrollTracking", function(){
   var mas_analytics;
 
   beforeEach(function(done){
+    $('body').html(JST['templates/scrollTracking']());
+
     require(['mas_analytics'], function(ma) {
       mas_analytics = ma;
       done()
     }, done);
   })
 
-  it("when there is no options object", function(){
+  it("should return false when there is no options object", function(){
     expect(mas_analytics.scrollTracking()).to.equal(false)
   })
 
-  it("when there are no trigger points defined", function(){
+  it("should return false when there are no trigger points defined", function(){
     expect(mas_analytics.scrollTracking({el:'body', triggerPoints:false})).to.equal(false)
   })
 
-  it("when the element is not in the DOM", function(){
-    expect(mas_analytics.scrollTracking({el:false, triggerPoints:[0.25]})).to.equal(false)
+  it("should return false when the trigger points are not an array", function(){
+    expect(mas_analytics.scrollTracking({el:'body', triggerPoints:{'test':'object'}})).to.equal(false)
   })
 
-  it("should log each triggerPoint when it binds the event", function(){
+  it("should return false when the element is not in the DOM", function(){
+    expect(mas_analytics.scrollTracking({el:'.notInDom', triggerPoints:[0.25]})).to.equal(false)
+  })
 
-    $('body').html(JST['templates/scrollTracking']());
-    var object = {el:'.wp-container', triggerPoints:[0.6]};
+  it("should ONLY push object to GTM datalayer the first time it scrolls past a triggerPoint, not on UP, or second down", function(done){
+    var object = {el:'.wp-container2', triggerPoints:[0.5]};
     var l = dataLayer.length;
+    var delay = 50;
 
+    // Bind events
     mas_analytics.scrollTracking(object);
+
+    // Beautiful code for forcing it to scroll down>up>down !
     $(window).scrollTop(3000);
-
     setTimeout(function(){
-      expect(dataLayer.length).to.equal( l+1 );
-    },200);
+      expect(dataLayer.length).to.equal(l+1);
 
+      $(window).scrollTop(0);
+      setTimeout(function(){
+        expect(dataLayer.length).to.equal(l+1);
+
+        $(window).scrollTop(3000);
+        setTimeout(function(){
+          expect(dataLayer.length).to.equal(l+1);
+          done();
+        },delay)
+      },delay);
+    },delay);
   })
 
 })
 
 
-// describe("mas_analytics#_handleScroll", function(){
-//   var mas_analytics;
+describe("mas_analytics#triggerAnalytics", function(){
+  var mas_analytics, spy;
 
-//   beforeEach(function(done){
-//     require(['mas_analytics'], function(ma) {
-//       mas_analytics = ma;
-//       done()
-//     }, done);
-//   })
+  beforeEach(function(done){
+    spy = sinon.spy(MAS, 'log');
+    require(['mas_analytics'], function(ma) {
+      mas_analytics = ma;
+      done()
+    }, done);
+  })
 
-//   it("should return false when user scrolls up", function(){
-//     var dir = 'up';
-//     var val = 0.25;
-//     expect( mas_analytics._handleScroll(dir,val) ).to.equal(false)
-//   })
+  it("should log event when triggerAnalytics() is called", function(){
+    var object = {'title':'test'};
 
-// })
+    mas_analytics.triggerAnalytics(object);
+    assert(spy.calledWith('mas_analytics.triggerAnalytics',object))
+  })
 
+  it("should push object to the GTM datalayer when triggerAnalytics() is called", function(){
+    var object = {'title':'test'};
+    var initialLength = dataLayer.length;
 
-// describe("mas_analytics#triggerAnalytics", function(){
-//   var mas_analytics, spy;
+    mas_analytics.triggerAnalytics(object);
+    expect(initialLength + 1).to.equal( dataLayer.length );
+  })
 
-//   beforeEach(function(done){
-//     spy = sinon.spy(MAS, 'log');
-//     require(['mas_analytics'], function(ma) {
-//       mas_analytics = ma;
-//       done()
-//     }, done);
-//   })
+  afterEach(function(){
+    MAS.log.restore();
+  })
 
-//   it("when triggerAnalytics is called it is logged", function(){
-//     var object = {'title':'test'};
-
-//     mas_analytics.triggerAnalytics(object);
-//     assert(spy.calledWith('mas_analytics.triggerAnalytics',object))
-//   })
-
-//   it("when triggerAnalytics is called object is pushed to the datalayer", function(){
-//     var object = {'title':'test'};
-//     var initialLength = dataLayer.length;
-
-//     mas_analytics.triggerAnalytics(object);
-//     assert.equal(dataLayer.length, (initialLength + 1));
-//   })
-
-//   afterEach(function(){
-//     MAS.log.restore();
-//   })
-
-// })
+})
 
 
 

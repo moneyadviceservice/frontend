4cfbbd2f73239213c692df5b4b8ae20a4bbb459b
Pablo Vicente <PabloVicente.Carrera@moneyadviceservice.org.uk>
2014-02-01 13:53:02 +0000

    Add whit_locale method to I18n

--- spec/config/initializers/i18n/helpers_spec.rb
+++ spec/config/initializers/i18n/helpers_spec.rb
@@ -6,4 +6,19 @@ describe I18n do
       expect(I18n.valid_locales).to_not include(:root);
     end
   end
+
+  describe '.with_locale' do
+    let(:current_locale) { I18n.locale }
+    let(:prob) { lambda{ I18n.locale } }
+
+    it 'change locale value for the block' do
+      expect(I18n.with_locale(:cy, &prob)).to eq(:cy)
+    end
+
+    it 'returns locale to the previous value' do
+      I18n.with_locale(:cy, &prob)
+
+      expect(I18n.locale).to eq(current_locale)
+    end
+  end
 end

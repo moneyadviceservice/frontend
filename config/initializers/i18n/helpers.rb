d7890b7da5d3d4c9d77b9074a3f951ad2d359562
Pablo Vicente <PabloVicente.Carrera@moneyadviceservice.org.uk>
2014-02-01 13:53:02 +0000

    Add whit_locale method to I18n

--- config/initializers/i18n/helpers.rb
+++ config/initializers/i18n/helpers.rb
@@ -1,4 +1,12 @@
 module I18n
+  def self.with_locale(locale, &block)
+    origin_locale = self.locale
+    self.locale = locale
+    return_value = yield
+    self.locale = origin_locale
+    return_value
+  end
+
   def self.valid_locales
     I18n.available_locales.reject { |locale| locale == :root }
   end

a386eaa1f14efa2f20137eb814f1dce7bc5c24f0
Andy Tennison <andy.tennison@moneyadviceservice.org.uk>
2014-02-06 20:40:32 +0000

    more tweaks

--- spec/javascripts/mas_analytics_spec.js
+++ spec/javascripts/mas_analytics_spec.js
@@ -16,20 +16,20 @@ describe("mas_analytics#scrollTracking", function(){
   })
 
   it("should return false when there are no trigger points defined", function(){
-    expect(mas_analytics.scrollTracking({el:'body', triggerPoints:false})).to.equal(false)
+    expect(mas_analytics.scrollTracking({el:'body', triggerPoints:false})).to.be.false
   })
 
   it("should return false when the trigger points are not an array", function(){
-    expect(mas_analytics.scrollTracking({el:'body', triggerPoints:{'test':'object'}})).to.equal(false)
+    expect(mas_analytics.scrollTracking({el:'body', triggerPoints:{'test':'object'}})).to.be.false
   })
 
   it("should return false when the element is not in the DOM", function(){
-    expect(mas_analytics.scrollTracking({el:'.notInDom', triggerPoints:[0.25]})).to.equal(false)
+    expect(mas_analytics.scrollTracking({el:'.notInDom', triggerPoints:[0.25]})).to.be.false
   })
 
   it("should ONLY push object to GTM datalayer the first time it scrolls past a triggerPoint, not on UP, or second down", function(done){
     var object = {el:'.wp-container2', triggerPoints:[0.5]},
-      delay = 50;
+      delay = 250;
 
     // Bind events
     mas_analytics.scrollTracking(object);
@@ -40,16 +40,17 @@ describe("mas_analytics#scrollTracking", function(){
     // Beautiful code for forcing it to scroll down>up>down !
     $(window).scrollTop(3000);
     setTimeout(function(){
-      expect(dataLayer.length).to.equal(l+1);
+      expect(dataLayer).to.have.length(l+1);
 
       $(window).scrollTop(0);
       setTimeout(function(){
-        expect(dataLayer.length).to.equal(l+1);
+        expect(dataLayer).to.have.length(l+1);
 
         $(window).scrollTop(3000);
         setTimeout(function(){
-          expect(dataLayer.length).to.equal(l+1);
+          expect(dataLayer).to.have.length(l+1);
           done();
+
         },delay)
       },delay);
     },delay);

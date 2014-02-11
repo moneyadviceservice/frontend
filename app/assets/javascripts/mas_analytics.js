d80cd60ff6621348a5141606d5c1d710b4e816c8
Andrew Garner <andrew@andrewgarner.com>
2014-02-11 14:49:38 +0000

    Format javascript source

--- app/assets/javascripts/mas_analytics.js
+++ app/assets/javascripts/mas_analytics.js
@@ -1,42 +1,42 @@
 /*
 
-MAS_analytics.js
-- this should contain all analytic methods required throught the site
-- current idea is to require this, then call .triggerAnalytics,
-- an alternative would be to use pubsub, but this is better for fire & forget
+ MAS_analytics.js
+ - this should contain all analytic methods required throught the site
+ - current idea is to require this, then call .triggerAnalytics,
+ - an alternative would be to use pubsub, but this is better for fire & forget
 
-*/
+ */
 
 
-define(['jquery', 'waypoints'],function ($) {
+define(['jquery', 'waypoints'], function ($) {
 
   var _this = this,
-    loadDelay = ((new Date().getTime()) -  MAS.timestamp)/1000;
+    loadDelay = ((new Date().getTime()) - MAS.timestamp) / 1000;
 
   // Cache analytics calls so we can prevent duplicate
   this._calledAlready = [];
 
   // General call to trigger GA analytics
-  this.triggerAnalytics = function(data){
+  this.triggerAnalytics = function (data) {
     MAS.log('mas_analytics.triggerAnalytics', data);
     MAS.datalayer.push(data);
   };
 
   // Private func to handle scrollTracking events
-  this._handleScroll = function(dir, val, contentRatio){
+  this._handleScroll = function (dir, val, contentRatio) {
     MAS.log('mas_analytics._handleScroll - ', arguments);
 
     // Only interested in down events
-    if(dir === 'up') return false;
+    if (dir === 'up') return false;
 
     // check if already triggered, ## might be a .once equivelant
-    if(_this._calledAlready.indexOf('scrollTracking-'+val) !== -1) return;
+    if (_this._calledAlready.indexOf('scrollTracking-' + val) !== -1) return;
 
     // Unbind to prevent future events - as all events are
-    _this._calledAlready.push('scrollTracking-'+val);
+    _this._calledAlready.push('scrollTracking-' + val);
 
     // get offset time from pageload - can we get from google?
-    var eventdelay = ((new Date().getTime()) -  MAS.timestamp)/1000,
+    var eventdelay = ((new Date().getTime()) - MAS.timestamp) / 1000,
       combinedDelay = eventdelay - loadDelay;
 
     // push to datalayer OR MAS abstracted datalayer
@@ -57,12 +57,12 @@ define(['jquery', 'waypoints'],function ($) {
   }
 
   // Tracks user scrolling down to different parts of page
-  this.scrollTracking = function(opts){
-    function areOptionsValid(){
-       return (!opts || !$(opts.el).length || !opts.triggerPoints || !$.isArray(opts.triggerPoints))
+  this.scrollTracking = function (opts) {
+    function areOptionsValid() {
+      return (!opts || !$(opts.el).length || !opts.triggerPoints || !$.isArray(opts.triggerPoints))
     };
 
-    if( areOptionsValid() ){
+    if (areOptionsValid()) {
       MAS.warn('mas_analytics.scrollTracking - missing element or triggerPoints', opts);
       // console.warn('mas_analytics.scrollTracking - missing element or triggerPoints', opts);
       return false;
@@ -82,13 +82,13 @@ define(['jquery', 'waypoints'],function ($) {
     // });
 
     // Bind event for each trigger point
-    $.each(opts.triggerPoints,function(i,val){
+    $.each(opts.triggerPoints, function (i, val) {
       MAS.info('mas_analytics.scrollTracking (bind each) - ', val);
-      var offsetVal = h*val - wh;
+      var offsetVal = h * val - wh;
 
-      $el.waypoint(function(dir){
-        _this._handleScroll(dir,val,contentRatio);
-      }, {offset: -offsetVal});  
+      $el.waypoint(function (dir) {
+        _this._handleScroll(dir, val, contentRatio);
+      }, {offset: -offsetVal});
 
     })
   }
--- spec/javascripts/mas_analytics_spec.js
+++ spec/javascripts/mas_analytics_spec.js
@@ -1,30 +1,30 @@
 //= require spec_helper
 
-describe("mas_analytics#scrollTracking", function(){
+describe("mas_analytics#scrollTracking", function () {
   var mas_analytics;
 
-  beforeEach(function(done){
+  beforeEach(function (done) {
     $('body').html(JST['templates/scrollTracking']());
-    require(['analytics'], function(ma) {
+    require(['analytics'], function (ma) {
       mas_analytics = ma;
       done()
     }, done);
   })
 
-  it("should return false when there is no options object", function(){
+  it("should return false when there is no options object", function () {
     expect(mas_analytics.scrollTracking()).to.equal(false)
   })
 
-  it("should return false when there are no trigger points defined", function(){
-    expect(mas_analytics.scrollTracking({el:'body', triggerPoints:false})).to.be.false
+  it("should return false when there are no trigger points defined", function () {
+    expect(mas_analytics.scrollTracking({el: 'body', triggerPoints: false})).to.be.false
   })
 
-  it("should return false when the trigger points are not an array", function(){
-    expect(mas_analytics.scrollTracking({el:'body', triggerPoints:{'test':'object'}})).to.be.false
+  it("should return false when the trigger points are not an array", function () {
+    expect(mas_analytics.scrollTracking({el: 'body', triggerPoints: {'test': 'object'}})).to.be.false
   })
 
-  it("should return false when the element is not in the DOM", function(){
-    expect(mas_analytics.scrollTracking({el:'.notInDom', triggerPoints:[0.25]})).to.be.false
+  it("should return false when the element is not in the DOM", function () {
+    expect(mas_analytics.scrollTracking({el: '.notInDom', triggerPoints: [0.25]})).to.be.false
   })
 
 
@@ -60,35 +60,32 @@ describe("mas_analytics#scrollTracking", function(){
 
 })
 
-
-describe("mas_analytics#triggerAnalytics", function(){
+describe("mas_analytics#triggerAnalytics", function () {
   var mas_analytics, spy;
 
-  beforeEach(function(done){
+  beforeEach(function (done) {
     spy = sinon.spy(MAS, 'log');
-    require(['analytics'], function(ma) {
+    require(['analytics'], function (ma) {
       mas_analytics = ma;
       done()
     }, done);
   })
 
-  it("should log event when triggerAnalytics() is called", function(){
-    var object = {'title':'test'};
+  it("should log event when triggerAnalytics() is called", function () {
+    var object = {'title': 'test'};
     mas_analytics.triggerAnalytics(object);
-    assert(spy.calledWith('mas_analytics.triggerAnalytics',object))
+    assert(spy.calledWith('mas_analytics.triggerAnalytics', object))
   })
 
-  it("should push object to the GTM datalayer when triggerAnalytics() is called", function(){
-    var object = {'title':'test'};
+  it("should push object to the GTM datalayer when triggerAnalytics() is called", function () {
+    var object = {'title': 'test'};
     var initialLength = dataLayer.length;
     mas_analytics.triggerAnalytics(object);
-    expect(initialLength + 1).to.equal( dataLayer.length );
+    expect(initialLength + 1).to.equal(dataLayer.length);
   })
 
-  afterEach(function(){
+  afterEach(function () {
     MAS.log.restore();
   })
 
 })
-
-

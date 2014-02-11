234f0a82529f8329eaba99319059cc558921e884
Andrew Garner <andrew@andrewgarner.com>
2014-02-11 14:59:44 +0000

    Simplify article decorator syntax

--- app/decorators/article_decorator.rb
+++ app/decorators/article_decorator.rb
@@ -4,33 +4,28 @@ class ArticleDecorator < Draper::Decorator
   delegate :title, :description
 
   def content
-    processed_body.html_safe
+    @content ||= processed_body.html_safe
   end
 
   private
 
   def processed_body
-    @process_body ||= begin
-      temp_body = nil
-      html_processors.each do |processor, xpaths|
-        temp_body = processor.new(temp_body || object.body).process(*xpaths)
-      end
-      temp_body
+    body = object.body
+
+    html_processors.each do |processor, xpaths|
+      body = processor.new(body).process(*xpaths)
     end
+
+    body
   end
 
   def html_processors
     {
-      HTMLProcessor::NodeRemover => [
-        HTMLProcessor::INTRO_IMG,
-        HTMLProcessor::ACTION_EMAIL,
-        HTMLProcessor::ACTION_FORM
-      ],
-      HTMLProcessor::VideoWrapper => [
-        HTMLProcessor::VIDEO_IFRAME
-      ]
+      HTMLProcessor::NodeRemover  => [HTMLProcessor::INTRO_IMG,
+                                      HTMLProcessor::ACTION_EMAIL,
+                                      HTMLProcessor::ACTION_FORM],
+
+      HTMLProcessor::VideoWrapper => [HTMLProcessor::VIDEO_IFRAME]
     }
   end
-
-
 end

9571dd7bb68c84cf8aed6937af39f7ea9b11f51d
Pablo Vicente <PabloVicente.Carrera@moneyadviceservice.org.uk>
2014-02-13 11:16:30 +0000

    Add action plan decorator.
    @alexwllms

--- app/controllers/action_plans_controller.rb
+++ app/controllers/action_plans_controller.rb
@@ -1,6 +1,8 @@
 require 'core/interactors/action_plan_reader'
 
 class ActionPlansController < ApplicationController
+  decorates_assigned :action_plan, with: ActionPlanDecorator
+
   def show
     @action_plan = Core::ActionPlanReader.new(params[:id]).call
   end
---
+++ app/decorators/action_plan_decorator.rb
@@ -0,0 +1,33 @@
+require 'html_processor'
+
+class ActionPlanDecorator < Draper::Decorator
+  delegate :title, :description
+
+  def content
+    @content ||= processed_body.html_safe
+  end
+
+  private
+
+  def processed_body
+    body = object.body
+
+    html_processors.each do |processor, xpaths|
+      body = processor.new(body).process(*xpaths)
+    end
+
+    body
+  end
+
+  def html_processors
+    {
+        HTMLProcessor::NodeRemover  => [HTMLProcessor::INTRO_IMG,
+                                        HTMLProcessor::ACTION_EMAIL,
+                                        HTMLProcessor::ACTION_FORM],
+
+        HTMLProcessor::VideoWrapper => [HTMLProcessor::VIDEO_IFRAME],
+
+        HTMLProcessor::TableWrapper => [HTMLProcessor::DATATABLE_DEFAULT]
+    }
+  end
+end

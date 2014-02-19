bb3f25eb388576dab87e6469552a7d54813a1754
Steven Wilkin <stevenwilkin@gmail.com>
2014-02-19 17:38:06 +0000

    Retrieve the category from the repository in the controller

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,4 +1,9 @@
+require 'core/interactors/category_reader'
+
 class CategoriesController < ApplicationController
   def show
+    @category = Core::CategoryReader.new(params[:id]).call do
+      not_found
+    end
   end
 end
---
+++ spec/controllers/categories_controller_spec.rb
@@ -0,0 +1,39 @@
+require 'spec_helper'
+require 'core/interactors/category_reader'
+
+describe CategoriesController do
+  describe 'GET show' do
+    let(:category) { double(Core::Category, id: 'test') }
+    let(:category_reader) { double(Core::CategoryReader, call: category) }
+
+    it 'is successful' do
+      allow(Core::CategoryReader).to receive(:new) { category_reader }
+
+      get :show, id: 'foo', locale: I18n.locale
+
+      expect(response).to be_ok
+    end
+
+    it 'instantiates a category reader' do
+      expect(Core::CategoryReader).to receive(:new).with(category.id) { category_reader }
+
+      get :show, locale: I18n.locale, id: category.id
+    end
+
+    it 'assigns @category to the result of category reader' do
+      allow_any_instance_of(Core::CategoryReader).to receive(:call) { category }
+
+      get :show, locale: I18n.locale, id: category.id
+
+      expect(assigns(:category)).to eq(category)
+    end
+
+    context 'when a category does not exist' do
+      it 'raises an ActionController RoutingError' do
+        allow_any_instance_of(Core::CategoryReader).to receive(:call).and_yield
+
+        expect { get :show, id: 'foo', locale: I18n.locale }.to raise_error(ActionController::RoutingError)
+      end
+    end
+  end
+end

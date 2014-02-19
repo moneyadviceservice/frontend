24a1dff70b3c02067b6e7f897300fa23d598e952
Steven Wilkin <stevenwilkin@gmail.com>
2014-02-19 16:02:13 +0000

    First cut of category entity

---
+++ spec/lib/core/entities/category_spec.rb
@@ -0,0 +1,23 @@
+require 'spec_helper'
+require 'core/entities/category'
+
+module Core
+  describe Category do
+    subject { described_class.new(double, attributes) }
+
+    let(:attributes) { { title:       double,
+                         description: double,
+                         contents:    double } }
+
+    it { should respond_to :title }
+    it { should respond_to :title= }
+
+    it { should respond_to :description }
+    it { should respond_to :description= }
+
+    it { should respond_to :contents }
+    it { should respond_to :contents= }
+
+    it { should validate_presence_of(:title) }
+  end
+end

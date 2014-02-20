266e407abb1cf50f52b0dc6da783123aee4740d5
Andrew Garner <andrew@andrewgarner.com>
2014-02-20 11:47:37 +0000

    Ensure Capybara Thin configuration is targeted

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,15 +4,6 @@ require_relative '../config/environment'
 require 'mas/development_dependencies/rspec/spec_helper'
 require 'webmock/rspec'
 
-Capybara.server do |app, port|
-  if Konacha.mode == :runner
-    require 'rack/handler/thin'
-    Rack::Handler::Thin.run(app, Port: port)
-  else
-    Capybara.run_default_server(app, port)
-  end
-end if defined?(Capybara)
-
 RSpec.configure do |c|
   c.alias_it_should_behave_like_to :it_has_behavior, 'exhibits behaviour of an'
 end

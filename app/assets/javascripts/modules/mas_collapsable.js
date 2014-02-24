1a95c349f98737918940953f8b4d1871de5d8ae1
Andy Tennison <andy.tennison@moneyadviceservice.org.uk>
2014-02-24 17:40:39 +0000

    collapsable refactored to work with primary nav + spec tests

--- app/assets/javascripts/modules/mas_collapsable.js
+++ app/assets/javascripts/modules/mas_collapsable.js
@@ -3,12 +3,15 @@ define(['jquery'], function ($) {
 
   "use strict";
 
-  console.log('! v3')
+  console.log('! v10')
 
   var defaults = {
+    targetType: 'next',                 // options: next | data | href
     triggerEl: '.collapsible',
     targetEl: '.collapsible-section',
-    activeClass: 'js-active',
+    activeClass: 'is-on',
+    inactiveClass: 'is-off',
+    showIcon: false,
     headingIcon: '<span class="icon icon--toggle"></span><span class="visually-hidden">{{txt}}</span>',
     closeOffFocus: false,
     accordion: false,
@@ -22,14 +25,53 @@ define(['jquery'], function ($) {
   var Collapsible = function(opts){
     this.o = $.extend(defaults, opts);
     this.sections = [];
+    this.selected = false;
 
     var triggers = $(this.o.triggerEl),
-        l = triggers.length;
+        l = triggers.length,
+        i = 0;
 
-    if(l === 0) MAS.warn('mas_collapsible => no trigger elements in page: ' + this.o.triggerEl);
+    if(l === 0) return MAS.warn('mas_collapsible => no trigger elements in page: ' + this.o.triggerEl);
 
-    while(l--){
-      Collapsible.prototype._setupEach.call(this, l, triggers[l])
+    for(i; i<l; i++){
+      Collapsible.prototype._setupEach.call(this, i, triggers[i])
+    }
+  }
+
+  Collapsible.prototype._modifyButtonHTML = function(i){
+    // move modify code here for cleaner separation of accessibility stuff
+    // Wrap in button element to assist with accessibility
+    var buttonTitle = this.sections[i].trigger.text();
+    // this.sections[i].trigger.html('<button class="button--unstyled">' + this.o.headingIcon + buttonTitle  + '</button>');
+    this.sections[i].trigger.attr('aria-role','button');
+    this.sections[i].trigger.attr('tabindex', '0');
+
+    if(this.o.showIcon){
+      this.sections[i].trigger.prepend(this.o.headingIcon.replace('{{txt}}', this.o.textString.show_this_section));
+      this.sections[i].icon = this.sections[i].trigger.find('.visually-hidden');
+    }
+  }
+
+  Collapsible.prototype._findTarget = function($el){
+    switch(this.o.targetType){
+      case 'data':
+        // Trigger must have data-target = string that can be interpreted by jQuery
+        var d = $el.attr('data-target');
+        return $(d);
+        break;
+      case 'href':
+        var href = $el.attr('href');
+        if( href.indexOf('#') !== 0 ){
+          // HREF must be formatted as #ID
+          return false;
+        }else{
+          return $(href);
+        }
+        break;
+      case 'next':
+      default:
+        return $el.next(this.o.targetEl);
+        break;
     }
   }
 
@@ -39,21 +81,27 @@ define(['jquery'], function ($) {
     this.sections[i] = {
       index: i,
       trigger: $el,
-      target: $el.next('.collapsible-section'),
-      hidden: $el.hasClass(this.o.activeClass)
+      target: this._findTarget($el),
+      hidden: $el.hasClass(this.o.inactiveClass)
     }
 
-    // Wrap in button element to assist with accessibility
-    var buttonTitle = this.sections[i].trigger.text();
-    // this.sections[i].trigger.html('<button class="button--unstyled">' + this.o.headingIcon + buttonTitle  + '</button>');
-    this.sections[i].trigger.prepend(this.o.headingIcon);
-    this.sections[i].trigger.attr('aria-role','button');
-    this.sections[i].trigger.attr('tabindex', '0');
-    this.sections[i].icon = this.sections[i].trigger.find('.visually-hidden');
+    // Set show / hide states based on options
+    if(this.o.showOnlyFirst){
+      this.sections[i].hidden = (i === 0)? false : true;
+    }
 
-    console.log('ariaHiddenVal = '+ariaHiddenVal)
-    var ariaHiddenVal = (this.sections[i].hidden)? 'true' : 'false';
-    this.sections[i].target.attr('aria-hidden', ariaHiddenVal);
+    if(this.o.accordion){
+      if(this.selected === false && !this.sections[i].hidden){
+      }else{
+        this.sections[i].hidden = true;
+      }
+    }
+
+    // Update Button HTML to make accessible
+    this._modifyButtonHTML(i);
+
+    // Set initial state
+    (this.sections[i].hidden) ? this.hide(i) : this.show(i);
 
     // Bind events
     var _this = this;
@@ -61,32 +109,44 @@ define(['jquery'], function ($) {
       (_this.sections[i].hidden)? _this.show(i) : _this.hide(i);
     });
 
-    this.sections[i].trigger.on('keyup', function(e){
+    // Accessibility support for spacebar
+    this.sections[i].trigger.on('keypress', function(e){
       if(e.which === 32) {
         _this.sections[i].trigger.trigger('click');
       }
     })
 
-    // Trigger events
-    if(this.o.showOnlyFirst && i >= 1){
-      this.sections[i].trigger.trigger('click');
+    this.sections[i].target.on('focusout', function(e){
+      alert('! focus <output></output>')
+    })
+  }
+
+  $.fn.swapClass = function(from,to){
+    var c = this[0].className;
+    if( c.indexOf(from) === -1 ){
+      this[0].className = c + ' ' + to;
+    }else{
+      this[0].className = c.replace(from,to);
     }
   }
 
   Collapsible.prototype.show = function(i){
     var item = this.sections[i];
-    item.icon.text('Show this section');
-    item.trigger.removeClass(this.o.activeClass);
-    item.target.removeClass(this.o.activeClass);
+    if(this.o.showIcon) item.icon.text(this.o.textString.show_this_section);
+    item.trigger.swapClass(this.o.inactiveClass, this.o.activeClass);
+    item.target.swapClass(this.o.inactiveClass, this.o.activeClass);
     item.target.attr('aria-hidden', 'false');
     item.hidden = false;
+
+    if(this.o.accordion && this.selected !== false) this.hide(this.selected);
+    this.selected = i;
   }
   
   Collapsible.prototype.hide = function(i){
     var item = this.sections[i];
-    item.icon.text('Hide this section');
-    item.trigger.addClass(this.o.activeClass);
-    item.target.addClass(this.o.activeClass);
+    if(this.o.showIcon) item.icon.text(this.o.textString.hide_this_section);
+    item.trigger.swapClass(this.o.activeClass, this.o.inactiveClass);
+    item.target.swapClass(this.o.activeClass, this.o.inactiveClass);
     item.target.attr('aria-hidden', 'true');
     item.hidden = true;
   }
--- spec/javascripts/mas_collapsable_spec.js
+++ spec/javascripts/mas_collapsable_spec.js
@@ -1,13 +1,12 @@
 //= require spec_helper
 //= require templates/collapsable
 
+describe("mas_collapsable basics", function () {
 
+  var ValidCollapsable, EmptyCollapsable, $body;
 
-describe("mas_collapsable", function () {
-
-  var ValidCollapsable,
-      EmptyCollapsable,
-      $body;
+  // beforeEach()
+  // afterEach()
 
   before(function (done) {
     $('body').html(JST['templates/collapsable']());
@@ -15,14 +14,12 @@ describe("mas_collapsable", function () {
 
     require(['collapsable'], function (mod) {
       ValidCollapsable = new mod();
-      EmptyCollapsable = new mod({triggerEl:'.doesntExist'});
+      EmptyCollapsable = new mod({triggerEl: '.doesntExist'});
       done();
     }, done);
   })
 
-
-  // NEW TOP NAV TESTS
-  describe("when initiated", function(){
+  describe("when initiated", function () {
     it("return false if no trigger elements in DOM", function () {
       expect(EmptyCollapsable.sections.length).to.be.equal(0);
     })
@@ -37,101 +34,217 @@ describe("mas_collapsable", function () {
   })
 
   describe("onload", function () {
+    var buttonVisible, targetVisible, buttonHidden, targetHidden;
 
-    it("adds aria-role=button to all toggle trigger elements", function(){
-      var button = $body.find('#toggleButton1');
-      expect(button.attr('aria-role')).to.equal('button');
+    before(function () {
+      buttonVisible = $body.find('#toggleButton1');
+      targetVisible = $body.find('#toggleTarget1');
+      buttonHidden = $body.find('#toggleButton2');
+      targetHidden = $body.find('#toggleTarget2');
+    })
+
+    it("adds aria-role=button to all toggle trigger elements", function () {
+      expect(buttonVisible.attr('aria-role')).to.equal('button');
     })
 
     describe("target element is hidden", function () {
-      it("adds aria-hidden=true to target element", function(){
-        var hiddenTarget = $body.find('#toggleTarget2');
-        expect(hiddenTarget.attr('aria-hidden')).to.equal('true')
+      it("adds aria-hidden=true to target element", function () {
+        expect(targetHidden.attr('aria-hidden')).to.equal('true')
       })
-      it("adds (active class) on both target and trigger elements", function(){
-        var hiddenTrigger = $body.find('#toggleButton2');
-        var hiddenTarget = $body.find('#toggleTarget2');
-        expect(hiddenTrigger.hasClass(ValidCollapsable.o.activeClass)).to.be.true;
-        expect(hiddenTarget.hasClass(ValidCollapsable.o.activeClass)).to.be.true;
+      it("adds (inactive class) on both target and trigger elements", function () {
+        expect(buttonHidden.hasClass(ValidCollapsable.o.inactiveClass)).to.be.true;
+        expect(targetHidden.hasClass(ValidCollapsable.o.inactiveClass)).to.be.true;
       })
     })
 
     describe("target element is visible", function () {
-      it("adds aria-hidden=false to target element", function(){
-        var visibleTarget = $body.find('#toggleTarget1');
-        expect(visibleTarget.attr('aria-hidden')).to.equal('false')
+      it("adds aria-hidden=false to target element", function () {
+        expect(targetVisible.attr('aria-hidden')).to.equal('false')
+      })
+      it("adds (active class) on both target and trigger elements", function () {
+        expect(buttonVisible.hasClass(ValidCollapsable.o.activeClass)).to.be.true;
+        expect(targetVisible.hasClass(ValidCollapsable.o.activeClass)).to.be.true;
       })
     })
   })
 
+  describe("when trigger is activated on closed element", function () {
+    var button, target;
 
-  describe("when trigger is activated on closed element", function(){
-    xit("sets aria-hidden=false on target element", function(){})
-    xit("adds (active class) on both target and trigger elements", function(){})
+    function setTargetToHidden(hidden) {
+      if (hidden && target.hasClass(ValidCollapsable.o.activeClass)) {
+        button.trigger('click')
+      } else if (!hidden && target.hasClass(ValidCollapsable.o.inactiveClass)) {
+        button.trigger('click')
+      }
+    }
 
-    describe("when the accordion options is enabled", function(){
-      xit("closes any open target elements in the collection", function(){})
+    before(function () {
+      button = $body.find('#toggleButton1');
+      target = $body.find('#toggleTarget1');
     })
 
-    it("adds keyboard(space) support to trigger elements", function(done){
-      var button = $body.find('#toggleButton1');
-      var target = $body.find('#toggleTarget1');
-      console.log(target.hasClass(ValidCollapsable.o.activeClass))
+    it("sets aria-hidden=false on target element", function () {
+      // Set initial state to hidden then trigger toggle
+      setTargetToHidden(true);
+      button.trigger('click');
+      expect(target.attr('aria-hidden')).to.equal('false');
+    })
+
+    it("adds (active class) on both target and trigger elements", function () {
+      // Set initial state to hidden then trigger toggle
+      setTargetToHidden(true);
+      button.trigger('click');
+      expect(button.hasClass(ValidCollapsable.o.activeClass)).to.be.true;
+      expect(target.hasClass(ValidCollapsable.o.activeClass)).to.be.true;
+    })
+
+    it("adds keyboard(space) support to trigger elements", function (done) {
+      setTargetToHidden(true);
+
       // Trigger keypress on spacebar
-      var e = jQuery.Event( 'keydown', { which: 32, keyCode: 32 } );
+      var e = jQuery.Event("keypress");
+      e.which = 32;
+      e.charCode = 32;
+
       button.trigger(e);
-      setTimeout(function(){
-      console.log(target.hasClass(ValidCollapsable.o.activeClass))
-        expect(target.hasClass(ValidCollapsable.o.activeClass)).to.be.true;
-        done()
-      },200)
+      expect(target.hasClass(ValidCollapsable.o.inactiveClass)).to.be.false;
+      expect(target.hasClass(ValidCollapsable.o.activeClass)).to.be.true;
+
+      done();
+    })
+
+    describe("when the accordion options is enabled", function () {
+      xit("closes any open target elements in the collection", function () {
+      })
+    })
+  })
+
+  describe("when trigger is activated on an open element", function () {
+    var button, target;
+
+    function setTargetToHidden(hidden) {
+      if (hidden && target.hasClass(ValidCollapsable.o.activeClass)) {
+        button.trigger('click')
+      } else if (!hidden && target.hasClass(ValidCollapsable.o.inactiveClass)) {
+        button.trigger('click')
+      }
+    }
+
+    before(function () {
+      button = $body.find('#toggleButton1');
+      target = $body.find('#toggleTarget1');
+    })
+
+    it("sets aria-hidden=true on target element", function () {
+      // Set initial state to hidden then trigger toggle
+      setTargetToHidden(false);
+      button.trigger('click');
+      expect(target.attr('aria-hidden')).to.equal('true');
+    })
+
+    it("adds (inactive class) on both target and trigger elements", function () {
+      // Set initial state to hidden then trigger toggle
+      setTargetToHidden(false);
+      button.trigger('click');
+      expect(button.hasClass(ValidCollapsable.o.inactiveClass)).to.be.true;
+      expect(target.hasClass(ValidCollapsable.o.inactiveClass)).to.be.true;
+    })
+
+    it("adds keyboard(space) support to trigger elements", function (done) {
+      setTargetToHidden(false);
+
+      // Trigger keypress on spacebar
+      var e = jQuery.Event("keypress");
+      e.which = 32;
+      e.charCode = 32;
+
+      button.trigger(e);
+      expect(target.hasClass(ValidCollapsable.o.activeClass)).to.be.false;
+      expect(target.hasClass(ValidCollapsable.o.inactiveClass)).to.be.true;
+
+      done();
+    })
+  })
+
+})
+
+describe("mas_collapsable#options", function () {
+  var Collapsable;
+  var $body;
+
+  before(function (done) {
+    $('body').html(JST['templates/collapsable']());
+    $body = $('body');
+
+    require(['collapsable'], function (mod) {
+      Collapsable = new mod({
+        triggerEl:     $('#optionsTest > li > a'),
+        targetEl:      'ul',
+        showOnlyFirst: true,
+        accordion:     true,
+        closeOffFocus: true
+      });
+
+      console.info('!!Collapsable options');
+      console.info(Collapsable);
+
+      done();
+    }, done);
+
+  })
+
+  // after(function(){
+  //   Collapsable = false;
+  // })
+
+  describe("when the showOnlyFirst option is enabled", function () {
+    it("accordion activeClass should = true", function () {
+      expect(Collapsable.o.showOnlyFirst).to.be.true;
+    })
+
+    it("add activeClass to the first item and inactive class to all subsequent sections", function () {
+      expect(Collapsable.o.showOnlyFirst).to.be.true;
+      var S = Collapsable.sections;
+      $.each(S, function (i, el) {
+        if (i === 0) {
+          expect(S[i].hidden).to.be.false;
+        } else {
+          expect(S[i].hidden).to.be.true;
+        }
+      })
     })
 
   })
 
-  // describe("when trigger is activated on open element", function(){
-  //   it("toggles aria-hidden=true on target element", function(){})
-  //   it("removes (active class) on both target and trigger elements", function(){})
-  // })
+  describe("when the accordion options is enabled", function () {
+    it("accordion option should = true", function () {
+      expect(Collapsable.o.accordion).to.be.true;
+    })
 
-  // describe("when the closeOffFocus option is enabled", function(){
-  //   it("closes the open element when focus leaves it", function(){})
-  // })
+    it("closes any open target elements in the collection", function () {
+      var S = Collapsable.sections;
 
+      S[2].trigger.click();
 
+      $.each(S, function (i, el) {
+        if (i === 2) {
+          expect(S[i].hidden).to.be.false;
+        } else {
+          expect(S[i].hidden).to.be.true;
+        }
+      })
+    })
+  })
 
+  describe("when the closeOffFocus option is enabled", function () {
+    xit("closes the open element when focus leaves it", function () {
+    })
+  })
 
-
-
-
-
-  // it("should add activeClass to all sections except first if o.showOnlyFirst is true", function(){
-  //   var S = $body.find(ValidCollapsable.o.triggerEl);
-  //   S.each(function(i,el){
-  //     if(i === 0){
-  //       expect($(el).hasClass(ValidCollapsable.o.activeClass)).to.be.true;
-  //     }else{
-  //       expect($(el).hasClass(ValidCollapsable.o.activeClass)).to.be.false;
-  //     }
-  //   })
-  // })
-
-  // it("should toggle the active class on both trigger and target element when the toggle element is clicked", function (done) {
-  //   var button = $body.find('#toggleButton1');
-  //   var target = $body.find('#toggleTarget1');
-
-  //   // Confirm initial state
-  //   expect(button.hasClass(ValidCollapsable.o.activeClass)).to.be.false;
-  //   expect(target.hasClass(ValidCollapsable.o.activeClass)).to.be.false;
-
-  //   // Force interaction
-  //   button.trigger('click');
-
-  //   // Test expected state
-  //   expect(button.hasClass(ValidCollapsable.o.activeClass)).to.be.true;
-  //   expect(target.hasClass(ValidCollapsable.o.activeClass)).to.be.true;
-  //   done();
-
-  // })
+  describe("ways of targeting the trigger and target elements", function () {
+    xit("closes the open element when focus leaves it", function () {
+    })
+  })
 
 })

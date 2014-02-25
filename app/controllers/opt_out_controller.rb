69a954a8eda5afca2c3510b09017238279c83f55
Pablo Vicente <PabloVicente.Carrera@moneyadviceservice.org.uk>
2014-02-25 10:52:34 +0000

    Get redirection page from params
    If redirection page is not provided it redirects to home page
    It avoids problems with HTTP_REFERRER headers

--- app/controllers/opt_out_controller.rb
+++ app/controllers/opt_out_controller.rb
@@ -1,6 +1,12 @@
 class OptOutController < ApplicationController
   def create
     cookies.permanent[:roptin] = 0
-    redirect_to(:back)
+    redirect_to(redirection_page)
+  end
+
+  private
+
+  def redirection_page
+    params[:redirection_page] || root_path
   end
 end

104bc2fd401b64d795a1689dd6362d8d49a650f3
Andrew Garner <andrew@andrewgarner.com>
2014-02-26 20:32:54 +0000

    Inline 'in_head.js' and remove from spec helper
    We can't reference this file in production. Doing so causes Sprockets to
    fail because it tries to interpret the asset but does not have the
    a Javascript runtime to do so successfully.
    
    Delete mas_analytics - this is supposed to be a unit test and should
    not depend on global objects. We need to start again with this test.

--- spec/javascripts/spec_helper.js
+++ spec/javascripts/spec_helper.js
@@ -1,10 +1,10 @@
 //= require sinonjs/sinon
-//= require in_head
 //= require requirejs/require
 //= require jquery
 //= require require_config
 
 var expect = chai.expect;
+var MAS = window.MAS || {};
 
 // There is a problem abstracting console.log to MAS.log - this prevents it from blocking the test
 // Might be that .log is not in the scope of window when run in CL

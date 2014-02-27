58c7184360a4829e55c3bb5abd1e7f193e7756c9
Andy Tennison <andy.tennison@moneyadviceservice.org.uk>
2014-02-27 12:53:37 +0000

    Added a log.js module
    start of refactor of global MAS object

---
+++ app/assets/javascripts/modules/mas_log.js
@@ -0,0 +1,22 @@
+define(['jquery'], function ($) {
+
+  // Wrap logging so it can be pushed to log in prod, and used safely on old browsers
+  var opts = ['log', 'info', 'warn', 'error'],
+      l = opts.length,
+      logs = {},
+      logged = [];
+
+  function logIt (name, options) {
+    logged.push([name, options, type])
+  }
+
+  while (l--) {
+    var type = l;
+    logs[opts[l]] = (window && window.console && MAS.env !== 'production') ? console[opts[l]]: logIt;
+  }
+
+  if(window) window.logged = logged;
+
+  return logs
+
+})

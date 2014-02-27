11bff24270d30b41287e57e610f8eef505ab73c7
Andy Tennison <andy.tennison@moneyadviceservice.org.uk>
2014-02-27 10:43:05 +0000

    fix collapsable JS bug and update spec test

--- app/assets/javascripts/modules/mas_collapsable.js
+++ app/assets/javascripts/modules/mas_collapsable.js
@@ -1,29 +1,42 @@
 
 define(['jquery'], function ($) {
-
   "use strict";
 
-  console.log('! v10')
-
   var defaults = {
-    targetType: 'next',                 // options: next | data | href
+    // Setup
     triggerEl: '.collapsible',
     targetEl: '.collapsible-section',
     activeClass: 'is-on',
     inactiveClass: 'is-off',
-    showIcon: false,
-    headingIcon: '<span class="icon icon--toggle"></span><span class="visually-hidden">{{txt}}</span>',
     closeOffFocus: false,
     accordion: false,
     showOnlyFirst: false,
+
+    // Display options
+    showIcon: false,
+    headingIcon: '<span class="icon icon--toggle"></span><span class="visually-hidden">{{txt}}</span>',
+    useButton: false,
+
+    // Localised text strings
     textString: {
       show_this_section: 'Show this section',
       hide_this_section: 'Hide this section'
     }
   };
 
+  $.fn.swapClass = function(from,to){
+    var c = this[0].className;
+    if( c.indexOf(to) !== -1 ){
+      return;
+    }else if( c.indexOf(from) === -1 ){
+      this[0].className = c + ' ' + to;
+    }else{
+      this[0].className = c.replace(from,to);
+    }
+  }
+
   var Collapsible = function(opts){
-    this.o = $.extend(defaults, opts);
+    this.o = $.extend({}, defaults, opts);
     this.sections = [];
     this.selected = false; 
 
@@ -34,58 +47,56 @@ define(['jquery'], function ($) {
     if(l === 0) return MAS.warn('mas_collapsible => no trigger elements in page: ' + this.o.triggerEl);
 
     for(i; i<l; i++){
-      Collapsible.prototype._setupEach.call(this, i, triggers[i])      
+      this._setupEach.call(this, i, triggers[i]);
     }
   }
 
   Collapsible.prototype._modifyButtonHTML = function(i){
+    var trigger = this.sections[i].trigger,
+        target =  this.sections[i].target,
+        icon = (this.o.showIcon)? trigger.prepend(this.o.headingIcon.replace('{{txt}}', this.o.textString.show_this_section)): '';
+
+    if(this.o.useButton){
+      var buttonTitle = trigger.text();
+
+      if(trigger[0].nodeName === 'A'){
+        // Anchor => replace elemnt
+        var newEl = $('<a class-"' + trigger[0].className + '" id="' + trigger[0] + '">' + icon + buttonTitle + '</a>');
+        // add new
+        trigger.after(newEl);
+        // remove old
+        trigger.remove();
+        trigger = newEl;
+      }else{
+        // Anything else => insert button inside
+        trigger.html('<button class="button--unstyled">' + icon + buttonTitle  + '</button>');
+      }
+    }else{
+      // Use aria-role
+      trigger.attr('aria-role','button');
+      trigger.attr('tabindex', '0');
+      trigger.prepend(icon);
+    }
+
+
     // move modify code here for cleaner separation of accessibility stuff
     // Wrap in button element to assist with accessibility
-    var buttonTitle = this.sections[i].trigger.text();
-    // this.sections[i].trigger.html('<button class="button--unstyled">' + this.o.headingIcon + buttonTitle  + '</button>');
-    this.sections[i].trigger.attr('aria-role','button');
-    this.sections[i].trigger.attr('tabindex', '0');
 
     if(this.o.showIcon){
-      this.sections[i].trigger.prepend(this.o.headingIcon.replace('{{txt}}', this.o.textString.show_this_section));
-      this.sections[i].icon = this.sections[i].trigger.find('.visually-hidden');
-    } 
-  }
-
-  Collapsible.prototype._findTarget = function($el){
-    switch(this.o.targetType){
-      case 'data':
-        // Trigger must have data-target = string that can be interpreted by jQuery
-        var d = $el.attr('data-target');
-        return $(d);
-        break;
-      case 'href':
-        var href = $el.attr('href');
-        if( href.indexOf('#') !== 0 ){
-          // HREF must be formatted as #ID 
-          return false;
-        }else{
-          return $(href);
-        }
-        break;
-      case 'next':
-      default:
-        return $el.next(this.o.targetEl);
-        break;
+      this.sections[i].icon = trigger.find('.visually-hidden');
     }
   }
 
   Collapsible.prototype._setupEach = function(i,el){
     var $el = $(el);
+    var _this = this;
 
     this.sections[i] = {
       index: i,
       trigger: $el,
-      target: this._findTarget($el),
+      target: $el.next(this.o.targetEl),
       hidden: !$el.hasClass(this.o.activeClass)
     }
-
-    console.log(i +' >> '+this.sections[i].hidden)
 
     // Dont modify or bind events if no target element
     if(!this.sections[i].target.length) return;
@@ -109,7 +120,6 @@ define(['jquery'], function ($) {
     (this.sections[i].hidden) ? this.hide(i) : this.show(i);
 
     // Bind events
-    var _this = this;
     this.sections[i].trigger.on('click', i, function(e){
       e.preventDefault();
       (_this.sections[i].hidden)? _this.show(i) : _this.hide(i);
@@ -122,24 +132,20 @@ define(['jquery'], function ($) {
       }
     })
 
-    this.sections[i].target.on('focusout', function(e){
-      alert('! focus <output></output>')
-    })    
-  }
-
-  $.fn.swapClass = function(from,to){
-    console.log(this)
-    var c = this[0].className;
-    if( c.indexOf(to) !== -1 ){
-      return;
-    }else if( c.indexOf(from) === -1 ){
-      this[0].className = c + ' ' + to;
-    }else{
-      this[0].className = c.replace(from,to);
+    if(this.o.closeOffFocus){
+      this.sections[i].target.on('focusout', function(e){
+        setTimeout(function(){
+          if( _this.sections[i].target.find(document.activeElement).length === 0 ){
+            _this.hide(i);
+          }
+        },10)
+      })
     }
   }
 
   Collapsible.prototype.show = function(i){
+    console.log('show'+i)
+    console.log(this.o)
     var item = this.sections[i];
     if(this.o.showIcon) item.icon.text(this.o.textString.show_this_section);
     item.trigger.swapClass(this.o.inactiveClass, this.o.activeClass);
@@ -152,6 +158,7 @@ define(['jquery'], function ($) {
   }
   
   Collapsible.prototype.hide = function(i){
+    console.log('hide'+i)
     var item = this.sections[i];
     if(this.o.showIcon) item.icon.text(this.o.textString.hide_this_section);
     item.trigger.swapClass(this.o.activeClass, this.o.inactiveClass);

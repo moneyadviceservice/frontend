e7389eb65a79aa6aec9ce84a5b186e1b672fd427
Jon Gilbraith <jon.gilbraith@moneyadviceservice.org.uk>
2014-03-06 11:16:54 +0000

    Adapt the core initializer to use the content service search repository.

--- config/initializers/core.rb
+++ config/initializers/core.rb
@@ -4,10 +4,13 @@ require 'core/registries/repository'
 require 'core/repositories/action_plans/public_website'
 require 'core/repositories/articles/public_website'
 require 'core/repositories/categories/public_website'
-require 'core/repositories/search/public_website'
+require 'core/repositories/search/content_service'
 
 Core::Registries::Connection[:public_website] =
   Core::ConnectionFactory.build(ENV['MAS_PUBLIC_WEBSITE_URL'])
+
+Core::Registries::Connection[:content_service] =
+  Core::ConnectionFactory.build(ENV['MAS_CONTENT_SERVICE_URL'])
 
 Core::Registries::Repository[:action_plan] =
   Core::Repositories::ActionPlans::PublicWebsite.new
@@ -19,4 +22,4 @@ Core::Registries::Repository[:category] =
   Core::Repositories::Categories::PublicWebsite.new
 
 Core::Registries::Repository[:search] =
-  Core::Repositories::Search::PublicWebsite.new
+  Core::Repositories::Search::ContentService.new

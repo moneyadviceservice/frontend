2859fd9380310f069d855910176d7e24fe15ad51
Steven Wilkin <stevenwilkin@gmail.com>
2014-03-06 14:12:50 +0000

    Use factories defined under features/factories/

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -8,7 +8,12 @@ end
 require_relative '../config/environment'
 require 'mas/development_dependencies/rspec/spec_helper'
 require 'webmock/rspec'
+require 'factory_girl'
+
+FactoryGirl.definition_file_paths << File.join(File.dirname(__FILE__), '..', 'features', 'factories')
+FactoryGirl.find_definitions
 
 RSpec.configure do |c|
+  c.include FactoryGirl::Syntax::Methods
   c.alias_it_should_behave_like_to :it_has_behavior, 'exhibits behaviour of an'
 end

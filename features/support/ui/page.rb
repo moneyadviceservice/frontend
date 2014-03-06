b7839047b8e59ee48d47e7bd10c476e2642db153
Jon Gilbraith <jon.gilbraith@moneyadviceservice.org.uk>
2014-03-06 14:55:05 +0000

    Move some of the repeated declaration in page objects into the base class.

--- features/support/ui/page.rb
+++ features/support/ui/page.rb
@@ -1,7 +1,12 @@
 require 'site_prism'
+require_relative 'sections/footer_site_links'
 
 module UI
   class Page < SitePrism::Page
+    element :heading, 'h1'
+
     element :description, :xpath, "//meta[@name='description']", visible: false
+
+    section :footer_site_links, UI::Sections::FooterSiteLinks, '.footer-site-links'
   end
 end

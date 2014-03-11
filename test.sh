ac5c84a45c882a3fb487294267e6f205b4da3e0e
Rich Kang <rich@saekang.co.uk>
2014-03-11 16:31:37 +0000

    Parallelise bundle install for tests

--- test.sh
+++ test.sh
@@ -6,10 +6,13 @@ export PATH=./bin:$PATH
 export RAILS_ENV=test
 export BUNDLE_WITHOUT=development
 
+CORES=$(grep -c ^processor /proc/cpuinfo 2>/dev/null || sysctl -n hw.ncpu)
+BUNDLE_JOBS=$((CORES-1))
+
 CI_PIPELINE_COUNTER=${GO_PIPELINE_COUNTER-0}
 CI_EXECUTOR_NUMBER=${EXECUTOR_NUMBER-0}
 
-bundle install
+bundle install --jobs $BUNDLE_JOBS
 bower install
 
 rake spec

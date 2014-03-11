ae35a37438a37b04af58e6d3c737593a68605741
Jon Gilbraith <jon.gilbraith@moneyadviceservice.org.uk>
2014-03-11 13:41:41 +0000

    Apply links to the search results.

--- app/decorators/search_result_decorator.rb
+++ app/decorators/search_result_decorator.rb
@@ -1,3 +1,17 @@
 class SearchResultDecorator < Draper::Decorator
   delegate :title, :description
+
+  def path
+    case object.type
+    when 'article', 'guide', 'news'
+      h.article_path(object.id, locale: I18n.locale)
+    when 'action-plan'
+      h.action_plan_path(object.id, locale: I18n.locale)
+    when 'category'
+      h.category_path(object.id, locale: I18n.locale)
+    when 'campaign', 'tool', 'video'
+      "/#{I18n.locale}/#{object.type.pluralize}/#{object.id}"
+    end
+  end
+
 end

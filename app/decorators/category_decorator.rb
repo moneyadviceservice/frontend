bcf723fedd3e56fb69c62b1ee577a8e3d0c8466a
Steven Wilkin <stevenwilkin@gmail.com>
2014-03-14 09:38:22 +0000

    Push responsibility of rendering the appropraire partial into the decorator

--- app/decorators/category_decorator.rb
+++ app/decorators/category_decorator.rb
@@ -1,6 +1,5 @@
 class CategoryDecorator < Draper::Decorator
   delegate :title, :description
-  delegate :grandparent?, :parent?, :child?
 
   def contents
     CategoryContentDecorator.decorate_collection(object.contents || [])
@@ -8,5 +7,17 @@ class CategoryDecorator < Draper::Decorator
 
   def path
     h.category_path(object.id)
+  end
+
+  def render_contents
+    partial = if object.grandparent?
+      'parent_categories'
+    elsif object.parent?
+      'child_categories'
+    elsif object.child?
+      'content_items'
+    end
+
+    h.render partial, contents: contents
   end
 end

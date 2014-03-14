b820840d3073fcd72e4dc585cda322b51518849d
Jon Gilbraith <jon.gilbraith@moneyadviceservice.org.uk>
2014-03-14 16:40:20 +0000

    WIP - Add a prism page for the 'partners' page.

---
+++ features/support/ui/pages/partners.rb
@@ -0,0 +1,9 @@
+require_relative '../page'
+require_relative '../sections/header'
+require_relative '../sections/footer_social_links'
+
+module UI::Pages
+  class Partners < UI::Page
+    set_url '/{locale}/categories/partners'
+  end
+end
--- features/support/ui/sections/footer_site_links.rb
+++ features/support/ui/sections/footer_site_links.rb
@@ -2,6 +2,7 @@ require_relative '../section'
 
 module UI::Sections
   class FooterSiteLinks < UI::Section
+    element :partners_link, 'li:nth-child(2)'
     element :welsh_link, '#cyLink'
     element :english_link, '#enLink'
   end

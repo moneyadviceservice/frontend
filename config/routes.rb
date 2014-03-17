7975154218613217f9a68c96f3c75c04f44a44a6
Andy Tennison <andy.tennison@moneyadviceservice.org.uk>
2014-03-17 14:36:16 +0000

    Add contact section to home page

--- config/routes.rb
+++ config/routes.rb
@@ -37,7 +37,7 @@ Rails.application.routes.draw do
           get 'pages_guide', path: '/guide'
           get 'pages_error', path: '/error'
           get 'pages_action_plan', path: '/action-plan'
-          get 'pages_homepage', path: '/homepage'
+          get 'pages_home', path: '/home'
           get 'pages_search_results', path: '/search_results'
           get 'pages_parent_category_page', path: '/parent_category_page'
           get 'pages_child_category_page', path: '/child_category_page'

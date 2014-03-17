4882b97ae268e17d80521f3f28969864ecfdecf2
Jon Gilbraith <jon.gilbraith@moneyadviceservice.org.uk>
2014-03-17 14:11:46 +0000

    Finish cookie disclosure features.
    Various refactoring performed based on discussions.

--- features/support/ui/sections/footer_site_links.rb
+++ features/support/ui/sections/footer_site_links.rb
@@ -3,6 +3,7 @@ require_relative '../section'
 module UI::Sections
   class FooterSiteLinks < UI::Section
     element :partners_link, 'li:nth-child(2)'
+    element :cookie_guide_link, '.footer-site-links__cookie-message a'
     element :welsh_link, '#cyLink'
     element :english_link, '#enLink'
   end

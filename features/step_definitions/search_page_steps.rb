db89779a05e634af95a0cc8fbf70efc3d154eab4
Andrew Garner <andrew@andrewgarner.com>
2014-03-18 16:54:30 +0000

    Add feature block to home page

--- features/step_definitions/search_page_steps.rb
+++ features/step_definitions/search_page_steps.rb
@@ -1,3 +1,28 @@
+When(/^I search for something relevant$/) do
+  home_page.search_box.input.set 'health'
+  VCR.use_cassette("search_relevant") do
+    home_page.search_box.submit.click
+  end
+end
+
+When(/^I search for something irrelevant$/) do
+  home_page.search_box.input.set 'cats'
+
+  VCR.use_cassette("search_irrelevant") do
+    home_page.search_box.submit.click
+  end
+end
+
+When(/^I submit a search with no query$/) do
+  home_page.search_box.input.set ''
+  home_page.search_box.submit.click
+end
+
+
+Then(/^I should see the search box$/) do
+  home_page.should have_search_box
+end
+
 Then(/^I should see search results$/) do
   search_results_page.has_results?
 end

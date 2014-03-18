2f29fb6a2b214ef937c92e49c0dba0fb53f6c3ca
alexwllms <alex.williams@moneyadviceservice.org.uk>
2014-03-18 15:07:48 +0000

    Move category item label and icon class to decorator

--- app/decorators/category_content_decorator.rb
+++ app/decorators/category_content_decorator.rb
@@ -1,5 +1,9 @@
 class CategoryContentDecorator < Draper::Decorator
-  delegate :id, :title, :description, :type
+  delegate :id, :title, :description
+
+  def label
+    "#{object.type.titleize} - "
+  end
 
   def contents
     CategoryContentDecorator.decorate_collection(object.contents || [])
@@ -7,15 +11,18 @@ class CategoryContentDecorator < Draper::Decorator
 
   def path
     case object.class.to_s
-    when 'Core::Article'
-      h.article_path(object.id, locale: I18n.locale)
-    when 'Core::ActionPlan'
-      h.action_plan_path(object.id, locale: I18n.locale)
-    when 'Core::Category'
-      h.category_path(object.id, locale: I18n.locale)
-    when 'Core::Other'
-      "/#{I18n.locale}/#{object.type.pluralize}/#{object.id}"
+      when 'Core::Article'
+        h.article_path(object.id, locale: I18n.locale)
+      when 'Core::ActionPlan'
+        h.action_plan_path(object.id, locale: I18n.locale)
+      when 'Core::Category'
+        h.category_path(object.id, locale: I18n.locale)
+      when 'Core::Other'
+        "/#{I18n.locale}/#{object.type.pluralize}/#{object.id}"
     end
   end
 
+  def icon_class
+    "icon--#{object.type.dasherize}"
+  end
 end

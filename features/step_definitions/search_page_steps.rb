624d2f190e6768b78d8eb9f4991078d00c43e3e9
Andrew Garner <andrew@andrewgarner.com>
2014-03-19 15:34:27 +0000

    Use expect syntax for search feature

--- features/step_definitions/search_page_steps.rb
+++ features/step_definitions/search_page_steps.rb
@@ -1,5 +1,6 @@
 When(/^I search for something relevant$/) do
   home_page.search_box.input.set 'health'
+
   VCR.use_cassette("search_relevant") do
     home_page.search_box.submit.click
   end
@@ -18,25 +19,24 @@ When(/^I submit a search with no query$/) do
   home_page.search_box.submit.click
 end
 
-
 Then(/^I should see the search box$/) do
-  home_page.should have_search_box
+  expect(home_page).to have_search_box
 end
 
 Then(/^I should see search results$/) do
-  search_results_page.has_results?
+  expect(search_results_page).to have_results
 end
 
 Then(/^I should see no search results$/) do
-  search_results_page.has_no_results?
+  expect(search_results_page).to have_no_results
 end
 
 Then(/^I should prompted to try another search term$/) do
-  search_results_page.has_content? I18n.t('search_results.index_no_results.body')
+  expect(search_results_page).to have_content(I18n.t('search_results.index_no_results.body'))
 end
 
 Then(/^I should prompted to try again with a search term$/) do
-  search_results_page.has_content? I18n.t('search_results.index.body')
+  expect(search_results_page).to have_content(I18n.t('search_results.index.body'))
 end
 
 Then(/^the search results page should have a robots tag with value noindex$/) do

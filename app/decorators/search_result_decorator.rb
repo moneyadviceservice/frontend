90745384aff60ce78d27005547f035ea0f7dc819
Andrew Garner <andrew@andrewgarner.com>
2014-03-19 11:59:58 +0000

    Extract search result partial

--- app/decorators/search_result_decorator.rb
+++ app/decorators/search_result_decorator.rb
@@ -1,17 +1,20 @@
 class SearchResultDecorator < Draper::Decorator
   delegate :title, :description
 
-  def path
-    case object.type
-    when 'article', 'guide'
-      h.article_path(object.id, locale: I18n.locale)
-    when 'action-plan'
-      h.action_plan_path(object.id, locale: I18n.locale)
-    when 'category'
-      h.category_path(object.id, locale: I18n.locale)
-    when 'campaign', 'news', 'tool', 'video'
-      "/#{I18n.locale}/#{object.type.pluralize}/#{object.id}"
-    end
+  def to_partial_path
+    'search_result'
   end
 
+  def path
+    case object.type
+      when 'article', 'guide'
+        h.article_path(object.id, locale: I18n.locale)
+      when 'action-plan'
+        h.action_plan_path(object.id, locale: I18n.locale)
+      when 'category'
+        h.category_path(object.id, locale: I18n.locale)
+      when 'campaign', 'news', 'tool', 'video'
+        "/#{I18n.locale}/#{object.type.pluralize}/#{object.id}"
+    end
+  end
 end

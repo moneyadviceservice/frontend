0d491df08ac975abb3003e4029407a4d607eb619
Andrew Garner <andrew@andrewgarner.com>
2014-03-19 14:52:21 +0000

    Set page title for search pages

--- features/step_definitions/search_page_steps.rb
+++ features/step_definitions/search_page_steps.rb
@@ -23,20 +23,37 @@ Then(/^I should see the search box$/) do
   expect(home_page).to have_search_box
 end
 
+Then(/^I should see the search page$/) do
+  expected_heading = ""
+  expected_title   = '%s - %s' % [I18n.t('search_results.index.document_title'),
+                                  I18n.t('layouts.base.title')]
+
+  expect(search_results_page.title).to eq(expected_title)
+  expect(search_results_page.heading).to have_content(expected_heading)
+end
+
 Then(/^I should see search results$/) do
+  expected_heading = I18n.t('search_results.index_with_results.page_title', query: 'health')
+  expected_title   = '%s - %s' % [I18n.t('search_results.index_with_results.document_title', query: 'health'),
+                                  I18n.t('layouts.base.title')]
+
+  expect(search_results_page.title).to eq(expected_title)
+  expect(search_results_page.heading).to have_content(expected_heading)
   expect(search_results_page).to have_results
 end
 
 Then(/^I should see no search results$/) do
+  expected_heading = I18n.t('search_results.index_no_results.page_title', query: 'cats')
+  expected_title   = '%s - %s' % [I18n.t('search_results.index_no_results.document_title', query: 'cats'),
+                                  I18n.t('layouts.base.title')]
+
+  expect(search_results_page.title).to eq(expected_title)
+  expect(search_results_page.heading).to have_content(expected_heading)
   expect(search_results_page).to have_no_results
 end
 
 Then(/^I should prompted to try another search term$/) do
   expect(search_results_page).to have_content(I18n.t('search_results.index_no_results.body'))
-end
-
-Then(/^I should prompted to try again with a search term$/) do
-  expect(search_results_page).to have_content(I18n.t('search_results.index.body'))
 end
 
 Then(/^the search results page should have a robots tag with value noindex$/) do

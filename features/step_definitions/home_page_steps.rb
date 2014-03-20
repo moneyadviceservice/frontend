20df0b9b0ae75a8105c08ef5203d09c9e5032842
Pablo Vicente <PabloVicente.Carrera@moneyadviceservice.org.uk>
2014-03-20 15:59:54 +0000

    Replace \n by space

--- features/step_definitions/home_page_steps.rb
+++ features/step_definitions/home_page_steps.rb
@@ -78,5 +78,5 @@ Then(/^the home page should have an alternate tag for the (.*) version$/) do |la
 end
 
 Then(/^the home page should have a description tag for that language version$/) do
-  expect { home_page.description[:content] }.to become(I18n.t('home.show.description'))
+  expect { home_page.description[:content] }.to become(I18n.t('home.show.description').gsub(/\n/, " "))
 end

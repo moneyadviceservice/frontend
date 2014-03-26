96b43d9bbb929bc153a0d28c82e6434fb76170c2
Pablo Vicente <PabloVicente.Carrera@moneyadviceservice.org.uk>
2014-03-26 11:51:35 +0000

    Redirect '/?locale=en' to '/' and '/?locale=cy' to '/cy'

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,16 @@
 class HomeController < ApplicationController
+  before_action :home_redirection
+
   def show
 
+  end
+
+  private
+
+  def home_redirection
+    case request.env['REQUEST_URI']
+    when '/?locale=en' then redirect_to '/'
+    when '/?locale=cy' then redirect_to '/cy'
+    end
   end
 end

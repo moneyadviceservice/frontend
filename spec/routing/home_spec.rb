064337f30b38145804afcd5c6ceef61adfab923a
Andrew Garner <andrew@surefirething.co.uk>
2014-03-27 13:39:59 +0000

    Add Welsh test case to home routing spec

--- spec/routing/home_spec.rb
+++ spec/routing/home_spec.rb
@@ -4,4 +4,8 @@ describe 'Home routing' do
   it 'routes /en to the home controller' do
     expect(get('/en')).to route_to(controller: "home", action: "show", locale: "en")
   end
+  
+  it 'routes /cy to the home controller' do
+    expect(get('/cy')).to route_to(controller: "home", action: "show", locale: "cy")
+  end
 end

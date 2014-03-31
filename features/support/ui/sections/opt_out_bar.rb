3470ace3638a03e59a86628a84ba4c74eea054b1
alexwllms <alex.williams@moneyadviceservice.org.uk>
2014-03-31 15:32:54 +0100

    Fix failing tests

--- features/support/ui/sections/opt_out_bar.rb
+++ features/support/ui/sections/opt_out_bar.rb
@@ -2,7 +2,7 @@ require_relative '../section'
 
 module UI::Sections
   class OptOutBar < UI::Section
-    element :dismiss_button, '.opt-out__close__button'
-    element :opt_out_button, '.opt-out__button'
+    element :dismiss_button, '.opt-out__close'
+    element :opt_out_button, '.opt-out__control'
   end
 end

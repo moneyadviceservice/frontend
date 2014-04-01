6fa2ce091e8e3ea7014e2a7067a00ed87eead273
Pablo Vicente <PabloVicente.Carrera@moneyadviceservice.org.uk>
2014-04-01 11:34:47 +0100

    Add css lint options

--- lib/asset_pipeline/processors/css_lint.rb
+++ lib/asset_pipeline/processors/css_lint.rb
@@ -2,21 +2,25 @@ module AssetPipeline
   module Processors
     class CssLint < Sprockets::Processor
       def evaluate(context, locals)
-      #  lint_results = CsslintRuby.run(data)
-      #  binding.pry
-      #  if lint_results.errors.present?
-      #    context.__LINE__ = format_errors(lint_results)
-      #    raise "Lint errors"
-      #  else
-      #    data
-      #  end
-      data
+       lint_results = CsslintRuby.run(data, options)
+       if lint_results.errors.present?
+         context.__LINE__ = format_errors(lint_results)
+         raise "Lint errors"
+       else
+         data
+       end
       end
 
       private
 
       def format_errors(lint_results)
         lint_results.errors.first[:message]
+      end
+
+      def options
+        YAML.load(File.open(
+          File.join(Rails.root, 'lib', 'asset_pipeline', 'processors', 'config', 'css_lint_options.yml'))
+        )
       end
     end
   end

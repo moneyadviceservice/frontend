ebb05ae6067a8b38de3e79b4d0690fa8d036b25e
Steven Wilkin <stevenwilkin@gmail.com>
2014-04-01 11:40:41 +0100

    Validate a category page in both locales

--- spec/features/html_validation_spec.rb
+++ spec/features/html_validation_spec.rb
@@ -18,4 +18,26 @@ describe 'html validation' do
       specify { expect(page).to have_valid_html }
     end
   end
+
+  describe 'category pages' do
+    let(:category) { build :category_hash, :content_items }
+    let(:repository) { Core::Repositories::Categories::Fake.new(category) }
+
+    before do
+      Core::Registries::Repository[:category] = repository
+      visit category_path(category['id'], locale: locale)
+    end
+
+    context 'in English' do
+      let(:locale) { 'en' }
+
+      specify { expect(page).to have_valid_html }
+    end
+
+    context 'in Welsh' do
+      let(:locale) { 'cy' }
+
+      specify { expect(page).to have_valid_html }
+    end
+  end
 end

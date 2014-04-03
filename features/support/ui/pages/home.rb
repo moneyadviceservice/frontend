2c1eff95c89c9ee4c9e6f51d213f06f3b5a19b05
alexwllms <alex.williams@moneyadviceservice.org.uk>
2014-04-03 16:52:05 +0100

    Fix the homepage feature tests after the refactor

--- features/support/ui/pages/home.rb
+++ features/support/ui/pages/home.rb
@@ -6,6 +6,7 @@ module UI::Pages
 
     element :strapline, '.home-feature__content h1'
     element :feature_list, '.home-feature__list'
+    element :feature_list_items, '.home-feature__list-item'
     elements :promoted_items, '.home-promoted__item'
 
     element :contact_heading, '.home-contact__header h2'

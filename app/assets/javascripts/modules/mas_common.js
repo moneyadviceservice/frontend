321d34e1ae606fed38d244aab6dbcaceb67ad6ef
Andy Tennison <andy.tennison@moneyadviceservice.org.uk>
2014-04-04 09:36:57 +0100

    refactor analytics
    moved basic analytics call into common
    changed "analytics" module to be scrollTracking module
    as most projects would never need this specific functionality

--- app/assets/javascripts/modules/mas_common.js
+++ app/assets/javascripts/modules/mas_common.js
@@ -1,18 +1,26 @@
 
-
 // MAS.bootstrap.I18n_locale == en || cy == file path in require config
-
 define(
-  ['jquery', 'pubsub', 'log', 'analytics', window.MAS.bootstrap.I18n_locale],
-  function ($, pubsub, log, analytics, text) {
+  ['jquery', 'pubsub', 'log', window.MAS.bootstrap.I18n_locale],
+  function ($, pubsub, log, text) {
 
     'use strict';
 
-    var common = window.MAS || {};
+    var common = window.MAS || {},
+        dataLayer = window.dataLayer || [];
+
+    // Extend common object with globally required scripts
     common.text = text;
-    common.analytics = analytics;
     $.extend(common, pubsub);
     $.extend(common, log);
+
+    // Fire analytics events
+    // TO USE: MAS.publish('analytics:trigger', {object with props})
+    common.subscribe('analytics:trigger', function(e, data){
+      common.log('mas_analytics.triggerAnalytics', data);
+      dataLayer.push(data);
+    });
+
     return common;
   }
 );

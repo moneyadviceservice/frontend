aaefc8b6d016f39079896d4e3790df76c43aa39d
Andy Tennison <andy.tennison@moneyadviceservice.org.uk>
2014-04-10 15:32:38 +0100

    fix merge error

--- app/assets/javascripts/modules/mas_collapsable.js
+++ app/assets/javascripts/modules/mas_collapsable.js
@@ -1,9 +1,5 @@
 
-<<<<<<< HEAD
 define(['jquery','common'], function ($, MAS) {
-=======
-define(['jquery', 'common'], function ($, MAS) {
->>>>>>> fix missing require dependance
 
   'use strict';
 

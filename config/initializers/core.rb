dab19637464961d4389bcfb2666c92860a5ec84f
Pablo Vicente <PabloVicente.Carrera@moneyadviceservice.org.uk>
2014-04-15 09:23:59 +0100

    Initialise google search

--- config/initializers/core.rb
+++ config/initializers/core.rb
@@ -5,13 +5,13 @@ require 'core/repositories/repository_cache'
 require 'core/repositories/action_plans/public_website'
 require 'core/repositories/articles/public_website'
 require 'core/repositories/categories/public_website'
-require 'core/repositories/search/content_service'
+require 'core/repositories/search/google'
 
 require 'faraday/request/host_header'
 require 'faraday/request/x_forwarded_proto'
 
-content_service_connection = Core::ConnectionFactory.build(ENV['MAS_CONTENT_SERVICE_URL'])
-public_website_connection  = Core::ConnectionFactory.build(ENV['MAS_PUBLIC_WEBSITE_URL'])
+google_api_connection     = Core::ConnectionFactory.build(ENV['GOOGLE_API_URL'])
+public_website_connection = Core::ConnectionFactory.build(ENV['MAS_PUBLIC_WEBSITE_URL'])
 
 public_website_connection.builder.insert_after(Faraday::Request::RequestId,
                                                Faraday::Request::HostHeader)
@@ -19,8 +19,8 @@ public_website_connection.builder.insert_after(Faraday::Request::RequestId,
 public_website_connection.builder.insert_after(Faraday::Request::RequestId,
                                                Faraday::Request::XForwardedProto)
 
-Core::Registries::Connection[:content_service] = content_service_connection
-Core::Registries::Connection[:public_website]  = public_website_connection
+Core::Registries::Connection[:google_api]     = google_api_connection
+Core::Registries::Connection[:public_website] = public_website_connection
 
 Core::Registries::Repository[:action_plan] =
   Core::Repositories::ActionPlans::PublicWebsite.new
@@ -32,4 +32,4 @@ Core::Registries::Repository[:category] = Core::RepositoryCache.new(
   Core::Repositories::Categories::PublicWebsite.new)
 
 Core::Registries::Repository[:search] =
-  Core::Repositories::Search::ContentService.new
+  Core::Repositories::Search::Google.new

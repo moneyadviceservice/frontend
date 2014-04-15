29c35a7e8912901e084ef543c785cda4325d1757
Pablo Vicente <PabloVicente.Carrera@moneyadviceservice.org.uk>
2014-04-15 16:42:40 +0100

    Mapper maps the whole request

---
+++ lib/core/repositories/search/google_request_mapper.rb
@@ -0,0 +1,30 @@
+module Core::Repositories
+  module Search
+    class GoogleRequestMapper
+      SECOND_TO_LAST = -2
+
+      def map(response)
+        items = response.body.fetch('items', [])
+        items.map do |item|
+          {
+            id:          item['link'].split('/').last,
+            type:        item['link'].split('/')[SECOND_TO_LAST].singularize,
+            description: extract_description(item['pagemap']['metatags']),
+            title:       item['title']
+          }
+        end
+      end
+
+      private
+
+      def extract_description(metatags_array)
+        description = ''
+        metatags_array.each do |metatags|
+          metatags.each { |key, value| return description = value if key == 'og:description' }
+        end
+
+        description
+      end
+    end
+  end
+end

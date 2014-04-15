c28f6df474d422bcb5c845d7db2c6f0a876170e7
Pablo Vicente <PabloVicente.Carrera@moneyadviceservice.org.uk>
2014-04-15 14:25:06 +0100

    Raise Core::Repository::RequestError on request error

--- spec/lib/core/repositories/search/google_spec.rb
+++ spec/lib/core/repositories/search/google_spec.rb
@@ -8,11 +8,20 @@ describe Core::Repositories::Search::Google  do
     let(:search_options) { "key=#{ENV['GOOGLE_API_KEY']}&cx=#{ENV['GOOGLE_API_CX']}&q=#{query}" }
     let(:status) { 200 }
 
+    subject { described_class.new.perform(query) }
+
     before do
       connection = Core::ConnectionFactory.build(url)
       allow(Core::Registries::Connection).to receive(:[]).with(:google_api) { connection }
       stub_request(:get, "https://example.com/path/to/url/customsearch/v1?#{search_options}").
         to_return(status: status, body: body, headers: {})
+    end
+
+    context 'when there is an error' do
+      let(:body) { nil }
+      let(:status) { 500 }
+
+      specify { expect { described_class.new.perform(query) }.to raise_error(described_class::RequestError) }
     end
 
     context 'when the request is successful' do
@@ -40,8 +49,6 @@ describe Core::Repositories::Search::Google  do
             ]
           }
       end
-
-      subject { described_class.new.perform(query) }
 
       it { should be_a(Array) }
 

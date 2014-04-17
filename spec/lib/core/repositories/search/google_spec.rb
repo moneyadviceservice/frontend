c6961cd756c24c55d3d523f8d96e7785473116fe
Andrew Garner <andrew@andrewgarner.com>
2014-04-17 08:47:16 +0100

    Simplify Core::Repositories::Search::Google spec

--- spec/lib/core/repositories/search/google_spec.rb
+++ spec/lib/core/repositories/search/google_spec.rb
@@ -1,37 +1,33 @@
 require 'spec_helper'
 require 'core/repositories/search/google'
 
-describe Core::Repositories::Search::Google  do
-  describe '#perform' do
-    let(:url) { 'https://example.com/path/to/url' }
-    let(:query) { 'tools' }
-    let(:search_options) { "key=#{ENV['GOOGLE_API_KEY']}&cx=#{ENV['GOOGLE_API_CX']}&q=#{query}" }
-    let(:status) { 200 }
-    let(:body) { { 'items' => ['title' => 'title'] } }
+describe Core::Repositories::Search::Google do
+  let(:connection) { double }
 
-    before do
-      connection = Core::ConnectionFactory.build(url)
-      allow(Core::Registries::Connection).to receive(:[]).with(:google_api) { connection }
-      stub_request(:get, "https://example.com/path/to/url/customsearch/v1?#{search_options}").
-        to_return(status: status, body: body, headers: {})
-    end
+  before do
+    allow(Core::Registries::Connection).to receive(:[]).with(:google_api) { connection }
+  end
+
+  describe '#perform' do
+    subject { described_class.new.perform(double) }
 
     context 'when there is an error' do
-      let(:body) { nil }
-      let(:status) { 500 }
+      before do
+        allow(connection).to receive(:get).and_raise(Core::Connection::ClientError)
+      end
 
-      specify { expect { described_class.new.perform(query) }.to raise_error(described_class::RequestError) }
+      specify do
+        expect { subject }.to raise_error(described_class::RequestError)
+      end
     end
 
     context 'when the request is successful' do
-      let(:request_mapper) { double(map: ['item']) }
+      before do
+        allow(connection).to receive(:get) { double(body: {}) }
+      end
 
-      subject { described_class.new }
-
-      before { subject.request_mapper = request_mapper}
-
-      it 'returns an array' do
-        expect(subject.perform(query)).to be_an(Array)
+      specify do
+        expect(subject).to be_an(Array)
       end
     end
   end

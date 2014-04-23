7dcd412359116b880ff44fdd303bad7c915de8b9
Pablo Vicente <PabloVicente.Carrera@moneyadviceservice.org.uk>
2014-04-23 15:47:59 +0100

    Use google search only in production

--- config/initializers/core.rb
+++ config/initializers/core.rb
@@ -6,12 +6,14 @@ require 'core/repositories/action_plans/public_website'
 require 'core/repositories/articles/public_website'
 require 'core/repositories/categories/public_website'
 require 'core/repositories/search/google_custom_search'
+require 'core/repositories/search/content_service'
 
 require 'faraday/request/host_header'
 require 'faraday/request/x_forwarded_proto'
 
-google_api_connection     = Core::ConnectionFactory.build(ENV['GOOGLE_API_URL'])
-public_website_connection = Core::ConnectionFactory.build(ENV['MAS_PUBLIC_WEBSITE_URL'])
+content_service_connection = Core::ConnectionFactory.build(ENV['MAS_CONTENT_SERVICE_URL'])
+google_api_connection      = Core::ConnectionFactory.build(ENV['GOOGLE_API_URL'])
+public_website_connection  = Core::ConnectionFactory.build(ENV['MAS_PUBLIC_WEBSITE_URL'])
 
 public_website_connection.builder.insert_after(Faraday::Request::RequestId,
                                                Faraday::Request::HostHeader)
@@ -19,8 +21,9 @@ public_website_connection.builder.insert_after(Faraday::Request::RequestId,
 public_website_connection.builder.insert_after(Faraday::Request::RequestId,
                                                Faraday::Request::XForwardedProto)
 
-Core::Registries::Connection[:google_api]     = google_api_connection
-Core::Registries::Connection[:public_website] = public_website_connection
+Core::Registries::Connection[:google_api]      = google_api_connection
+Core::Registries::Connection[:content_service] = content_service_connection
+Core::Registries::Connection[:public_website]  = public_website_connection
 
 Core::Registries::Repository[:action_plan] =
   Core::Repositories::ActionPlans::PublicWebsite.new
@@ -31,5 +34,10 @@ Core::Registries::Repository[:article] =
 Core::Registries::Repository[:category] = Core::RepositoryCache.new(
   Core::Repositories::Categories::PublicWebsite.new)
 
-Core::Registries::Repository[:search] =
-  Core::Repositories::Search::GoogleCustomSearch.new(ENV['GOOGLE_API_KEY'], ENV['GOOGLE_API_CX_EN'], ENV['GOOGLE_API_CX_CY'])
+if Rails.env.production?
+  Core::Registries::Repository[:search] =
+    Core::Repositories::Search::GoogleCustomSearch.new(ENV['GOOGLE_API_KEY'], ENV['GOOGLE_API_CX_EN'], ENV['GOOGLE_API_CX_CY'])
+else
+  Core::Registries::Repository[:search] =
+    Core::Repositories::Search::ContentService.new
+end

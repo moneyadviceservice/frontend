95324fa0eabf86d74bb8b3b5d5993830c33980fa
Andrew Garner <andrew@andrewgarner.com>
2014-04-24 13:56:51 +0100

    Strip site title from search results

--- app/decorators/search_result_decorator.rb
+++ app/decorators/search_result_decorator.rb
@@ -1,5 +1,5 @@
 class SearchResultDecorator < Draper::Decorator
-  delegate :title, :description
+  delegate :description
 
   def to_partial_path
     'search_result'
@@ -19,4 +19,25 @@ class SearchResultDecorator < Draper::Decorator
         "/#{I18n.locale}/#{object.type.pluralize}/#{object.id}"
     end
   end
+
+  def title
+    object.title.sub(site_title_regex, '')
+  end
+
+  private
+
+  def site_title_regex
+    Regexp.new(site_title_suffixes.join('$|'))
+  end
+
+  def site_title_suffixes
+    title  = h.t('layouts.base.title')
+    length = title.length
+
+    length.times.map do |i|
+      truncated = i < length -1
+      " - #{title[0..i]}" + (truncated ? ' ...' : '')
+    end
+  end
+
 end

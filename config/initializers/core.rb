922af5694c3993b895fc24f0e68bd30cf543f125
Pablo Vicente <PabloVicente.Carrera@moneyadviceservice.org.uk>
2014-04-24 11:00:02 +0100

    Revert "Use google search only in production"
    This reverts commit 59cdb93fe743bb2e48e0b5502bb2fcfd74420070.
    
    Conflicts:
    	config/initializers/core.rb

--- config/initializers/core.rb
+++ config/initializers/core.rb
@@ -5,15 +5,13 @@ require 'core/repositories/repository_cache'
 require 'core/repositories/action_plans/public_website'
 require 'core/repositories/articles/public_website'
 require 'core/repositories/categories/public_website'
-require 'core/repositories/search/content_service'
 require 'core/repositories/search/google_custom_search_engine'
 
 require 'faraday/request/host_header'
 require 'faraday/request/x_forwarded_proto'
 
-content_service_connection = Core::ConnectionFactory.build(ENV['MAS_CONTENT_SERVICE_URL'])
-google_api_connection      = Core::ConnectionFactory.build(ENV['GOOGLE_API_URL'])
-public_website_connection  = Core::ConnectionFactory.build(ENV['MAS_PUBLIC_WEBSITE_URL'])
+google_api_connection     = Core::ConnectionFactory.build(ENV['GOOGLE_API_URL'])
+public_website_connection = Core::ConnectionFactory.build(ENV['MAS_PUBLIC_WEBSITE_URL'])
 
 public_website_connection.builder.insert_after(Faraday::Request::RequestId,
                                                Faraday::Request::HostHeader)
@@ -21,9 +19,8 @@ public_website_connection.builder.insert_after(Faraday::Request::RequestId,
 public_website_connection.builder.insert_after(Faraday::Request::RequestId,
                                                Faraday::Request::XForwardedProto)
 
-Core::Registries::Connection[:google_api]      = google_api_connection
-Core::Registries::Connection[:content_service] = content_service_connection
-Core::Registries::Connection[:public_website]  = public_website_connection
+Core::Registries::Connection[:google_api]     = google_api_connection
+Core::Registries::Connection[:public_website] = public_website_connection
 
 Core::Registries::Repository[:action_plan] =
   Core::Repositories::ActionPlans::PublicWebsite.new
@@ -34,10 +31,5 @@ Core::Registries::Repository[:article] =
 Core::Registries::Repository[:category] = Core::RepositoryCache.new(
   Core::Repositories::Categories::PublicWebsite.new)
 
-if Rails.env.production?
-  Core::Registries::Repository[:search] =
-    Core::Repositories::Search::GoogleCustomSearchEngine.new(ENV['GOOGLE_API_KEY'], ENV['GOOGLE_API_CX_EN'], ENV['GOOGLE_API_CX_CY'])
-else
-  Core::Registries::Repository[:search] =
-    Core::Repositories::Search::ContentService.new
-end
+Core::Registries::Repository[:search] =
+  Core::Repositories::Search::GoogleCustomSearchEngine.new(ENV['GOOGLE_API_KEY'], ENV['GOOGLE_API_CX_EN'], ENV['GOOGLE_API_CX_CY'])

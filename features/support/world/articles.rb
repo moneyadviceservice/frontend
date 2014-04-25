05214a6ebc97dd6dc4f758fa9354a6791b595974
Jon Gilbraith <jon.gilbraith@moneyadviceservice.org.uk>
2014-04-25 13:48:54 +0100

    Convert article viewing feature to use VCR.

--- features/support/world/articles.rb
+++ features/support/world/articles.rb
@@ -1,20 +1,22 @@
+require 'core/entities/article'
+require 'core/registries/repository'
+
 module World
   module Articles
-    def current_article_in(locale)
+    def article_id_for_locale(locale)
       case locale
-        when 'en'
-          @current_article_en ||= build(:article,
-            id:          'why-it-pays-to-save-regularly',
-            title:       'Why it pays to save regularly',
-            description: 'A regular savings habit makes your savings grow fast – getting started is quick and easy',
-            body:        'If you save regularly, you’ll quickly find that your savings add up and keep growing. Get into the habit and watch your money turn into more money.')
-        when 'cy'
-          @current_article_cy ||= build(:article,
-            id:          'pam-bod-cynilon-rheolaidd-yn-talu-ffordd',
-            title:       "Pam bod cynilo'n rheolaidd yn talu ffordd",
-            description: "Mae mynd i'r arfer o gynilo'n rheolaidd yn golygu y bydd eich cynilion yn tyfu'n gyflym – mae'n gyflym ac yn hawdd dechrau arni",
-            body:        "Os byddwch yn cynilo'n rheolaidd, buan y byddwch yn ychwanegu at eich cynilion ac y byddant yn parhau i dyfu. Ewch i'r arfer a gwyliwch wrth i'ch arian droi'n fwy o arian.")
+      when 'en'
+        'help-to-buy-scheme-everything-you-need-to-know'
+      when 'cy'
+        'cynllun-help-i-brynu-cael-gwybod-popeth-sydd-ei-angen-arnoch'
       end
+    end
+
+    def article_for_locale(locale)
+      id = article_id_for_locale(locale)
+      data = ::Core::Registries::Repository[:article].find(id)
+
+      ::Core::Article.new(id, data)
     end
   end
 end

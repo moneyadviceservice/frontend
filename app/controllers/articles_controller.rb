1132490b1b55986fe5f5f00e005cb93140e845fb
Jon Gilbraith <jon.gilbraith@moneyadviceservice.org.uk>
2014-04-28 09:23:14 +0100

    Revert "Update the article show template to the new design with related content."
    This reverts commit 259e0881767966bbd7cd9b76d60d8ca066da8da6.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,12 +2,10 @@ require 'core/interactors/article_reader'
 
 class ArticlesController < ApplicationController
   decorates_assigned :article, with: ArticleDecorator
-  decorates_assigned :related_categories, with: CategoryDecorator
 
   def show
     @article = Core::ArticleReader.new(params[:id]).call do
       not_found
     end
-    @related_categories = @article.categories
   end
 end

0d2313ed77756bb83276c5e3c73847f5b6f390f4
Steven Wilkin <stevenwilkin@gmail.com>
2014-04-29 14:05:32 +0100

    Don't duplicate ArticleReader behaviour in test setup

--- features/support/world/articles.rb
+++ features/support/world/articles.rb
@@ -1,5 +1,5 @@
 require 'core/entities/article'
-require 'core/interactors/category_reader'
+require 'core/interactors/article_reader'
 require 'core/registries/repository'
 
 module World
@@ -24,24 +24,12 @@ module World
 
     def article_for_locale(locale)
       id = article_id_for_locale(locale)
-      data = I18n.with_locale(locale) { ::Core::Registries::Repository[:article].find(id) }
-
-      ::Core::Article.new(id, data).tap do |article|
-        article.categories = article.categories.map do |category_id|
-          ::Core::CategoryReader.new(category_id).call
-        end
-      end
+      retrieve_article_for_locale(id, locale)
     end
 
     def alternate_article_for_locale(locale)
       id = alternate_article_id_for_locale(locale)
-      data = I18n.with_locale(locale) { ::Core::Registries::Repository[:article].find(id) }
-
-      ::Core::Article.new(id, data).tap do |article|
-        article.categories = article.categories.map do |category_id|
-          ::Core::CategoryReader.new(category_id).call
-        end
-      end
+      retrieve_article_for_locale(id, locale)
     end
 
     def category_containing_articles(locale)
@@ -51,10 +39,18 @@ module World
       build(:category_hash,
         id: 'affordable-housing-schemes',
         contents: [
-          { 'type' => 'guide', 'id' => article.id, 'title' => article.title },
-          { 'type' => 'guide', 'id' => alternate.id, 'title' => alternate.title }
+          build(:article_hash, id: article.id, title: article.title),
+          build(:article_hash, id: alternate.id, title: alternate.title),
         ]
       )
+    end
+
+    private
+
+    def retrieve_article_for_locale(id, locale)
+      I18n.with_locale(locale) do
+        Core::ArticleReader.new(id).call
+      end
     end
   end
 end

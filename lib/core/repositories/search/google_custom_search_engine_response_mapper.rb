124f9da97ff0677025e671324d88cc2444982610
Pablo Vicente <PabloVicente.Carrera@moneyadviceservice.org.uk>
2014-04-29 15:15:00 +0100

    Modify GoogleCustomSearchResponseMapper to handle a paginated response.
    Refactor the spec a little too.

--- lib/core/repositories/search/google_custom_search_engine_response_mapper.rb
+++ lib/core/repositories/search/google_custom_search_engine_response_mapper.rb
@@ -3,9 +3,26 @@ module Core::Repositories
     class GoogleCustomSearchEngineResponseMapper
       SECOND_TO_LAST = -2
 
-      def map(response)
-        items = response.body.fetch('items', [])
-        items.map do |item|
+      def initialize(response)
+        @body = response.body
+      end
+
+      def mapped_response
+        {
+          total_results: total_results,
+          page: page,
+          per_page: per_page,
+          items: items
+        }
+      end
+
+      private
+
+      attr_reader :body
+
+      def items
+        raw_items = body.fetch('items', [])
+        raw_items.map do |item|
           link_tokens = item['link'].split('/')
           {
             id:          link_tokens.last,
@@ -18,7 +35,25 @@ module Core::Repositories
         end
       end
 
-      private
+      def paging_metadata
+        body['queries']['request'].first
+      end
+
+      def total_results
+        paging_metadata['totalResults'].to_i
+      end
+
+      def per_page
+        paging_metadata['count'].to_i
+      end
+
+      def offset
+        paging_metadata['startIndex'].to_i - 1
+      end
+
+      def page
+        (offset / per_page) + 1
+      end
 
       def extract_description(item)
         return '' unless item.key?('pagemap')
--- spec/lib/core/repositories/search/google_custom_search_engine_response_mapper_spec.rb
+++ spec/lib/core/repositories/search/google_custom_search_engine_response_mapper_spec.rb
@@ -4,63 +4,107 @@ require 'core/repositories/search/google_custom_search_engine_response_mapper'
 describe Core::Repositories::Search::GoogleCustomSearchEngineResponseMapper do
 
   describe '#map' do
-    let(:id) { 'action-plan-id' }
-    let(:type) { 'action_plan' }
-    let(:title) { 'The action plan' }
-    let(:description) { 'Action plan description' }
-    let(:link) { "https://www.moneyadviceservice.org.uk/en/#{type.pluralize}/#{id}" }
-    let(:item) do
+    let(:total_results) { 30 }
+    let(:count) { 10 }
+    let(:start_index) { 1 }
+
+    let(:body) do
       {
-       "link"=>link,
-       "title"=>title,
-       "pagemap"=>{
-          "metatags"=>[
-             {
-                "csrf-param"=>"authenticity_token",
-                "og:description"=>description
-             }
+        'queries' => {
+          'request' => [
+            'totalResults' => total_results.to_s,
+            'count' => count.to_s,
+            'startIndex' => start_index.to_s
           ]
         }
       }
     end
-    let(:response) { double(body: { 'items' => [item] })}
 
-    subject(:mapped_response) { described_class.new.map(response) }
+    let(:response) { double(body: body)}
+    subject { described_class.new(response).mapped_response }
 
-    it 'maps the id correctly' do
-      expect(mapped_response.first[:id]).to eq(id)
-    end
+    describe 'the body attributes' do
+      it 'maps the total results correctly' do
+        expect(subject[:total_results]).to eq(total_results)
+      end
 
-    it 'maps the type correctly' do
-      expect(mapped_response.first[:type]).to eq(type)
-    end
+      it 'maps the per page attribute correctly' do
+        expect(subject[:per_page]).to eq(count)
+      end
 
-    it 'maps the description correctly' do
-      expect(mapped_response.first[:description]).to eq(description)
-    end
+      context 'when on the first page' do
+        it 'calculates the page number correctly' do
+          expect(subject[:page]).to eq 1
+        end
+      end
 
-    context 'when response has no items' do
-      let(:response) { double(body: { "kind"=>"customsearch#search" }) }
+      context 'when on the second page' do
+        let(:start_index) { 11 }
 
-      it 'returns an emty array' do
-        expect(mapped_response).to be_a(Array)
-        expect(mapped_response).to be_empty
+        it 'calculates the page number correctly' do
+          expect(subject[:page]).to eq 2
+        end
       end
     end
 
-    context "when item doesn't have pagemap" do
-      let(:item) { { "link"=>link, "title"=>title } }
-
-      it 'maps the description to empty string' do
-        expect(mapped_response.first[:description]).to be_empty
+    describe 'items' do
+      let(:id) { 'action-plan-id' }
+      let(:type) { 'action_plan' }
+      let(:title) { 'The action plan' }
+      let(:description) { 'Action plan description' }
+      let(:link) { "https://www.moneyadviceservice.org.uk/en/#{type.pluralize}/#{id}" }
+      let(:item) do
+        {
+         'link' => link,
+         'title' => title,
+         'pagemap' => {
+            'metatags' => [
+               {
+                  'csrf-param' => 'authenticity_token',
+                  'og:description' => description
+               }
+            ]
+          }
+        }
       end
-    end
 
-    context "when item doesn't have metatags" do
-      let(:item) { { "link"=>link, "title"=>title, "pagemap"=>{} } }
+      before { body['items'] = [item] }
 
-      it 'maps the description to empty string' do
-        expect(mapped_response.first[:description]).to be_empty
+      it 'maps the id correctly' do
+        expect(subject[:items].first[:id]).to eq(id)
+      end
+
+      it 'maps the type correctly' do
+        expect(subject[:items].first[:type]).to eq(type)
+      end
+
+      it 'maps the description correctly' do
+        expect(subject[:items].first[:description]).to eq(description)
+      end
+
+      context 'when a response has none' do
+        before { body['items'] = [] }
+
+        it 'returns an empty array' do
+          expect(subject[:items]).to be_a(Array)
+          expect(subject[:items]).to be_empty
+        end
+      end
+
+      context "when it doesn't have pagemap" do
+        let(:item) { { 'link' => link, 'title' => title } }
+
+        it 'maps the description to empty string' do
+          expect(subject[:items].first[:description]).to be_empty
+        end
+      end
+
+      context "when it doesn't have metatags" do
+        let(:item) { { "link"=>link, "title"=>title, "pagemap"=>{} } }
+
+        it 'maps the description to empty string' do
+          expect(subject[:items].first[:description]).to be_empty
+        end
       end
     end
   end

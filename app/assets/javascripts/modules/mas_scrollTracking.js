65faa03f97be5757920de1b39f3e3211f407b214
Alistair Duggin <alistair.duggin@btinternet.com>
2014-04-29 16:41:58 +0100

    Add query dependency to scrollTracking

--- app/assets/javascripts/modules/mas_scrollTracking.js
+++ app/assets/javascripts/modules/mas_scrollTracking.js
@@ -6,7 +6,7 @@
  - we could extend this to use pubSub so remove the possible depandancy
  */
 
-define(['common', 'waypoints'], function (MAS) {
+define(['jquery', 'common', 'waypoints'], function ($, MAS) {
 
   'use strict';
 

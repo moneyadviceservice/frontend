db6f1fcc4dc456112f4bee0cc5724bb5fc0af4d8
Jon Gilbraith <jon.gilbraith@moneyadviceservice.org.uk>
2014-05-02 13:51:00 +0100

    Add a bunch of helper methods to SearchResultCollection.

--- lib/core/entities/search_result_collection.rb
+++ lib/core/entities/search_result_collection.rb
@@ -14,5 +14,25 @@ module Core
     def any?
       items.present?
     end
+
+    def first_page?
+      page == 1
+    end
+
+    def last_page?
+      page == number_of_pages
+    end
+
+    def previous_page
+      first_page? ? nil : page - 1
+    end
+
+    def next_page
+      last_page? ? nil : page + 1
+    end
+
+    def number_of_pages
+      total_results <= per_page ? 1 : (((total_results - 1) / per_page) + 1)
+    end
   end
 end
--- spec/lib/core/entities/search_result_collection_spec.rb
+++ spec/lib/core/entities/search_result_collection_spec.rb
@@ -38,5 +38,124 @@ module Core
         it { should be_false }
       end
     end
+
+    context 'with 5 pages of 10 results' do
+      let(:collection) { described_class.new(double) }
+
+      before do
+        collection.per_page = 10
+        collection.total_results = 50
+      end
+
+      describe '#first_page?' do
+        subject { collection.first_page? }
+
+        context 'when the current page is 1' do
+          before { collection.page = 1 }
+          it { should be_true }
+        end
+
+        context 'when the current page is 3' do
+          before { collection.page = 3 }
+          it { should be_false }
+        end
+
+        context 'when the current page is 5' do
+          before { collection.page = 5 }
+          it { should be_false }
+        end
+      end
+
+      describe '#last_page?' do
+        subject { collection.last_page? }
+
+        context 'when the current page is 1' do
+          before { collection.page = 1 }
+          it { should be_false }
+        end
+
+        context 'when the current page is 3' do
+          before { collection.page = 3 }
+          it { should be_false }
+        end
+
+        context 'when the current page is 5' do
+          before { collection.page = 5 }
+          it { should be_true }
+        end
+      end
+
+      describe '#previous_page' do
+        subject { collection.previous_page }
+
+        context 'when the current page is 1' do
+          before { collection.page = 1 }
+          it { should be_nil }
+        end
+
+        context 'when the current page is 3' do
+          before { collection.page = 3 }
+          it { should eql 2 }
+        end
+
+        context 'when the current page is 5' do
+          before { collection.page = 5 }
+          it { should eql 4 }
+        end
+      end
+
+      describe '#next_page' do
+        subject { collection.next_page }
+
+        context 'when the current page is 1' do
+          before { collection.page = 1 }
+          it { should eql 2 }
+        end
+
+        context 'when the current page is 3' do
+          before { collection.page = 3 }
+          it { should eql 4 }
+        end
+
+        context 'when the current page is 5' do
+          before { collection.page = 5 }
+          it { should be_nil }
+        end
+      end
+    end
+
+    describe '#number_of_pages' do
+      let(:collection) { described_class.new(double) }
+      subject { collection.number_of_pages }
+
+      context 'with 10 results per page' do
+        before { collection.per_page = 10 }
+
+        context 'and a total of 20 results' do
+          before { collection.total_results = 20 }
+          it { should eql 2 }
+        end
+
+        context 'and a total of 15 results' do
+          before { collection.total_results = 15 }
+          it { should eql 2 }
+        end
+
+        context 'and a total of 10 results' do
+          before { collection.total_results = 10 }
+          it { should eql 1 }
+        end
+
+        context 'and a total of 5 results' do
+          before { collection.total_results = 5 }
+          it { should eql 1 }
+        end
+
+        context 'and zero results' do
+          before { collection.total_results = 0 }
+          it { should eql 1 }
+        end
+      end
+    end
   end
 end

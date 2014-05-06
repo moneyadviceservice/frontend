e51b044c2f807ee27b113f79131518568838af10
Ben Barnett <ben.barnett@moneyadviceservice.org.uk>
2014-05-06 10:44:37 +0100

    updating spec helper

--- spec/javascripts/spec_helper.js
+++ spec/javascripts/spec_helper.js
@@ -5,27 +5,37 @@
 var expect = chai.expect;
 
 // Any properties that require Ruby @ runtime
-var MAS = window.MAS || {};
-MAS.bootstrap = {
-  env: 'production',
-  timestamp: new Date().getTime(),
-  I18nLocale: 'en'
-};
 
-MAS.supports = (function(w,d){
-  var S = {};
-  function supportTest(prop, context){
-    try {
-      return prop in context && context[prop] !== null;
-    } catch (e) {
-      return false;
-    }
+var require = {
+  config: {
+    globals: (function() {
+      var MAS = {};
+
+      MAS.bootstrap = {
+        env: 'production',
+        timestamp: new Date().getTime(),
+        I18nLocale: 'en'
+      };
+
+      MAS.supports = (function(w,d){
+        var S = {};
+        function supportTest(prop, context){
+          try {
+            return prop in context && context[prop] !== null;
+          } catch (e) {
+            return false;
+          }
+        }
+
+        S.fonts = false; // Required for webfont loader to work, default false, set true in view/base
+        S.js = ( supportTest('querySelector',d) && supportTest('localStorage',w) && supportTest('addEventListener',w) ) ? 'advanced' : 'basic';
+        S.touch = ( supportTest('ontouchstart', w) || supportTest('onmsgesturechange',w) );
+        S.localstorage = supportTest('localStorage', w);
+
+        return S;
+      })(window, document);
+
+      return MAS;
+    }());
   }
-
-  S.fonts = false; // Required for webfont loader to work, default false, set true in view/base
-  S.js = ( supportTest('querySelector',d) && supportTest('localStorage',w) && supportTest('addEventListener',w) ) ? 'advanced' : 'basic';
-  S.touch = ( supportTest('ontouchstart', w) || supportTest('onmsgesturechange',w) );
-  S.localstorage = supportTest('localStorage', w);
-
-  return S;
-})(window, document);
+}

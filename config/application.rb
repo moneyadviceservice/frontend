c4321fd090a333758cb23cfc13f5b912ef98170e
Andrew Garner <andrew@andrewgarner.com>
2014-05-07 07:10:48 +0100

    Move icon sprite to frontend-assets

--- config/application.rb
+++ config/application.rb
@@ -30,7 +30,7 @@ module Frontend
                                   styleguide/styleguide_responsive.css)
 
     # Include fonts when precompiling assets.
-    config.assets.precompile << /\.(?:svg|eot|woff|ttf)$/
+    config.assets.precompile << /\.(?:png|svg|eot|woff|ttf)$/
 
     # Configure additional application JS assets to precompile.
     config.assets.precompile += %w(html_inspector.js

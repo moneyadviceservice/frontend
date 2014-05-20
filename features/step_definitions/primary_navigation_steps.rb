abe4666235fe00210b7731f6f65bce2ccb58a52f
Steven Wilkin <stevenwilkin@gmail.com>
2014-05-20 16:46:06 +0100

    Cover scenario of article existing in 2 categories within the same parent category

--- features/step_definitions/primary_navigation_steps.rb
+++ features/step_definitions/primary_navigation_steps.rb
@@ -7,6 +7,15 @@ Given 'I am on an article that lives in a single category' do
   browse_to_article article
 end
 
+Given 'I am on an article that lives in 2 categories in the same parent' do
+  article, categories = article_in_two_categories
+
+  populate_article_repository_with article
+  populate_category_repository_with *categories
+
+  browse_to_article article
+end
+
 Then 'I should see the primary navigation with the parent category expanded' do
   category_nav_categories = article_page.category_nav.categories.map { |c| c.title.text }
 
@@ -22,9 +31,13 @@ Then 'I should see the primary navigation with the parent category expanded' do
   expect(selected_categories.first.title.text).to eq(all_categories.first['title'])
 end
 
-And 'the relevant child category selected' do
-  selected_child_categories = article_page.category_nav.selected_categories.first.selected_categories
+And /the relevant child categor(y|ies) selected/ do |plural|
+  selected_child_categories = []
+  article_page.category_nav.selected_categories.each do |category|
+    category.selected_categories.each do |child_category|
+      selected_child_categories << child_category.text
+    end
+  end
 
-  expect(selected_child_categories.count).to eq(1)
-  expect(selected_child_categories.first.text).to eq(all_categories.first['contents'].first['title'])
+  expect(selected_child_categories.sort).to eq(current_article['categories'].sort)
 end

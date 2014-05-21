253a99f13d12025ce05c97ca991b9aaac687cad5
Andrew Garner <andrew@andrewgarner.com>
2014-05-21 08:00:10 +0100

    Use Regexp.union to process search result titles

--- app/decorators/search_result_decorator.rb
+++ app/decorators/search_result_decorator.rb
@@ -10,22 +10,20 @@ class SearchResultDecorator < Draper::Decorator
   end
 
   def title
-    object.title.sub(site_title_regex, '')
+    object.title.sub(Regexp.union(title_suffix_regexps), '')
   end
 
   private
 
-  def site_title_regex
-    Regexp.new(site_title_suffixes.join('$|'))
-  end
-
-  def site_title_suffixes
+  def title_suffix_regexps
     title  = h.t('layouts.base.title')
     length = title.length
 
     length.times.map do |i|
-      truncated = i < length -1
-      " - #{title[0..i]}" + (truncated ? ' ...' : '')
+      is_truncated = i < (length - 1)
+
+      Regexp.new(' - %<suffix>s%<ellipsis>s$' % { suffix:   title[0..i],
+                                                  ellipsis: is_truncated ? ' ...' : nil })
     end
   end
 

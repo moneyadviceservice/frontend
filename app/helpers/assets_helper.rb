f96712f1f9e9a29c5436ab0c5d18b52bd0ff6064
Jakub Pawlowicz <jakub.pawlowicz@moneyadviceservice.org.uk>
2014-05-23 09:50:11 +0100

    Adds caching to optimizely tag helper.

--- app/helpers/assets_helper.rb
+++ app/helpers/assets_helper.rb
@@ -1,7 +1,11 @@
 require 'assets/optimizely_script'
 
 module AssetsHelper
+  CACHE_EXPIRY = 150.seconds
+
   def optimizely_include_tag
-    Assets::OptimizelyScript.new.to_s.html_safe
+    Rails.cache.fetch('optimizely_script', expires_in: CACHE_EXPIRY, race_condition_ttl: 100) do
+      Assets::OptimizelyScript.new.to_s.html_safe
+    end
   end
 end

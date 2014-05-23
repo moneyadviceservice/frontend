df3955b4d07a3073f09a33c621fc8ba0a5e45e03
Steven Wilkin <stevenwilkin@gmail.com>
2014-05-23 19:26:04 +0100

    Use Navigation concern to specify currently active categories
    The partial doesn't need to know about the current article now

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,11 +3,17 @@ require 'core/interactors/article_reader'
 class ArticlesController < ApplicationController
   decorates_assigned :article, with: ContentItemDecorator
 
+  include Navigation
+
   def show
     @article = Core::ArticleReader.new(params[:id]).call do
       not_found
     end
 
     @breadcrumbs = BreadcrumbTrail.build(@article, category_tree)
+
+    (@article.categories.map(&:id) + @article.parent_category_ids).each do |category|
+      active_category category
+    end
   end
 end

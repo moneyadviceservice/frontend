f72baec98be44a7acd28c531d3292dccbbf0eebc
Jon Gilbraith <jon.gilbraith@moneyadviceservice.org.uk>
2014-05-29 10:32:42 +0100

    Blacklist the partner page and all linked articles.

--- spec/requests/black_listed_resources_spec.rb
+++ spec/requests/black_listed_resources_spec.rb
@@ -1,9 +1,30 @@
 RSpec.describe 'Request blacklisted resource', :type => :request do
   %W(about-our-debt-work am-ein-gwaith-dyled
-     debt-publications cyhoeddiadau-ar-ddyledion).each do |article|
+     debt-publications cyhoeddiadau-ar-ddyledion
+     partners-overview-parhub
+     partner-reg-parhub
+     syndicating-tools-parhub
+     video-syndication-parhub
+     toolkits-parhub pecynnau-cymorth-cyngor-ariannol
+     linking-parhub
+     examples-parhub
+     licence-agreement-parhub).each do |article|
     context "request article #{article}" do
       it 'returns a 501 response' do
         get("/en/articles/#{article}")
+
+        expect(response.status).to eq(501)
+      end
+    end
+  end
+
+  %W(partners
+     partners-uc-banks
+     partners-uc-landlords
+     resources-for-professionals-working-with-young-people-and-parents).each do |category|
+    context "request category #{category}" do
+      it 'returns a 501 response' do
+        get("/en/categories/#{category}")
 
         expect(response.status).to eq(501)
       end

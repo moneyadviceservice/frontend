56941a467bcd8a72ec32007c4ee7f4c4e0978211
Andrew Garner <andrew@andrewgarner.com>
2014-06-04 17:59:38 +0100

    Configure VCR to ignore Code Climate

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -29,6 +29,7 @@ PageValidations::HTMLValidation.ignored_tag_errors       = ['main']
 VCR.configure do |c|
   c.cassette_library_dir = 'spec/cassettes'
   c.hook_into :webmock
+  c.ignore_hosts 'codeclimate.com'
 end
 
 WebMock.disable_net_connect!(allow: 'codeclimate.com')

61168927252b1b994cd80920ff2710766d1763ec
Andrew Garner <andrew@andrewgarner.com>
2014-06-04 10:19:43 +0100

    Use fixtures for article acceptance tests

--- features/step_definitions/article_breadcrumbs_steps.rb
+++ features/step_definitions/article_breadcrumbs_steps.rb
@@ -3,20 +3,15 @@ Given(/^I read an article belonging to a single category$/) do
 end
 
 Given(/^I read an article belonging to multiple categories$/) do
-  locale = 'en'
-  article_page.load(locale: locale, id: article_id_for_locale(locale))
+  article_page.load(locale: 'en', id: 'changes-to-child-benefit-from-2013')
 end
 
 Given(/^I read an orphaned article$/) do
-  populate_article_repository_with(single_article)
-  article_page.load(locale: 'en', id: article_id_for_locale('en'))
+  article_page.load(locale: 'en', id: 'if-your-baby-is-stillborn')
 end
 
 Then(/^I can see breadcrumbs for that category and it's parent$/) do
-  current_article = article_for_locale('en')
-
-  expect(article_page.breadcrumbs).to have_content(I18n.t('layouts.home'))
-  expect(article_page.breadcrumbs).to have_content(current_article.categories.first.title)
+  expect(article_page.breadcrumbs).to have_content('Home > Money topics > Births, deaths and family')
 end
 
 Then(/^I can see that it appears in those categories$/) do
--- features/step_definitions/article_steps.rb
+++ features/step_definitions/article_steps.rb
@@ -1,6 +1,5 @@
 When(/^I view (?:a|an|the) article in (.*)$/) do |language|
-  locale = language_to_locale(language)
-  article_page.load(locale: locale, id: article_id_for_locale(locale))
+  browse_to_article article(language)
 end
 
 When(/^I translate the article into (.*)$/) do |language|
@@ -14,23 +13,23 @@ When(/^I translate the article into (.*)$/) do |language|
 end
 
 Then(/^I should see the article in (.*)$/) do |language|
-  current_article = article_for_locale(language_to_locale(language))
+  article = article(language)
 
-  expect(article_page.title).to eq("#{current_article.title} - #{I18n.t('layouts.base.title')}")
-  expect(article_page.description[:content]).to include(current_article.description)
-  expect(article_page.heading).to have_content(current_article.title)
+  intro_test           = Nokogiri::HTML(article.body).at('p.intro').inner_html
+  sample_of_intro_text = strip_tags(intro_test).split(/\./)[0]
 
-  # Use the first sentence in the body p.intro tag as the intro string to search for
-  sample_of_intro_text = strip_tags(Nokogiri::HTML(current_article.body).at('p.intro').inner_html).split(/\./)[0]
+  body_text           = Nokogiri::HTML(article.body).at('p:not([class])').inner_html
+  sample_of_body_text = strip_tags(body_text).split(/\./)[0]
+
+  expect(article_page.title).to eq("#{article.title} - #{I18n.t('layouts.base.title')}")
+  expect(article_page.description[:content]).to include(article.description)
+  expect(article_page.heading).to have_content(article.title)
   expect(article_page.intro).to have_content(strip_tags(sample_of_intro_text))
-
-  # Use the first sentence in the first p tag with no class in the body main content string to search for
-  sample_of_body_text = strip_tags(Nokogiri::HTML(current_article.body).at('p:not([class])').inner_html).split(/\./)[0]
   expect(article_page.main_content).to have_content(strip_tags(sample_of_body_text))
 end
 
 Then(/^I should not see the article title in the related content in (.*)$/) do |language|
-  article = article_for_locale(language_to_locale(language))
+  article           = article_for_locale(language_to_locale(language))
   decorated_article = ArticleDecorator.decorate(article)
 
   decorated_article.related_categories.each do |category, contents|
@@ -42,18 +41,22 @@ Then(/^I should not see the article title in the related content in (.*)$/) do |
 end
 
 Then(/^the article should have a canonical tag for that language version$/) do
-  expected_href = article_url(id: article_for_locale(current_locale).id, locale: current_locale)
+  expected_href = article_url(id: current_article.id, locale: current_article.locale)
 
   expect { article_page.canonical_tag[:href] }.to become(expected_href)
 end
 
 Then(/^the article page should have alternate tags for the supported locales$/) do
-  expected_hreflangs = ["en-GB", "cy-GB"]
-  expected_hrefs = I18n.available_locales.map do |locale|
-    article_url(id: article_for_locale(locale.to_s).id, locale: locale, protocol: 'https', host: 'www.moneyadviceservice.org.uk')
+  expected_hreflangs = %w(en-GB cy-GB)
+  expected_hrefs     = I18n.available_locales.map do |locale|
+    article_url(id:       article(locale).id,
+                locale:   locale,
+                protocol: 'https',
+                host:     'www.moneyadviceservice.org.uk')
   end
 
   expect(article_page.alternate_tags.size).to eq(expected_hreflangs.size)
+
   article_page.alternate_tags.each do |alternate_tag|
     expect(expected_hreflangs).to include(alternate_tag[:hreflang])
     expect(expected_hrefs).to include(alternate_tag[:href])

a18581ff64bbbec0c7cf3325b29d6dc454eb6052
Andrew Garner <andrew@andrewgarner.com>
2014-06-05 12:19:04 +0100

    Remove obsolete require for static pages

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,8 +1,6 @@
 require 'core/interactors/static_page_reader'
-require 'core/interactors/category_parents_reader'
 
 class StaticPagesController < ApplicationController
-
   decorates_assigned :static_page, with: ContentItemDecorator
 
   def show

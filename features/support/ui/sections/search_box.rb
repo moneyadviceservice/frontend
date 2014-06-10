143a524a32b4ee31ed13cf9b665292b4db071af1
alexwllms <alex.williams@moneyadviceservice.org.uk>
2014-06-10 11:42:41 +0100

    Add conditional statement to fix broken tests

--- features/support/ui/sections/search_box.rb
+++ features/support/ui/sections/search_box.rb
@@ -2,7 +2,7 @@ require_relative '../section'
 
 module UI::Sections
   class SearchBox < UI::Section
-    element :input, 'input.search-box__input'
-    element :submit, 'button.search-box__submit'
+    element :input, Feature.active?(:new_header) ? '.search__input' : '.search-box__input'
+    element :submit, Feature.active?(:new_header) ? '.search__submit' : '.search-box__submit'
   end
 end

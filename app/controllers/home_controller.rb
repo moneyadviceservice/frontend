ed18cade92d28ad38380c922465c493d01a06ead
Andrew Garner <andrew@andrewgarner.com>
2014-06-11 18:27:40 +0100

    Remove header and directory release toggles
    Ready for IA release

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,9 +4,7 @@ class HomeController < ApplicationController
   decorates_assigned :directory_categories, with: CategoryDecorator
 
   def show
-    if Feature.active?(:dynamic_directory)
-      @directory_categories = Core::CategoryTreeReader.new.call
-    end
+
   end
 
   def display_menu_button_in_header?
--- features/support/ui/sections/search_box.rb
+++ features/support/ui/sections/search_box.rb
@@ -2,7 +2,7 @@ require_relative '../section'
 
 module UI::Sections
   class SearchBox < UI::Section
-    element :input, Feature.active?(:new_header) ? '.search__input' : '.search-box__input'
-    element :submit, Feature.active?(:new_header) ? '.search__submit' : '.search-box__submit'
+    element :input, '.search__input'
+    element :submit, '.search__submit'
   end
 end

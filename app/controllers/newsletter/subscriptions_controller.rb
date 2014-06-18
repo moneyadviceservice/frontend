9775894c861a62f822a6d388b24c53fbb8ac1963
Jiannis Georgiadis <jiannis.georgiadis@moneyadviceservice.org.uk>
2014-06-18 17:43:01 +0100

    Add newsletter subscriptions controller and route.

--- app/controllers/newsletter/subscriptions_controller.rb
+++ app/controllers/newsletter/subscriptions_controller.rb
@@ -1,7 +1,31 @@
+require 'core/interactors/newsletter/subscriber'
+
 module Newsletter
   class SubscriptionsController < ApplicationController
+    decorates_assigned :subscription, with: SubscriptionDecorator
+
     def create
-      render :show
+      @subscription = Core::Newsletter::Subscriber.new(subscription_params).call
+
+      respond_to do |format|
+        format.js { render :show }
+
+        format.html do
+          if subscription.success?
+            flash[:info] = subscription.success_message
+          else
+            flash[:error]  = subscription.error_message
+          end
+
+          redirect_to :back
+        end
+      end
+    end
+
+    private
+
+    def subscription_params
+      params.require(:subscription).permit(:email)
     end
   end
 end
---
+++ spec/controllers/newsletter/subscriptions_controller_spec.rb
@@ -0,0 +1,90 @@
+require 'core/interactors/newsletter/subscriber'
+require 'core/entities/newsletter/subscription'
+
+module Newsletter
+  RSpec.describe SubscriptionsController, :type => :controller do
+    let(:subscription) { instance_double(Core::Newsletter::Subscription, success: double, message: '') }
+    let(:subscriber) { instance_double(Core::Newsletter::Subscriber, email: email) }
+
+    before do
+      allow_any_instance_of(Core::Newsletter::Subscriber).to receive(:call) { subscription }
+      allow(subscriber).to receive(:call) { subscription }
+    end
+
+    describe 'XHR POST create' do
+      context 'with valid email' do
+        let(:email) { { email: 'clark.kent@example.com' } }
+
+        it 'instantiates a subscriber' do
+          expect(Core::Newsletter::Subscriber).to receive(:new).with(email) { subscriber }
+
+          xhr :post, :create, locale: I18n.locale, subscription: email
+        end
+
+        it 'assigns subscription to an instance variable' do
+          xhr :post, :create, locale: I18n.locale, subscription: email
+
+          expect(assigns(:subscription)).to eq(subscription)
+        end
+
+        it 'defines subscription method to access the decorated instance variable' do
+          xhr :post, :create, locale: I18n.locale, subscription: email
+
+          expect(subject.subscription).to be_decorated_with SubscriptionDecorator
+        end
+
+        it 'is successful' do
+          xhr :post, :create, locale: I18n.locale, subscription: email
+
+          expect(response).to be_ok
+        end
+
+        it 'renders the right template' do
+          xhr :post, :create, locale: I18n.locale, subscription: email
+
+          expect(response).to render_template('show')
+        end
+      end
+    end
+
+    describe 'POST create' do
+      before { request.env['HTTP_REFERER'] = 'example.com' }
+
+      context 'with valid email' do
+        before { allow(subscription).to receive(:success?) { true } }
+
+        let(:email) { { email: 'clark.kent@example.com' } }
+
+        it 'adds a flash info message' do
+          post :create, locale: I18n.locale, subscription: email
+
+          expect(request.flash[:info]).to include I18n.t('newsletter.subscription.success')
+        end
+
+        it 'redirects back' do
+          post :create, locale: I18n.locale, subscription: email
+
+          expect(response).to redirect_to('example.com')
+        end
+      end
+
+      context 'with invalid email' do
+        before { allow(subscription).to receive(:success?) { false } }
+
+        let(:email) { { email: 'clark.kent@dailyplanet' } }
+
+        it 'adds a flash error message' do
+          post :create, locale: I18n.locale, subscription: email
+
+          expect(request.flash[:error]).to include I18n.t('newsletter.subscription.error')
+        end
+
+        it 'redirects back' do
+          post :create, locale: I18n.locale, subscription: email
+
+          expect(response).to redirect_to('example.com')
+        end
+      end
+    end
+  end
+end

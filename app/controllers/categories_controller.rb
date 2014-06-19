3e50e90f21a114759b22944b6925efd5ed5dfb93
Andrew Garner <andrew@andrewgarner.com>
2014-06-19 09:32:27 +0100

    Set active categories when viewing a category

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -3,11 +3,16 @@ require 'core/interactors/category_reader'
 class CategoriesController < ApplicationController
   decorates_assigned :category, with: CategoryDecorator
 
+  include Navigation
+
   def show
     @category = Core::CategoryReader.new(params[:id]).call do
       not_found
     end
 
     @breadcrumbs = BreadcrumbTrail.build(@category, category_tree)
+
+    active_category @category.id
+    active_category @category.parent_id if @category.child?
   end
 end

085b835351fb3d6192c4fd8bf8380cf82bc17217
Pablo Vicente <PabloVicente.Carrera@moneyadviceservice.org.uk>
2014-06-24 09:51:02 +0100

    NewsArticleReader retrieves information from repository

--- lib/core/interactors/news_article_reader.rb
+++ lib/core/interactors/news_article_reader.rb
@@ -1,3 +1,5 @@
+require 'core/entities/news_article'
+
 module Core
   class NewsArticleReader
     attr_accessor :id
@@ -12,6 +14,14 @@ module Core
     end
 
     def call(&block)
+      data         = Registries::Repository[:news_article].find(id)
+      news_article = NewsArticle.new(id, data)
+
+      if news_article.valid?
+        news_article
+      else
+        block.call if block_given?
+      end
     end
   end
 end
---
+++ spec/lib/core/interactors/news_article_reader_spec.rb
@@ -0,0 +1,91 @@
+require_relative 'shared_examples/optional_failure_block'
+
+require 'core/interactors/news_article_reader'
+require 'core/entities/news_article'
+
+module Core
+  RSpec.describe NewsArticleReader do
+    subject { described_class.new(id) }
+
+    let(:id) { 'the-news-article' }
+
+    describe '.call' do
+      before do
+        allow(Registries::Repository).to receive(:[]).with(:news_article) do
+          double(find: data)
+        end
+      end
+
+      context 'when the repository returns no data' do
+        let(:data) { nil }
+
+        it_has_behavior 'optional failure block'
+      end
+
+      context 'when the repository returns some data' do
+        let(:title) { 'The article' }
+        let(:data) { { title: title } }
+
+        context 'when a block is given' do
+          let(:probe) { lambda {} }
+
+          it 'calls the block' do
+            expect(probe).to receive(:call)
+
+            subject.call(&probe)
+          end
+        end
+
+        context 'when no block is given' do
+          it 'returns nil' do
+            expect(subject.call).to be_nil
+          end
+        end
+      end
+
+      context 'when the repository returns data' do
+        let(:title) { 'The news article' }
+        let(:description) { 'The description' }
+        let(:body) { '<h1>The news article</h1><p>Lorem ipsum dolor sit amet</p>'}
+
+        let(:data) do
+          { title: title, description: description, body: body }
+        end
+
+        it "maps the news article's 'id' to the repositories 'id' value" do
+          expect(NewsArticle).to receive(:new).with(id, kind_of(Hash)).and_call_original
+
+          subject.call
+        end
+
+        %w(title description body).each do |attribute|
+          it "maps the article's '#{attribute}'' to the repositories '#{attribute}' value" do
+            expect(NewsArticle).to receive(:new) do |_, attributes|
+              expect(attributes[attribute.to_sym].to eq(send(attribute)))
+            end.and_call_original
+
+            subject.call
+          end
+        end
+
+        context 'when the News Article entity is valid' do
+          before do
+            expect_any_instance_of(NewsArticle).to receive(:valid?).and_return(true)
+          end
+
+          it 'returns a News Article' do
+            expect(subject.call).to be_a(NewsArticle)
+          end
+        end
+
+        context 'when the News Article entity is invalid' do
+          before do
+            expect_any_instance_of(NewsArticle).to receive(:valid?).and_return(false)
+          end
+
+          it_has_behavior 'optional failure block'
+        end
+      end
+    end
+  end
+end

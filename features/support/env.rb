5f654706051ee3d17baea795876845e203d30414
Pablo Vicente <PabloVicente.Carrera@moneyadviceservice.org.uk>
2014-06-25 10:57:58 +0100

    Add vcr support to news article

--- features/support/env.rb
+++ features/support/env.rb
@@ -7,17 +7,19 @@ require 'core/repositories/vcr'
 
 I18n.available_locales = [:en, :cy]
 
-action_plan_repository = Core::Registries::Repository[:action_plan]
-article_repository     = Core::Registries::Repository[:article]
-category_repository    = Core::Registries::Repository[:category]
-search_repository      = Core::Registries::Repository[:search]
-static_page_repository = Core::Registries::Repository[:static_page]
+action_plan_repository  = Core::Registries::Repository[:action_plan]
+article_repository      = Core::Registries::Repository[:article]
+category_repository     = Core::Registries::Repository[:category]
+search_repository       = Core::Registries::Repository[:search]
+static_page_repository  = Core::Registries::Repository[:static_page]
+news_article_repository = Core::Registries::Repository[:news_article]
 
 Core::Registries::Repository[:action_plan] = Core::Repositories::VCR.new(action_plan_repository)
 Core::Registries::Repository[:article]     = Core::Repositories::VCR.new(article_repository)
 Core::Registries::Repository[:category]    = Core::Repositories::VCR.new(category_repository)
 Core::Registries::Repository[:search]      = Core::Repositories::VCR.new(search_repository)
 Core::Registries::Repository[:static_page] = Core::Repositories::VCR.new(static_page_repository)
+Core::Registries::Repository[:news_article] = Core::Repositories::VCR.new(news_article_repository)
 
 Before('@fake-articles') do
   @real_article_repository = Core::Registries::Repository[:article]

0e8debfb06a2a6cbcbe143845a2e7c38e64244b2
Steven Wilkin <stevenwilkin@gmail.com>
2014-06-26 17:16:08 +0100

    Add first cut of HomeCategory domain object

---
+++ app/domain/home_category.rb
@@ -0,0 +1,11 @@
+class HomeCategory
+  include Rails.application.routes.url_helpers
+
+  def title
+    I18n.t('layouts.home')
+  end
+
+  def path
+    root_path(locale: I18n.locale)
+  end
+end
---
+++ spec/domain/home_category_spec.rb
@@ -0,0 +1,12 @@
+RSpec.describe HomeCategory do
+  let(:locale) { :en }
+
+  subject { described_class.new }
+
+  before do
+    allow(I18n).to receive(:locale).and_return(locale)
+  end
+
+  specify { expect(subject.title).to eq(I18n.t('layouts.home')) }
+  specify { expect(subject.path).to eq(root_path(locale: locale)) }
+end

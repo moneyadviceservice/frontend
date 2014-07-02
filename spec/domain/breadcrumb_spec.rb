5331c9b958d21c9ea5e2c2c5063afb1d6bc149c4
Steven Wilkin <stevenwilkin@gmail.com>
2014-07-02 11:11:55 +0100

    Instantiate a Breadcrumb from a NewsCategory

--- spec/domain/breadcrumb_spec.rb
+++ spec/domain/breadcrumb_spec.rb
@@ -2,7 +2,7 @@ RSpec.describe Breadcrumb do
   let(:locale)   { :en }
   let(:id)       { 'category-id' }
   let(:title)    { 'category-title' }
-  let(:category) { double(id: id, title: title, home?: false) }
+  let(:category) { double(id: id, title: title, home?: false, news?: false) }
 
   subject { described_class.new(category) }
 
@@ -15,6 +15,13 @@ RSpec.describe Breadcrumb do
 
   context 'when the category is a HomeCategory' do
     let(:category) { HomeCategory.new }
+
+    specify { expect(subject.title).to eq(category.title) }
+    specify { expect(subject.path).to eq(category.path) }
+  end
+
+  context 'when the category is a NewsCategory' do
+    let(:category) { NewsCategory.new }
 
     specify { expect(subject.title).to eq(category.title) }
     specify { expect(subject.path).to eq(category.path) }

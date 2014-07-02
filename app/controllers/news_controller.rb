426a0da1da1216d174543db682934ea188dc1297
Pablo Vicente <PabloVicente.Carrera@moneyadviceservice.org.uk>
2014-07-02 11:53:14 +0100

    Add index action to NewsController

--- app/controllers/news_controller.rb
+++ app/controllers/news_controller.rb
@@ -1,6 +1,12 @@
 class NewsController < ApplicationController
   decorates_assigned :news_article, with: NewsArticleDecorator
 
+  def index
+    @news = Core::NewsReader.new.call do
+      not_found
+    end
+  end
+
   def show
     @news_article = Core::NewsArticleReader.new(params[:id]).call do
       not_found

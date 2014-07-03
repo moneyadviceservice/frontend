06cbbe67c04820586a4388f917238732efac4240
Pablo Vicente <PabloVicente.Carrera@moneyadviceservice.org.uk>
2014-07-03 11:26:32 +0100

    Add intro to NewsArticleDecorator

--- spec/decorators/news_article_decorator_spec.rb
+++ spec/decorators/news_article_decorator_spec.rb
@@ -2,24 +2,18 @@ RSpec.describe NewsArticleDecorator do
 
   subject(:decorator) { described_class.decorate(news_article) }
 
-  let(:news_article) { Core::NewsArticle.new('news_article_id') }
+  let(:news_article) { Core::NewsArticle.new('news_article_id', attributes) }
+  let(:attributes) { {} }
 
   it { is_expected.to respond_to(:title) }
   it { is_expected.to respond_to(:description) }
   it { is_expected.to respond_to(:content) }
   it { is_expected.to respond_to(:date) }
+  it { is_expected.to respond_to(:intro) }
 
   describe '#content' do
-    let(:fixture) { 'spec/fixtures/news_article.json' }
-    let(:attributes) do
-      {
-        title:        'news aticle title',
-        description:  'news article description',
-        body:         MultiJson.load(File.read(fixture))['body']
-      }
-    end
+    let(:attributes) { { body: '<p>Body content</><p><em>Photo: PA</em></p>'} }
 
-    let(:news_article) { Core::NewsArticle.new('news_article_id', attributes) }
     let(:html) { Nokogiri::HTML(decorator.content) }
 
     it 'removes the image author paragraph' do
@@ -42,7 +36,6 @@ RSpec.describe NewsArticleDecorator do
 
   describe '#date' do
     let(:attributes) { { date: '2014-03-17T09:42:11+00:00' } }
-    let(:news_article) { Core::NewsArticle.new('news_article_id', attributes) }
 
     it "returns the date with format 'day abbr_month year'" do
       expect(decorator.date).to eq('17 Mar 2014')
@@ -52,4 +45,14 @@ RSpec.describe NewsArticleDecorator do
       expect(decorator.date(format: :long)).to eq('2014-03-17 09:42')
     end
   end
+
+  describe '#intro' do
+    let(:intro) { 'This is the intro' }
+    let(:attributes) { { body: "<p class=\'intro\'>#{intro}</p><p>Other content</p>" } }
+
+    it 'returns the intro paragraph' do
+      expect(decorator.intro).to eq(intro)
+    end
+  end
+
 end

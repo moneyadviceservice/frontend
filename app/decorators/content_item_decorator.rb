6998ade499a1f798d595f061b3c17c621225b5dd
Jon Gilbraith <jon.gilbraith@moneyadviceservice.org.uk>
2014-07-04 16:01:40 +0100

    No longer need primary/secondary business in the content item decorator.

--- app/decorators/content_item_decorator.rb
+++ app/decorators/content_item_decorator.rb
@@ -20,35 +20,7 @@ class ContentItemDecorator < Draper::Decorator
   end
 
   def content
-    case object
-      when Core::StaticPage
-
-        case object.id
-          when 'contact-us'
-            h.render 'contact_page/en_contact_detail_primary'
-          else
-            processed_body.html_safe
-        end
-      else
-        processed_body.html_safe
-    end
-  end
-
-  def secondary_content
-    case object
-      when Core::StaticPage
-
-        case object.id
-          when 'contact-us'
-            h.render 'contact_page/en_contact_detail_secondary'
-        end
-      else
-        nil
-    end
-  end
-
-  def secondary_content?
-    !!secondary_content
+    processed_body.html_safe
   end
 
   private

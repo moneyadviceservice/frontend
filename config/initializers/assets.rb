94db1e6f51fdbab4c9d35deabf06dd98fb8d6636
Andrew Garner <andrew@andrewgarner.com>
2014-07-15 08:06:00 +0100

    Rename assets initialiser to mirror config naming

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -3,3 +3,4 @@ require 'asset_pipeline/processors/css_lint'
 
 Rails.application.assets.register_postprocessor('application/javascript', AssetPipeline::Processors::JsHint)
 Rails.application.assets.register_postprocessor('text/css', AssetPipeline::Processors::CssLint)
+

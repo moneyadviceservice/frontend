6a0df9fc607f83c1a5dd0a4b0cbddef3ff8ba8e2
Jon Gilbraith <jon.gilbraith@moneyadviceservice.org.uk>
2014-07-21 15:44:40 +0100

    Store the current page in the session.
    public_website uses this to know where to redirect to after login.

--- app/controllers/concerns/authentication.rb
+++ app/controllers/concerns/authentication.rb
@@ -3,11 +3,16 @@ module Authentication
 
   included do
     helper_method :user_signed_in?
+    before_action :store_location
   end
 
   private
 
   def user_signed_in?
     session.keys.member?('warden.user.user.key')
+  end
+
+  def store_location
+    session[:user_return_to] = request.fullpath
   end
 end

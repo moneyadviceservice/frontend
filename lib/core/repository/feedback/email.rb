a43b710b13e285f559151c863e5959b8419ca163
Jon Gilbraith <jon.gilbraith@moneyadviceservice.org.uk>
2014-07-22 16:58:00 +0100

    Integrate the smtp connection with the feedback email repository.

--- lib/core/repository/feedback/email.rb
+++ lib/core/repository/feedback/email.rb
@@ -1,17 +1,55 @@
 module Core::Repository
   module Feedback
-    class Email
+    class Email < Core::Repository::Base
+      def initialize
+        self.connection = Core::Registry::Connection[:internal_email]
+      end
+
       def create(entity)
-        Mailer.feedback_email(entity).deliver
+        email_details = {
+          recipient: recipient_for_entity(entity),
+          subject:   subject_for_entity(entity),
+          body:      body_for_entity(entity)
+        }
+        connection.deliver(email_details)
       end
-    end
 
-    class Mailer < ActionMailer::Base
+      private
 
-      def feedback_email(feedback_entity)
+      attr_accessor :connection
 
-        true
+      def recipient_for_entity(entity)
+        case entity
+        when Core::Feedback::Article
+          Rails.configuration.article_feedback_email
+        when Core::Feedback::Technical
+          Rails.configuration.technical_feedback_email
+        end
       end
+
+      def subject_for_entity(entity)
+        case entity
+        when Core::Feedback::Article
+          'Article Feedback'
+        when Core::Feedback::Technical
+          'Technical Feedback'
+        end
+      end
+
+      def body_for_entity(entity)
+        case entity
+        when Core::Feedback::Article
+          "Did they find the article useful: #{entity.useful}\n\n" +
+          "Their suggestions:\n\n" +
+          "#{entity.suggestions}"
+        when Core::Feedback::Technical
+          "What they were trying to do:\n\n" +
+          "#{entity.attempting}\n\n\n" +
+          "What happened when they tried:\n\n" +
+          "#{entity.occurred}"
+        end
+      end
+
     end
   end
 end
--- spec/lib/core/repository/feedback/email_spec.rb
+++ spec/lib/core/repository/feedback/email_spec.rb
@@ -1,22 +1,74 @@
 RSpec.describe Core::Repository::Feedback::Email do
+  let(:connection) { double }
   let(:feedback_entity) { double }
-  let(:mailer) { double }
   subject(:repository) { described_class.new }
 
   before do
-    allow(Core::Repository::Feedback::Mailer).to receive(:feedback_email) { mailer }
-    allow(mailer).to receive(:deliver)
+    allow(Core::Registry::Connection).to receive(:[]).with(:internal_email) do
+      connection
+    end
   end
 
   describe '#create' do
-    it 'calls GeneralMailer.feedback_email to instantiate a mailer' do
-      expect(Core::Repository::Feedback::Mailer).to receive(:feedback_email).with(feedback_entity)
-      subject.create(feedback_entity)
+    let(:recipient) { double }
+    let(:subject) { double }
+    let(:body) { double }
+
+    let(:expected_email_details) do
+      {
+        recipient: recipient,
+        subject:   subject,
+        body:      body
+      }
     end
 
-    it 'calls deliver on the mailer' do
-      expect(mailer).to receive(:deliver)
-      subject.create(feedback_entity)
+    before do
+      allow(repository).to receive(:recipient_for_entity) { recipient }
+      allow(repository).to receive(:subject_for_entity) { subject }
+      allow(repository).to receive(:body_for_entity) { body }
+      allow(connection).to receive(:deliver)
+    end
+
+    it 'calls #recipient_for_entity to get the recipient' do
+      expect(repository).to receive(:recipient_for_entity).with(feedback_entity)# { recipient }
+      repository.create(feedback_entity)
+    end
+
+    it 'calls #subject_for_entity to get the recipient' do
+      expect(repository).to receive(:subject_for_entity).with(feedback_entity)# { recipient }
+      repository.create(feedback_entity)
+    end
+
+    it 'calls #recipient_for_entity to get the recipient' do
+      expect(repository).to receive(:body_for_entity).with(feedback_entity)# { recipient }
+      repository.create(feedback_entity)
+    end
+
+    it 'calls deliver on the connection with the right attributes' do
+      expect(connection).to receive(:deliver).with(expected_email_details)
+      repository.create(feedback_entity)
+    end
+  end
+
+  describe 'private methods' do
+    describe 'recipient_for_entity' do
+      subject { repository.send(:recipient_for_entity, entity) }
+
+      context 'when the entity is article feedback' do
+        let(:entity) { Core::Feedback::Article.new }
+
+        it 'returns the article feedback email from the rails configuration object' do
+          expect(subject).to eql Rails.configuration.article_feedback_email
+        end
+      end
+
+      context 'when the entity is technical feedback' do
+        let(:entity) { Core::Feedback::Technical.new }
+
+        it 'returns the article tecnical email from the rails configuration object' do
+          expect(subject).to eql Rails.configuration.technical_feedback_email
+        end
+      end
     end
   end
 end

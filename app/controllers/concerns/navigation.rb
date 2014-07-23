7c7023ae68ab389f54da2ef38c11ac4ebe396944
Pablo Vicente <PabloVicente.Carrera@moneyadviceservice.org.uk>
2014-07-23 11:20:35 +0100

    Add assign_active_categories to Navigation concern

--- app/controllers/concerns/navigation.rb
+++ app/controllers/concerns/navigation.rb
@@ -8,12 +8,17 @@ module Navigation
 
   private
 
-  def active_categories
-    @active_categories
+  attr_reader :active_categories
+
+  def assign_active_categories(*categories)
+    categories.each do |category|
+      active_category category.id
+      active_category category.parent_id if category.child?
+    end
   end
 
-  def active_category(category)
-    @active_categories << category
+  def active_category(category_id)
+    @active_categories << category_id
   end
 
   def clear_active_categories

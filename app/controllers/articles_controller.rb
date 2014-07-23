368cf87d311bae50a4d689742f2bece9d16d0359
Pablo Vicente <PabloVicente.Carrera@moneyadviceservice.org.uk>
2014-07-23 16:06:14 +0100

    Fix rubocop styling error

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -10,8 +10,8 @@ class ArticlesController < ApplicationController
     end
 
     @breadcrumbs = BreadcrumbTrail.build(@article, category_tree)
-    @related_content = CategoriesWithRestrictedContents.build(@article.categories,
-      RelatedContent.build(@article))
+    @related_content = CategoriesWithRestrictedContents
+      .build(@article.categories, RelatedContent.build(@article))
 
     @article.categories.each do |category|
       active_category category.id

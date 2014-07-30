fb4c9f64cf94d9b781a766a4d5c0e752482a03b9
Phil Lee <asmega@ph-lee.com>
2014-07-30 16:23:57 +0100

    WIP add devise registrations

--- features/step_definitions/registration_steps.rb
+++ features/step_definitions/registration_steps.rb
@@ -1,7 +1,19 @@
 When(/^I attempt to register$/) do
-  sign_up_page.load
+  sign_up_page.load(locale: 'en')
 end
 
 When(/^I register$/) do
-  sign_up_page.load
+  Rails.application.reload_routes!
+  Devise.regenerate_helpers!
+  User.delete_all
+
+  sign_up_page.load(locale: 'en')
+  sign_up_page.email.set "phil@example.com"
+  sign_up_page.password.set "password"
+  sign_up_page.password_confirmation.set "password"
+  sign_up_page.submit.click
+end
+
+Then(/^My MAS account should be created$/) do
+  expect(User.count(email: "phil@example.com")).to eql(1)
 end

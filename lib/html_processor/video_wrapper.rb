2e784981dc40ee91794e317a4d81a713bdd1ff74
Pablo Vicente <PabloVicente.Carrera@moneyadviceservice.org.uk>
2014-08-01 15:18:18 +0100

    Add title attribute to video frames

--- lib/html_processor/video_wrapper.rb
+++ lib/html_processor/video_wrapper.rb
@@ -5,9 +5,12 @@ module HTMLProcessor
     def process(*xpaths)
       doc.xpath(*xpaths).each do |node|
         node.parent.name == 'p' ? swap_node(node.parent) : swap_node(node)
+        node.set_attribute('title', 'Video') unless node.attribute('title')
       end
       super
     end
+
+    private
 
     def swap_node(node)
       node.replace(HTMLProcessor::VIDEO_TAG_WRAPPER).first << node

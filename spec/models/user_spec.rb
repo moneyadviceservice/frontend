6e0fa9ce4c09d5cda706725510539d8029d6e068
David Kewal <david.kewal@gmail.com>
2014-08-06 09:09:28 +0100

    spec for the fake_send_confirmation_email method

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,4 +1,17 @@
 require 'spec_helper'
 
 RSpec.describe User, :type => :model do
+
+  subject(:user) { described_class.new(attributes) }
+
+  let(:attributes) { { email:                 'david@example.com',
+                       password:              'password',
+                       password_confirmation: 'password' } }
+
+  describe '#fake_send_confirmation_email' do
+    it "sends a fake confirmation email when user is saved" do
+      user.save!
+      expect(user.confirmation_sent_at).to_not be_nil
+    end
+  end
 end

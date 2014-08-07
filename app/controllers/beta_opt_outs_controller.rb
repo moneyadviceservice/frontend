954a6fa8d28db484c5a3bed7b0275d1f9d0a6867
Phil Lee <asmega@ph-lee.com>
2014-08-07 11:49:13 +0100

    do not store location when opting out
    otherwise if the user opts out then logs in they are redirected to the opt out controller

--- app/controllers/beta_opt_outs_controller.rb
+++ app/controllers/beta_opt_outs_controller.rb
@@ -2,6 +2,8 @@ class BetaOptOutsController < ApplicationController
   BETA_OPT_OUT_COOKIE_NAME = 'roptin'
   BETA_OPT_OUT_COOKIE_VALUE = 0
 
+  skip_before_action :store_location
+
   def create
     cookies.permanent[BETA_OPT_OUT_COOKIE_NAME] = BETA_OPT_OUT_COOKIE_VALUE
     redirect_to(redirection_page)

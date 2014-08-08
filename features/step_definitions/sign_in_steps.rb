e48e5d32061b672d5fd6cf02b17a83c0d3828338
Phil Lee <asmega@ph-lee.com>
2014-08-08 11:57:37 +0100

    cucumber test user signed into multiple sessions

--- features/step_definitions/sign_in_steps.rb
+++ features/step_definitions/sign_in_steps.rb
@@ -6,7 +6,7 @@ Then(/^I am told that the functionality is not implemented$/) do
   expect(status_code).to eql(501)
 end
 
-When(/^I sign in$/) do
+When(/^I (?:sign|am signed) in$/) do
   user = User.new(email: 'user@example.com', password: 'password')
   user.save!
 
@@ -32,11 +32,24 @@ Then(/^I should receive a "(.*?)" validation message$/) do |message|
   expect(page.body).to include(message)
 end
 
-Given(/^I am signed in$/) do
-  step "I sign in"
-end
-
 When(/^I sign out$/) do
   article_page.sign_out.click
 end
 
+When(/^I sign in elsewhere$/) do
+  Capybara.using_session(:other_session) do
+    sign_in_page.load(locale: 'en')
+    sign_in_page.email.set 'user@example.com'
+    sign_in_page.password.set 'password'
+    sign_in_page.submit.click
+  end
+end
+
+Then(/^I should be signed in in both places$/) do
+  Capybara.using_session(:other_session) do
+    expect(page.html).to include('My Account')
+  end
+
+  home_page.load(locale: 'en')
+  step "I should be signed in"
+end

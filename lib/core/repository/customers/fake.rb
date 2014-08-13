88cf9fd9666121c4db2fcb1e5f6c824673e41680
Phil Lee <asmega@ph-lee.com>
2014-08-13 17:07:16 +0100

    add crud for fake customers

--- lib/core/repository/customers/fake.rb
+++ lib/core/repository/customers/fake.rb
@@ -2,10 +2,29 @@ module Core
   module Repository
     module Customers
       class Fake
+        @@customers = []
+        cattr_reader :customers
+
         def find(id)
-          if id == 'known'
-            Customer.new(id)
-          end
+          customers.detect{|c| c.id == id}
+        end
+
+        def create(customer)
+          raise('Already exists') if customers.detect{|c| c.id == customer.id}
+
+          customers << customer
+        end
+
+        def update(customer)
+          c = find(customer.id)
+
+          raise 'does not exist' unless c
+
+          c.first_name = customer.first_name
+        end
+
+        def clear
+          customers.clear
         end
       end
     end
--- spec/lib/core/repository/customers/fake_spec.rb
+++ spec/lib/core/repository/customers/fake_spec.rb
@@ -1,13 +1,60 @@
-RSpec.describe Core::Repository::Customers::Fake do
-  describe 'find' do
-    context 'when the customer is non-existent' do
-      it { expect(subject.find('unknown')).to be_nil }
+module Core
+  RSpec.describe Repository::Customers::Fake do
+    before :each do
+      subject.clear
     end
 
-    context 'when the customer exists' do
-      subject{ described_class.new.find('known') }
+    describe '#find' do
+      context 'when the customer is non-existent' do
+        it { expect(subject.find('unknown')).to be_nil }
+      end
 
-      it { expect(subject.id).to eql('known') }
+      context 'when the customer exists' do
+        before :each do
+          customer = Customer.new('known', first_name: 'exists')
+          subject.create(customer)
+        end
+
+        it { expect(subject.find('known').id).to eql('known') }
+      end
+    end
+
+    describe '#create' do
+      it 'creates the customer' do
+        customer = Customer.new(first_name: 'Phil')
+        subject.create(customer)
+
+        expect(subject.customers).to_not be_empty
+      end
+
+      context 'when the customer already exists' do
+        it 'throws an exception' do
+          customer = Customer.new('phil', first_name: 'Phil')
+          subject.create(customer)
+
+          expect{ subject.create(customer) }.to raise_error
+        end
+      end
+    end
+
+    describe '#update' do
+      it 'updates the customer' do
+        customer = Customer.new('phil', first_name: 'Phil')
+        subject.create(customer)
+
+        customer = Customer.new('phil', first_name: 'Philip')
+        subject.update(customer)
+
+        expect(subject.find('phil').first_name).to eql('Philip')
+      end
+
+      context 'when the customer does not exist' do
+        it 'throws an exception' do
+          customer = Customer.new('phil', first_name: 'Phil')
+
+          expect{ subject.update(customer) }.to raise_error('does not exist')
+        end
+      end
     end
   end
 end

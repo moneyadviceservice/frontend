ffe19640cf45f794956ddffe7464f8b5dcf24ed0
Steven Wilkin <stevenwilkin@gmail.com>
2014-08-13 15:41:33 +0100

    Add chat section to page, add welsh warning element and rejig existing elements

--- features/support/ui/sections/chat.rb
+++ features/support/ui/sections/chat.rb
@@ -2,9 +2,9 @@ require_relative '../section'
 
 module UI::Sections
   class Chat < UI::Section
-    element :chat_heading, '.t-chat-heading'
-    element :chat_description, '.t-chat-description'
-    element :chat_button, '.t-chat-button'
-    element :chat_opening_times, '.t-chat-opening-times'
+    element :description, '.t-chat-description'
+    element :button, '.t-chat-button'
+    element :opening_times, '.t-chat-opening-times'
+    element :welsh_warning, '.t-welsh-warning'
   end
 end

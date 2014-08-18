a3dac10eb29bdbfb83740cf64def1c0f2d0a55c9
Phil Lee <asmega@ph-lee.com>
2014-08-18 16:02:00 +0100

    split out styleguide tests for better granularity

--- spec/requests/styleguide_spec.rb
+++ spec/requests/styleguide_spec.rb
@@ -1,25 +1,26 @@
 RSpec.describe 'Styleguide', :type => :request do
   let(:repository) { Core::Repository::Categories::Fake.new }
-  let(:routes) do
-    Rails.application.routes.routes.map do |route|
-      route.path.spec.to_s
-    end.select do |route|
-      route =~ /styleguide/
-    end.map do |route|
-      route.sub(/\(.:format\)/, '')
-    end.map do |route|
-      route.sub(/:locale/, 'en')
-    end
-  end
+
+  routes = Rails.application.routes.routes.map do |route|
+             route.path.spec.to_s
+           end.select do |route|
+             route =~ /styleguide/
+           end.map do |route|
+             route.sub(/\(.:format\)/, '')
+           end.map do |route|
+             route.sub(/:locale/, 'en')
+           end
 
   before do
     allow(Core::Registry::Repository).to receive(:[]).with(:category).and_return(repository)
   end
 
-  it 'gives a 200 for each styleguide page' do
+  describe 'styleguide pages' do
     routes.each do |route|
-      get route
-      expect(response.status).to eq(200)
+      it "gives a 200 for each styleguide page #{route}" do
+        get route
+        expect(response.status).to eq(200)
+      end
     end
   end
 end

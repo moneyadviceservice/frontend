1f2ae56a7c474d5c6bfdcc81246d7abb1c6b2551
Jon Gilbraith <jon.gilbraith@moneyadviceservice.org.uk>
2014-08-26 15:36:53 +0100

    Create a ToolCategory object.

---
+++ app/domain/tool_category.rb
@@ -0,0 +1,27 @@
+class ToolCategory
+
+  attr_reader :id
+
+  def initialize(category_id)
+    @id = category_id.to_s
+  end
+
+  def title
+    entity.title
+  end
+
+  def home?
+    false
+  end
+
+  def news?
+    false
+  end
+
+  private
+
+  def entity
+    @entity ||= Core::CategoryReader.new(id).call
+  end
+
+end

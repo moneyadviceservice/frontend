74adb5ee2b8908fff3254dbb4384dcbc78abd18e
Pablo Vicente <PabloVicente.Carrera@moneyadviceservice.org.uk>
2014-08-26 14:16:00 +0100

    CarCampaignController show action creates a campaign from template

--- app/controllers/car_campaigns_controller.rb
+++ app/controllers/car_campaigns_controller.rb
@@ -1,5 +1,6 @@
 class CarCampaignsController < ApplicationController
   def show
+     @campaign = Template.new.build_campaign(params[:id])
   end
 
   def display_menu_button_in_header?
--- spec/controllers/car_campaigns_controller_spec.rb
+++ spec/controllers/car_campaigns_controller_spec.rb
@@ -1,7 +1,20 @@
 RSpec.describe CarCampaignsController, :type => :controller do
   describe 'GET show' do
+    let(:id) { 'revealed-the-true-cost-of-buying-a-car' }
+    let(:template) { double }
+
+    before { allow(Template).to receive(:new) { template } }
+
+    it 'builds a campaing' do
+      expect(template).to receive(:build_campaign).with(id)
+
+      get :show, locale: I18n.locale, id: id
+    end
+
     it 'is successful' do
-      get :show, locale: I18n.locale
+      allow(template).to receive(:build_campaign) { double }
+
+      get :show, locale: I18n.locale, id: id
 
       expect(response).to be_ok
     end

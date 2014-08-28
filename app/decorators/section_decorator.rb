e8b69227f3a57a719c6da39af917a2f0911a79db
Pablo Vicente <PabloVicente.Carrera@moneyadviceservice.org.uk>
2014-08-28 12:21:34 +0100

    Adjust campaign css and view

--- app/decorators/section_decorator.rb
+++ app/decorators/section_decorator.rb
@@ -1,7 +1,11 @@
 class SectionDecorator < Draper::CollectionDecorator
-  delegate :name
+  delegate :name, :separator
 
   def decorator_class
     ArticleDecorator
+  end
+
+  def css_name
+    name.tr("_","-")
   end
 end

9035f81f1d0b570b256999d3d8ee727c873fdde5
Steven Wilkin <stevenwilkin@gmail.com>
2014-09-09 11:27:06 +0100

    Add `query` attribute to the collection

--- lib/core/entity/search_result_collection.rb
+++ lib/core/entity/search_result_collection.rb
@@ -3,9 +3,9 @@ module Core
     include Enumerable
     extend Forwardable
 
-    attr_accessor :items, :page, :per_page, :total_results
+    attr_accessor :items, :page, :per_page, :total_results, :query
 
-    private :items=
+    private :items=, :query=
 
     def_delegators :items, :<<
 
@@ -14,6 +14,7 @@ module Core
       self.page = attributes[:page]
       self.per_page = attributes[:per_page]
       self.total_results = attributes[:total_results]
+      self.query = attributes[:query]
     end
 
     def each(*args, &block)
--- spec/lib/core/entity/search_result_collection_spec.rb
+++ spec/lib/core/entity/search_result_collection_spec.rb
@@ -13,6 +13,8 @@ module Core
     it { is_expected.to respond_to :per_page }
     it { is_expected.to respond_to :per_page= }
 
+    it { is_expected.to respond_to :query }
+
     it 'is a collection' do
       expect(subject.to_a).to be_kind_of(Array)
     end

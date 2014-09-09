7f5a1472b025a53a23cb160af6326d1dae787d9b
alexwllms <alex.williams@moneyadviceservice.org.uk>
2014-09-09 12:11:25 +0100

    Add new fixture and tests for clearing input with initial value

--- spec/javascripts/tests/ClearInput_spec.js
+++ spec/javascripts/tests/ClearInput_spec.js
@@ -1,4 +1,4 @@
-describe('clear input field', function () {
+describe('clear input field without initial value', function () {
 
   'use strict';
 
@@ -17,23 +17,71 @@ describe('clear input field', function () {
       }, done);
     });
 
-  it('should show the reset button when entering text into the input', function () {
-    this.$input.val('Debt').trigger('keydown');
-    expect(this.$button).to.have.class('is-active');
+    it('shows the reset button when entering text into the input', function () {
+      this.$input.val('Debt').trigger('keydown');
+      expect(this.$button).to.have.class('is-active');
+    });
+
+    it('hides the reset button when it is clicked', function () {
+      this.$input.val('Debt').trigger('keydown');
+      expect(this.$button).to.have.class('is-active');
+      this.$button.trigger('click');
+      expect(this.$button).to.not.have.class('is-active');
+    });
+
+    it('hides the reset button when text is cleared', function () {
+      this.$input.val('Debt').trigger('keydown');
+      expect(this.$button).to.have.class('is-active');
+      this.$input.val('').trigger('keydown');
+      expect(this.$button).to.not.have.class('is-active');
+    });
+
+    it('focuses back on the input box when the text is cleared', function () {
+      this.$input.val('Debt').trigger('keydown');
+      expect(this.$button).to.have.class('is-active');
+      this.$input.val('').trigger('keydown');
+      expect(this.$button).to.not.have.class('is-active');
+      expect(this.$input.filter(':focus')).to.exist;
+    });
   });
 
-  it('should hide the reset button when it is clicked', function () {
-    this.$input.val('Debt').trigger('keydown');
-    expect(this.$button).to.have.class('is-active');
-    this.$button.trigger('click');
-    expect(this.$button).to.not.have.class('is-active');
-  });
+  describe('clear input field with initial value', function () {
 
-  it('should hide the reset button when text is cleared', function () {
-    this.$input.val('Debt').trigger('keydown');
-    expect(this.$button).to.have.class('is-active');
-    this.$input.val('').trigger('keydown');
-    expect(this.$button).to.not.have.class('is-active');
-  });
+    'use strict';
 
-});
+    beforeEach(function (done) {
+      var self = this;
+
+      requirejs(
+        ['jquery', 'ClearInput'],
+        function ($, ClearInput) {
+
+          self.$html = $(window.__html__['spec/javascripts/fixtures/ClearInputWithValue.html']);
+          self.$input = self.$html.find('[data-dough-clear-input]');
+          self.$button = self.$html.find('[data-dough-clear-input-button]');
+          self.mod = new ClearInput(self.$html);
+          done();
+        }, done);
+      });
+
+      it('shows the reset button when there is an initial value', function () {
+        expect(this.$button).to.have.class('is-active');
+      });
+
+      it('hides the reset button when it is clicked', function () {
+        this.$button.trigger('click');
+        expect(this.$button).to.not.have.class('is-active');
+      });
+
+      it('hides the reset button when text is cleared', function () {
+        this.$input.val('').trigger('keydown');
+        expect(this.$button).to.not.have.class('is-active');
+      });
+
+      it('focuses back on the input box when the text is cleared', function () {
+        this.$input.val('').trigger('keydown');
+        expect(this.$button).to.not.have.class('is-active');
+        expect(this.$input.filter(':focus')).to.exist;
+      });
+
+    });

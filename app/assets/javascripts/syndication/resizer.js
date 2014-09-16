ae9be50e4ceda394d1615ff8dfe80f55017098bb
Jon Wyatt <jonnywyatt@yahoo.co.uk>
2014-09-16 10:57:51 +0100

    JSHint compliance

--- app/assets/javascripts/syndication/resizer.js
+++ app/assets/javascripts/syndication/resizer.js
@@ -1,22 +1,20 @@
 window.handleMASResizeMessage = function(event) {
-  var error, height, iframe, parts, prefix;
+  var height, iframe, parts, prefix;
   try {
-    if (typeof event.data === "string") {
-      parts = event.data.split("-");
+    if (typeof event.data === 'string') {
+      parts = event.data.split('-');
       prefix = parts[0];
       height = parseInt(parts[1]);
-      if (prefix === "MASRESIZE") {
-        iframe = document.querySelector(".mas-widget-iframe");
-        iframe.style.height = height + "px";
+      if (prefix === 'MASRESIZE') {
+        iframe = document.querySelector('.mas-widget-iframe');
+        iframe.style.height = height + 'px';
       }
     }
-  } catch (_error) {
-    error = _error;
-  }
+  } catch (err) { }
 };
 
 if (window.addEventListener) {
-  addEventListener("message", window.handleMASResizeMessage, false);
+  addEventListener('message', window.handleMASResizeMessage, false);
 } else {
-  attachEvent("onmessage", window.handleMASResizeMessage);
+  attachEvent('onmessage', window.handleMASResizeMessage);
 }

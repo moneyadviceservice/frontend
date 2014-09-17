0aec7dd5203cdc6fe72a435544c901f853aa8f2d
Mark Connell <mark@markconnell.co.uk>
2014-09-17 12:31:05 +0100

    Create a ToolMountPoint for the car cost calculator. Add engine mounting to route.rb

--- config/routes.rb
+++ config/routes.rb
@@ -40,6 +40,9 @@ Rails.application.routes.draw do
     mount PensionsCalculator::Engine => '/tools/:tool_id',
           constraints: ToolMountPoint.for(:pensions_calculator)
 
+    mount CarCostTool::Engine => '/tools/:tool_id',
+          constraints: ToolMountPoint.for(:car_cost_tool)
+
     Feature.with(:budget_planner) do
       bpmp = ToolMountPoint.for(:budget_planner)
       budget_planner_url_constraint = /#{bpmp.en_id}|#{bpmp.cy_id}/

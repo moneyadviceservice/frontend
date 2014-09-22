e33dbe4843514bb4b884b11015ac2a00717386c9
alexwllms <alex.williams@moneyadviceservice.org.uk>
2014-09-22 12:14:54 +0100

    Remove unused article and advice feedback elements

--- features/support/ui/sections/feedback_panel.rb
+++ features/support/ui/sections/feedback_panel.rb
@@ -2,8 +2,6 @@ require_relative '../section'
 
 module UI::Sections
   class FeedbackPanel < UI::Section
-    element :article_feedback_link, '.t-feedback-panel__article-feedback-link'
-    element :technical_feedback_link, '.t-feedback-panel__technical-feedback-link'
-    element :advice_link, '.t-feedback-panel__advice-link'
+    element :technical_feedback_link, '.t-technical-feedback-link'
   end
 end

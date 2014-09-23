5569ee4baf40f381ce65200366d85d3544ad96d9
alexwllms <alex.williams@moneyadviceservice.org.uk>
2014-09-23 09:51:46 +0100

    Change technical_feedbacks to technical_feedback

--- features/step_definitions/feedback_steps.rb
+++ features/step_definitions/feedback_steps.rb
@@ -49,7 +49,7 @@ Then(/^I should be on the article feedback page$/) do
 end
 
 Then(/^I should be on the technical feedback page$/) do
-  expect(current_page.heading).to have_content(I18n.t('technical_feedbacks.new.title'))
+  expect(current_page.heading).to have_content(I18n.t('technical_feedback.new.title'))
 end
 
 Then(/^I should be on the advice page$/) do
@@ -89,7 +89,7 @@ Then(/^I should see a confirmation message that my article feedback has been rec
 end
 
 Then(/^I should see a confirmation message that my technical feedback has been received$/) do
-  expect(current_page).to have_content(I18n.t('technical_feedbacks.create.flash_notice'))
+  expect(current_page).to have_content(I18n.t('technical_feedback.create.flash_notice'))
 end
 
 Then(/^the (technical|article) feedback email should have been sent$/) do |type|

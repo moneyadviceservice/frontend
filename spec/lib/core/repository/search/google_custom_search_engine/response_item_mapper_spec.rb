ee891037ce0c55ed2b484d4957aa9c75cd3cc085
Pablo Vicente <PabloVicente.Carrera@moneyadviceservice.org.uk>
2014-09-25 15:09:46 +0100

    Remove description mapping

--- spec/lib/core/repository/search/google_custom_search_engine/response_item_mapper_spec.rb
+++ spec/lib/core/repository/search/google_custom_search_engine/response_item_mapper_spec.rb
@@ -4,7 +4,6 @@ module Core::Repository::Search
     describe '#mapped_item_response' do
       let(:id) { 'action-plan-id' }
       let(:title) { 'The action plan' }
-      let(:description) { 'Action plan description' }
       let(:type) { 'action_plan' }
       let(:link) { "https://www.moneyadviceservice.org.uk/en/#{type.pluralize}/#{id}" }
       let(:snippet) { 'Action plan snippet' }
@@ -13,14 +12,6 @@ module Core::Repository::Search
           'link'      => link,
           'htmlTitle' => title,
           'htmlSnippet'   => snippet,
-          'pagemap'   => {
-            'metatags' => [
-              {
-                'csrf-param'     => 'authenticity_token',
-                'og:description' => description
-              }
-            ]
-          }
         }
       end
 
@@ -34,32 +25,12 @@ module Core::Repository::Search
         expect(subject[:title]).to eq(title)
       end
 
-      it 'maps the description correctly' do
-        expect(subject[:description]).to eq(description)
-      end
-
       it 'maps the link correctly' do
         expect(subject[:link]).to eq(link)
       end
 
       it 'maps the snippet correctly' do
         expect(subject[:snippet]).to eq(snippet)
-      end
-
-      context "when it doesn't have pagemap" do
-        before { item.delete_if { |key, val| key == 'pagemap' } }
-
-        it 'maps the description to empty string' do
-          expect(subject[:description]).to be_empty
-        end
-      end
-
-      context "when it doesn't have metatags" do
-        before { item['pagemap'].delete_if { |key, val| key == 'metatags' } }
-
-        it 'maps the description to empty string' do
-          expect(subject[:description]).to be_empty
-        end
       end
     end
   end

6755c54a40460a276f7d1c74302baccd3e00a00c
Mark Connell <mark@markconnell.co.uk>
2014-09-29 16:08:35 +0100

    correctly handle the different urls for stamp duty and mortgage affordability tools.

--- lib/tool_mount_point/mortgage_calculator.rb
+++ lib/tool_mount_point/mortgage_calculator.rb
@@ -1,5 +1,16 @@
 module ToolMountPoint
   class MortgageCalculator < Base
+    ADDITIONAL_TOOLS = {
+      'cy' => {
+        'stamp-duty-calculator'             => 'cyfrifiannell-treth-stamp',
+        'mortgage-affordability-calculator' => 'cyfrifiannell-fforddiadwyedd-morgais'
+      },
+      'en' => {
+        'cyfrifiannell-treth-stamp'            => 'stamp-duty-calculator',
+        'cyfrifiannell-fforddiadwyedd-morgais' => 'mortgage-affordability-calculator'
+      }
+    }
+
     def alternate_tool_id(current_tool_id)
       {
         'mortgage-calculator'   => 'cyfrifiannell-morgais',
@@ -7,6 +18,22 @@ module ToolMountPoint
         'house-buying'          => 'prynu-ty',
         'prynu-ty'              => 'house-buying',
       }.fetch(current_tool_id)
+    end
+
+    def alternate_url(url, locale=nil)
+      return url if locale.nil?
+      ADDITIONAL_TOOLS[locale].keys.each do |calculator_name|
+        if url.include?(calculator_name)
+          return url.gsub(calculator_name, translated_calculator_name(locale, calculator_name))
+        end
+      end
+      url
+    end
+
+    private
+
+    def translated_calculator_name(locale, calculator_name)
+      ADDITIONAL_TOOLS[locale][calculator_name]
     end
   end
 end
---
+++ spec/controllers/embedded_tools_controller_spec.rb
@@ -0,0 +1,38 @@
+RSpec.describe EmbeddedToolsController do
+
+  describe '#alternate_url' do
+    before do
+      allow(subject).to receive(:url_for).and_return('http://some.url')
+    end
+
+    context 'no alternate_url on the mount_point' do
+      let(:mount_point) do
+        double(alternate_locale: 'en', alternate_tool_id: 'tool_name')
+      end
+
+      it 'ignores the mount point' do
+        allow(ToolMountPoint).to receive(:for).and_return(mount_point)
+        expect(subject.send(:alternate_url)).to eq('http://some.url')
+      end
+    end
+
+    context 'mount_point has alternate_url' do
+      let(:mount_point) do
+        double(alternate_url: 'http://alternate.url',
+               alternate_locale: 'en', alternate_tool_id: 'tool_name')
+      end
+
+      it 'returns an alternate_url if one is available' do
+        allow(ToolMountPoint).to receive(:for).and_return(mount_point)
+        expect(subject.send(:alternate_url)).to eq('http://alternate.url')
+      end
+
+      it 'passes the url and locale to the mount_point' do
+        allow(ToolMountPoint).to receive(:for).and_return(mount_point)
+        expect(mount_point).to receive(:alternate_url).with('http://some.url', 'en')
+        subject.send(:alternate_url)
+      end
+    end
+  end
+
+end

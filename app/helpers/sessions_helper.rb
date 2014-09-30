8957ce73939465d8262ded6a946752f24b6c187c
Phil Lee <asmega@ph-lee.com>
2014-09-30 13:30:22 +0100

    swap double for single quotes

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,11 +1,11 @@
 module SessionsHelper
   def authentication_sign_in_title
     I18n.t(session['authentication_sign_in_title'],
-           default: I18n.t("authentication.sign_in_page.title"))
+           default: I18n.t('authentication.sign_in_page.title'))
   end
 
   def authentication_registration_title
     I18n.t(session['authentication_registration_title'],
-           default: I18n.t("authentication.registration.title"))
+           default: I18n.t('authentication.registration.title'))
   end
 end

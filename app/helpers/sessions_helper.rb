9e7264fd6f6493affbda13c77a724b29c2d60391
Phil Lee <asmega@ph-lee.com>
2014-09-30 11:53:24 +0100

    change API for custom auth titles

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,5 +1,5 @@
 module SessionsHelper
-  def authentication_sign_in_page_title
+  def authentication_sign_in_title
     I18n.t(session['authentication_sign_in_title'],
            default: I18n.t("authentication.sign_in_page.title"))
   end

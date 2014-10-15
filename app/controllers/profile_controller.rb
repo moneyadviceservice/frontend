556943a66f1405e2aba48b8c7c8ad1c22435aa7f
Lawrance Shepstone <lawrance.shepstone@gmail.com>
2014-10-15 14:13:45 +0100

    Ensure the user is signed in for all profile related actions.

--- app/controllers/profile_controller.rb
+++ app/controllers/profile_controller.rb
@@ -1,5 +1,12 @@
 class ProfileController < ArticlesController
-  def edit
+  before_action :require_sign_in
 
+  def edit
+  end
+
+  private
+
+  def require_sign_in
+    redirect_to new_user_session_path unless user_signed_in?
   end
 end

9cafd4f2d76166e3fa038865488907151694e253
Steven Wilkin <stevenwilkin@gmail.com>
2014-10-15 15:13:14 +0100

    Use RSpec meta-data to turn on specific features for specs

--- spec/requests/registration_spec.rb
+++ spec/requests/registration_spec.rb
@@ -1,16 +1,6 @@
 require 'spec_helper'
 
-RSpec.describe 'Registration routing', type: :request do
-  around :each do |example|
-    Feature.run_with_activated(:registration) do
-      Rails.application.reload_routes!
-
-      example.run
-    end
-
-    Rails.application.reload_routes!
-  end
-
+RSpec.describe 'Registration routing', features: [:registration], type: :request do
   it '501 on user updating account details' do
     get('/en/users/edit')
     expect(response.status).to eq(501)

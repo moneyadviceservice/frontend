a6fc06ef478d75a31b737d9d3ff229405d897c7c
Lawrance Shepstone <lawrance.shepstone@gmail.com>
2014-10-15 14:37:34 +0100

    Goal section to profile page, with English translations only.

--- features/step_definitions/profile_steps.rb
+++ features/step_definitions/profile_steps.rb
@@ -3,5 +3,12 @@ When(/^I view my profile page$/) do
 end
 
 Then(/^I see my name$/) do
-  expect(current_page.heading.text).to include(User.all.first.first_name)
+  expect(profile_page.heading.text).to include(User.all.first.first_name)
+end
+
+Then(/^I see my goal and goal date is blank$/) do
+  expect(profile_page.goal_text.value).to eql('')
+  expect(profile_page.goal_date.value).to eql('')
+end
+
 end
--- features/support/ui/pages/profile.rb
+++ features/support/ui/pages/profile.rb
@@ -5,5 +5,8 @@ module UI::Pages
     set_url '{/locale}/users/profile'
 
     element :heading, 'h1'
+    element :goal_text, '#goal_form input[name="goal_text"]'
+    element :goal_date, '#goal_form input[name="goal_date"]'
+    element :goal_save, '#goal_form input[type="submit"]'
   end
 end

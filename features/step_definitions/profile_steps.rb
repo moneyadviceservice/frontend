5974654530d9ad3f27f01ebdb666881bb5001ce5
Lawrance Shepstone <lawrance.shepstone@gmail.com>
2014-10-20 11:00:09 +0100

    Use `be_empty` instead of `eql('')`.

--- features/step_definitions/profile_steps.rb
+++ features/step_definitions/profile_steps.rb
@@ -7,8 +7,8 @@ Then(/^I see my name$/) do
 end
 
 Then(/^I see my goal and goal date is blank$/) do
-  expect(profile_page.goal_statement.value).to eql('')
-  expect(profile_page.goal_deadline.value).to eql('')
+  expect(profile_page.goal_statement.value).to be_empty
+  expect(profile_page.goal_deadline.value).to be_empty
 end
 
 When(/^I set a new goal and goal date$/) do

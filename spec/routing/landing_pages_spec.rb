89f51b849e1d34161e6a6eee24ce70edae483d0d
Ben Lovell <benjamin.lovell@gmail.com>
2014-11-07 16:09:28 +0000

    Fixes minor typo

--- spec/routing/landing_pages_spec.rb
+++ spec/routing/landing_pages_spec.rb
@@ -1,4 +1,4 @@
-RSpec.describe 'Static page rounting', type: :routing  do
+RSpec.describe 'Static page routing', type: :routing  do
   context 'when route is /tools/annuities' do
     it 'routes to static pages controller' do
       expect(get('/en/tools/annuities')).to route_to(

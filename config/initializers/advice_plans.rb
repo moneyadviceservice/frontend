ebfcb89486139d377e88cc7d3544fe3d1b987eda
jiggypete <jiggypete@gmail.com>
2014-11-11 10:37:23 +0000

    remove const_missing so frontend can start

--- config/initializers/advice_plans.rb
+++ config/initializers/advice_plans.rb
@@ -1,8 +1,4 @@
 module AdvicePlans
   ChangeOwnershipCommand = Struct.new(:current_owner)
   StartTaskCommand       = Struct.new(:current_owner, :plan_slug, :task_id, :options)
-
-  def self.const_missing(name)
-    const_set(name, Class.new)
-  end
 end

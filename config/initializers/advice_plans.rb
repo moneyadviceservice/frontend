73af72c0c5dcc1a2dfe6580069b74b8e2c12cf00
jiggypete <jiggypete@gmail.com>
2014-12-02 13:56:48 +0000

    removing now unnecessary advice plans config

--- config/initializers/advice_plans.rb
+++ config/initializers/advice_plans.rb
@@ -1,8 +1,3 @@
-module AdvicePlans
-  ChangeOwnershipCommand = Struct.new(:current_owner)
-  StartTaskCommand       = Struct.new(:current_owner, :plan_slug, :task_id, :options)
-end
-
 AdvicePlans.parent_controller = '::EmbeddedToolsController'
 
 AdvicePlans.decision_trees_url = lambda do

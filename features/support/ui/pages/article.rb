64130bb1e9403f66f79fb8fce53738c80fd81248
Phil Lee <asmega@ph-lee.com>
2015-01-15 16:36:05 +0000

    add cucumbers for want to talk feature

--- features/support/ui/pages/article.rb
+++ features/support/ui/pages/article.rb
@@ -1,6 +1,7 @@
 require_relative '../page'
 require_relative '../sections/category_nav'
 require_relative '../sections/feedback_panel'
+require_relative '../sections/want_to_talk_panel'
 
 module UI::Pages
   class Article < UI::Page
@@ -12,5 +13,8 @@ module UI::Pages
 
     section :category_nav, UI::Sections::CategoryNav, 'nav .link-list-primary'
     section :feedback_panel, UI::Sections::FeedbackPanel, '.feedback-panel'
+
+    section :want_to_talk_side_panel, UI::Sections::WantToTalkPanel, '.want-to-talk--sidebar'
+    section :want_to_talk_inline_panel, UI::Sections::WantToTalkPanel, '.want-to-talk--inline'
   end
 end

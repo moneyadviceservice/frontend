22fbb6363b3859c2982ae7a795b2a2bba8ca8b2b
Phil Lee <asmega@ph-lee.com>
2015-01-29 14:03:33 +0000

    move x frame options to ApplicationController
    Regardless of this change this presents the vulnerability of
    clickjacking when tools are syndicated

--- app/controllers/rio_controller.rb
+++ app/controllers/rio_controller.rb
@@ -1,7 +1,6 @@
 class RioController < EmbeddedToolsController
   def parent_template
     if syndicated_tool_request?
-      response.headers['X-Frame-Options'] = 'ALLOWALL'
       'layouts/engine_syndicated'
     else
       'layouts/engine_unconstrained'

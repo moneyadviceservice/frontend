d0905640a7dc4112e24138d03276b9f9ff35b579
Peter Guntrip <peter@guntrisoft.com>
2015-02-03 11:19:11 +0000

    further improvements to the JS code for newsletter signup injection

--- app/assets/javascripts/components/Newsletter.js
+++ app/assets/javascripts/components/Newsletter.js
@@ -24,36 +24,28 @@ define(['jquery', 'DoughBaseComponent'], function($, DoughBaseComponent) {
   };
 
   Newsletter.prototype.showInArticle = function() {
-    var $panel = this.getPanel();
-    var $article = $('[data-article]');
-
-    this.moveToArticle($panel, $article);
-
-    $panel.addClass('news-signup-test--with-image news-signup-test--in-article');
+    this.moveToArticle();
+    this.$el.addClass('news-signup-test--with-image news-signup-test--in-article');
   };
 
-  Newsletter.prototype.moveToArticle = function($panel, $article) {
-    var article = $article[0],
+  Newsletter.prototype.moveToArticle = function() {
+    var $article = $('[data-dough-newsletter-article]'),
         placed = false,
-        minimumPosition = article.offsetHeight / 4;
+        minimumPosition = $article[0].offsetHeight / 4;
 
-    $panel.css('clear', 'both');
+    this.$el.css('clear', 'both');
 
-    $article.find('> h2, > h3').each(function (i, el) {
+    $article.find('> h2, > h3').each($.proxy(function (i, el) {
       if (el.offsetTop >= minimumPosition) {
-        $panel.insertBefore(el);
+        this.$el.insertBefore(el);
         placed = true;
         return false;
       }
-    });
+    }, this));
 
     if (!placed) {
-      $panel.insertAfter($article.children().last());
+      this.$el.insertAfter($article.children().last());
     }
-  };
-
-  Newsletter.prototype.getPanel = function() {
-    return $('[data-dough-component="Newsletter"]');
   };
 
   return Newsletter;

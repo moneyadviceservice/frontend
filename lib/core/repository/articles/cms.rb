8f145560757b5d4772b9e898a65f63d18e7a0984
Tomas D'Stefano <tomas_stefano@successoft.com>
2015-02-06 17:02:53 +0000

    Create article cms repository

---
+++ lib/core/repository/articles/cms.rb
@@ -0,0 +1,6 @@
+module Core::Repository
+  module Articles
+    class CMS < ::Core::Repository::CMS::CMS
+    end
+  end
+end
---
+++ spec/lib/core/repository/articles/cms_spec.rb
@@ -0,0 +1,4 @@
+module Core::Repository::Articles
+  RSpec.describe CMS do
+  end
+end

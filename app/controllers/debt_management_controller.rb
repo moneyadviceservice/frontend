4fd844973d2743b4403b01689cc38202ab54ed47
Ben Barnett <ben.barnett@moneyadviceservice.org.uk>
2015-02-10 13:45:14 +0000

    Removing unnecessary controller methods

--- app/controllers/debt_management_controller.rb
+++ app/controllers/debt_management_controller.rb
@@ -1,15 +1,6 @@
 class DebtManagementController < ApplicationController
   layout '_unconstrained'
 
-  def show
-  end
-
-  def faq
-  end
-
-  def companies
-  end
-
   def display_menu_button_in_header?
     false
   end

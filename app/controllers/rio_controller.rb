a9211ffaaf474d783fa840b745c84ab0c444b175
Phil Lee <asmega@ph-lee.com>
2015-03-10 15:43:54 +0000

    add retirement breadcrumbs to rio

--- app/controllers/rio_controller.rb
+++ app/controllers/rio_controller.rb
@@ -7,7 +7,19 @@ class RioController < MountController
     'work-pensions-and-retirement'
   end
 
+  def breadcrumbs
+    categories.map(&Breadcrumb.public_method(:new))
+  end
+
   private
+
+  def category
+    @category ||= Core::CategoryReader.new('work-pensions-and-retirement').call
+  end
+
+  def categories
+    RootToNodePath.build(category, category_tree)
+  end
 
   def alternate_engine_id
     send "#{alternate_locale}_engine_id"

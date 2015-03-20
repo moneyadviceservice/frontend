c52be16f8c8fd93c641aec06e3f3735ad535fe7d
Jones Agyemang <mightyj@hotmail.co.uk>
2015-03-20 11:19:10 +0000

    [changed] tool id

--- lib/tool_mount_point/cutback_calculator.rb
+++ lib/tool_mount_point/cutback_calculator.rb
@@ -1,6 +1,6 @@
 module ToolMountPoint
   class CutbackCalculator < Base
-    EN_ID = 'cutback-calculator'
+    EN_ID = 'quick-cash-finder'
     #TODO [add] welsh id for this tool [status: \awaiting translation]
     CY_ID = ''
   end

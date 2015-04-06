80aca44600bcaabc229b7db85b77df5410fc0ee9
Mark Connell <mark@markconnell.co.uk>
2015-04-06 16:27:48 +0100

    add setter for bug snag key

--- config/initializers/mortgage_calculator.rb
+++ config/initializers/mortgage_calculator.rb
@@ -1,3 +1,5 @@
 MortgageCalculator.parent_controller = '::EmbeddedToolsController'
 
 MortgageCalculator.affordability_enabled = true
+
+MortgageCalculator.bug_snag_key = ENV["MORTGAGE_CALC_BUG_SNAG_KEY"]

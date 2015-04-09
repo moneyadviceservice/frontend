7b007b66877f732320ceb32ec41dc4c152b100ef
Phil Lee <asmega@ph-lee.com>
2015-04-09 14:38:41 +0100

    cutback_calculator welsh link ignores params

--- app/controllers/cutback_calculator_controller.rb
+++ app/controllers/cutback_calculator_controller.rb
@@ -1,6 +1,18 @@
 class CutbackCalculatorController < EmbeddedToolsController 
-  protected
-    def category_id
-      'managing-money'
+  def alternate_url
+    new_params = { script_name: "/#{alternate_locale}/tools/#{alternate_tool_id}" }
+    url = url_for(new_params)
+
+    if mount_point.respond_to? :alternate_url
+      mount_point.alternate_url(url, alternate_locale)
+    else
+      url
     end
+  end
+
+  protected
+
+  def category_id
+    'managing-money'
+  end
 end

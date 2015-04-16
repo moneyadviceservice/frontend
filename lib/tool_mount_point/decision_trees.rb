037db3879e6ec8d97d311030ca8c3b3a17b4088b
Jones Agyemang <mightyj@hotmail.co.uk>
2015-04-16 16:38:29 +0100

    [mount] workplace pension advice tool on responsive site

--- lib/tool_mount_point/decision_trees.rb
+++ lib/tool_mount_point/decision_trees.rb
@@ -1,6 +1,34 @@
 module ToolMountPoint
   class DecisionTrees < Base
-    EN_ID = 'health-check-questions'
-    CY_ID = 'health-check-questions'
+     EN_ID = 'health-check-questions'
+     CY_ID = 'health-check-questions'
+
+     class HealthCheck < Base
+       EN_ID = 'health-check-questions'
+       CY_ID = 'health-check-questions'
+     end
+
+     class WorkplacePensionAdviceTool < Base
+       EN_ID = 'workplace-pension-advice-tool'
+       CY_ID = 'workplace-pension-advice-tool'
+     end
+
+     def initialize
+       @health_check_mount_point = HealthCheck.new
+       @workplace_pension_advice_tool_mount_point = WorkplacePensionAdviceTool.new
+     end
+
+     def matches?(request)
+       @health_check_mount_point.matches?(request) || @workplace_pension_advice_tool_mount_point.matches?(request)
+     end
+
+     def alternate_tool_id(current_tool_id)
+       {
+         HealthCheck::EN_ID => HealthCheck::CY_ID,
+         HealthCheck::CY_ID => HealthCheck::EN_ID,
+         WorkplacePensionAdviceTool::EN_ID => WorkplacePensionAdviceTool::CY_ID,
+         WorkplacePensionAdviceTool::CY_ID => WorkplacePensionAdviceTool::EN_ID
+       }.fetch(current_tool_id)
+     end
   end
 end

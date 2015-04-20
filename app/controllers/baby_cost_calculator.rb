82ae0fb753bd709726e341f6d44023036186d3e4
Jones Agyemang <mightyj@hotmail.co.uk>
2015-04-20 16:18:49 +0100

    Engine initialiser for baby cost calc (+2 squashed commits) Squashed commits: [79feb8f] [mount] using ToolMountPoint [292ede9] [add] baby_cost_calc controller

---
+++ app/controllers/baby_cost_calculator.rb
@@ -0,0 +1,6 @@
+class BabyCostCalculator < ApplicationController
+  protected
+    def category_id
+      'having-a-baby'
+    end
+end
---
+++ lib/tool_mount_point/baby_cost_calculator.rb
@@ -0,0 +1,6 @@
+module ToolPointMount
+  class BabyCostCalculator < Base
+    EN_ID = 'baby-cost-calculator'
+    CY_ID = 'cyfrifiannell-costau-babi'
+  end
+end

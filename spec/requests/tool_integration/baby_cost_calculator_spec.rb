7ac484895fa9fa44e9818be770865fa05a0deac9
Jones Agyemang <mightyj@hotmail.co.uk>
2015-04-20 16:32:32 +0100

    mount baby_cost_calc engine behind feature flag

---
+++ spec/requests/tool_integration/baby_cost_calculator_spec.rb
@@ -0,0 +1,13 @@
+RSpec.describe ToolMountPoint::BabyCostCalculator, type: :request, features: [:baby_cost_calculator] do 
+  %W(
+    /en/tools/#{ToolMountPoint::BabyCostCalculator::EN_ID}
+  ).each do |path|
+    describe path do 
+      before do 
+        get path
+      end
+
+      specify { expect(response).to be_ok }
+    end
+  end
+end

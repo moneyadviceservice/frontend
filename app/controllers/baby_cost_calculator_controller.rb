c371e02962af365a87b2ec9cf714e7b19f215b95
Ben Lovell <benjamin.lovell@gmail.com>
2015-04-21 15:54:20 +0100

    expose private method to views as an helper

--- app/controllers/baby_cost_calculator_controller.rb
+++ app/controllers/baby_cost_calculator_controller.rb
@@ -1,6 +1,7 @@
-class BabyCostCalculatorController < EmbeddedToolsController 
+class BabyCostCalculatorController < EmbeddedToolsController
   protected
-    def category_id
-      'having-a-baby'
-    end
+  def category_id
+    'having-a-baby'
+  end
+  helper_method :category_id
 end

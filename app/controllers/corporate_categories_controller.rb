4adc68ba9bc3e9a80362cd76d11f78920b142df0
Phil Lee <asmega@ph-lee.com>
2015-04-27 10:37:54 +0100

    corporate cetegories displays default first page

--- app/controllers/corporate_categories_controller.rb
+++ app/controllers/corporate_categories_controller.rb
@@ -1,6 +1,25 @@
 class CorporateCategoriesController < CategoriesController
   def show
     super
+
     @category = Core::CorporateCategory.new(@category.id, @category.attributes)
+    set_article
+  end
+
+  private
+
+  def article_interactor
+    Core::CorporateReader.new(default_article_id)
+  end
+
+  def set_article
+    @article = article_interactor.call do
+      not_found
+    end
+    @article = ContentItemDecorator.new(@article)
+  end
+
+  def default_article_id
+    category.contents.first.id
   end
 end

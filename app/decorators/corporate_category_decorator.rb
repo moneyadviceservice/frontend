07deb45114bb635aa44db0c8de22598390143e3f
Phil Lee <asmega@ph-lee.com>
2015-04-28 14:59:41 +0100

    corporate sub categories always use child partial

--- app/decorators/corporate_category_decorator.rb
+++ app/decorators/corporate_category_decorator.rb
@@ -2,26 +2,12 @@ class CorporateCategoryDecorator < CategoryDecorator
   delegate :third_level_navigation
 
   def render_contents
-    partial = if object.parent?
-                parent_partial_path
-              elsif object.child?
-                child_partial_path
-              end
-
-    h.render partial, contents: contents
+    h.render partial_path, contents: contents
   end
 
   private
 
-  def parent_partial_path
-    if self.object.third_level_navigation
-      'child_categories'
-    else
-      'categories/child_categories'
-    end
-  end
-
-  def child_partial_path
+  def partial_path
     if self.object.third_level_navigation
       'content_items'
     else

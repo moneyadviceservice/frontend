c6c4e37f4d25eb531d5fcbdb9bcff0a65213f7df
Phil Lee <asmega@ph-lee.com>
2015-04-28 17:28:43 +0100

    add paywall for debt evaluation toolkit

--- app/controllers/corporate_categories_controller.rb
+++ app/controllers/corporate_categories_controller.rb
@@ -1,5 +1,6 @@
 class CorporateCategoriesController < CategoriesController
   decorates_assigned :category, with: CorporateCategoryDecorator
+  before_action :paywall_debt_advice_evaluation_toolkit
 
   def show
     super
@@ -10,6 +11,12 @@ class CorporateCategoriesController < CategoriesController
   end
 
   private
+
+  def paywall_debt_advice_evaluation_toolkit
+    if params[:id] == 'debt-advice-evaluation-toolkit' && session[:agreement_token].blank?
+      redirect_to "/#{I18n.locale}/agreements/debt_evaluation_toolkit/new"
+    end
+  end
 
   def article_interactor
     Core::CorporateReader.new(default_article_id)

399b74fa1970940cdd38aac697ac4a83f0b39525
Phil Lee <asmega@ph-lee.com>
2015-04-29 11:28:54 +0100

    move corporate category to a method

--- app/controllers/corporate_controller.rb
+++ app/controllers/corporate_controller.rb
@@ -3,18 +3,22 @@ class CorporateController < ArticlesController
   decorates_assigned :category, with: CorporateCategoryDecorator
 
   def index
-    @category = Core::CategoryReader.new('corporate-home').call
+    @category = corporate_category
     assign_active_categories(@category)
   end
 
   def show
     super
-    @corporate_category = Core::CategoryReader.new('corporate-home').call
+    @corporate_category = corporate_category
     @category = @article.categories.last
     assign_active_categories(@corporate_category)
   end
 
   private
+
+  def corporate_category
+    @corporate_category ||= Core::CategoryReader.new('corporate-home').call
+  end
 
   def interactor
     Core::CorporateReader.new(params[:id])

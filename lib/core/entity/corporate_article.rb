c586efb95fcba30c5ffd587020d74f27df504791
Phil Lee <asmega@ph-lee.com>
2015-05-01 10:56:39 +0100

    only next/previous links go to corporate articles

--- lib/core/entity/corporate_article.rb
+++ lib/core/entity/corporate_article.rb
@@ -1,8 +1,22 @@
 module Core
   class CorporateArticle < Article
+    def previous_link
+      corporate_navigation_link 'previous_link'
+    end
+
+    def next_link
+      corporate_navigation_link 'next_link'
+    end
+
     private
 
-    def build_article_link(data)
+    def corporate_navigation_link(key)
+      return nil if related_content.nil? || related_content[key].blank?
+
+      build_corporate_article_link related_content[key]
+    end
+
+    def build_corporate_article_link(data)
       ArticleLink.new(data['title'], data['path'].sub('/articles/', '/corporate/'))
     end
   end

02ac69c46970705b18c1c4fbee14f8033fc086ca
David Kewal <david.kewal@gmail.com>
2015-05-21 13:10:49 +0100

    Feature spec refactor
    * Refactors the corporate page feature since flag is no longer being
    used
    * Refactors the corporate tool page object to make the corporate tool
    page feature pass

--- features/corporate_page.feature
+++ features/corporate_page.feature
@@ -3,11 +3,6 @@ Feature: Settings
   I want to be able to view more information on partnering
   So that I am aware of different ways of partnering with MAS
 
-  @wip @enable-corporate
   Scenario: Accessing the corporate page
     When  I visit the corporate page
     Then  I should be informed that I am on the correct page
-
-  Scenario: Corporate page is not accessible when feature is disabled
-    When I visit the corporate page
-    Then  I am told that the functionality is not implemented
--- features/support/ui/pages/corporate_tool_page.rb
+++ features/support/ui/pages/corporate_tool_page.rb
@@ -4,12 +4,12 @@ module UI::Pages
   class CorporateTool < UI::Page
     set_url '{/locale}/corporate/budget-planner-syndication'
 
-    element :organisation_name, "input[name='/[organisation]']"
-    element :organisation_email, "input[name='/[email]']"
-    element :tool_language, "input[name='/[lang]']"
-    element :tool_width_unit, "input[name='/[width-unit]']"
-    element :tool_width, "input[name='/[width]']"
+    element :organisation_name, "input[name='partner[name]']"
+    element :organisation_email, "input[name='partner[email]']"
+    element :tool_language, "#partner_tool_language_en"
+    element :tool_width_unit, "#partner_tool_width_unit_px"
+    element :tool_width, "input[name='partner[tool_width]']"
 
-    element :submit, "button[type='submit']"
+    element :submit, "input[type='submit']"
   end
 end

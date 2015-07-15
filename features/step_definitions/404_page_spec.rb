61930f046d93f3e2cab858d6591bddd631f9ff56
Jones Agyemang <mightyj@hotmail.co.uk>
2015-07-15 11:30:00 +0100

    remove unnecessary feature spec file

--- features/step_definitions/404_page_spec.rb
+++
@@ -1,7 +0,0 @@
-Given(/^that I visit a non\-existent link$/) do
-  visit '/en/bad'
-end
-
-Then(/^I should see a page not found message$/) do
-    pending # express the regexp above with the code you wish you had
-end

79573b8f5a107cfa27a6252d1120de547e748590
Phil Lee <asmega@ph-lee.com>
2015-07-28 10:31:46 +0100

    refactor interactors with inheritance

--- lib/core/interactor/corporate_reader.rb
+++ lib/core/interactor/corporate_reader.rb
@@ -1,19 +1,10 @@
 module Core
   class CorporateReader < ArticleReader
-    def call
-      data    = repository.find(id)
-      article = CorporateArticle.new(id, data)
-
-      if article.valid?
-        article.tap do |a|
-          a.categories = build_categories(a.categories)
-        end
-      elsif block_given?
-        yield
-      end
-    end
-
     private
+
+    def entity_class
+      CorporateArticle
+    end
 
     def repository
       Registry::Repository[:corporate]
--- lib/core/interactor/video_reader.rb
+++ lib/core/interactor/video_reader.rb
@@ -1,34 +1,13 @@
 module Core
-  class VideoReader
-    attr_reader :id
-
-    def initialize(id)
-      @id = id
-    end
-
-    def call
-      data  = repository.find(id)
-      video = Video.new(id, data)
-
-      if video.valid?
-        video.tap do |v|
-          v.categories = build_categories(v.categories)
-        end
-      elsif block_given?
-        yield
-      end
-    end
-
+  class VideoReader < BaseContentReader
     private
+
+    def entity_class
+      Video
+    end
 
     def repository
       Registry::Repository[:video]
-    end
-
-    def build_categories(category_ids)
-      category_ids.map do |category_id|
-        CategoryReader.new(category_id).call
-      end
     end
   end
 end

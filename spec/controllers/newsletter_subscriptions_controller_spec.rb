0a098e7c0346408d93e7cffe0c42dcec947c110d
Jones Agyemang <mightyj@hotmail.co.uk>
2015-08-07 09:44:28 +0100

    fix spec add template to subscription params

--- spec/controllers/newsletter_subscriptions_controller_spec.rb
+++ spec/controllers/newsletter_subscriptions_controller_spec.rb
@@ -9,34 +9,34 @@ RSpec.describe NewsletterSubscriptionsController, type: :controller do
   describe 'XHR POST create' do
     context 'with valid email' do
       let(:email) { { email: 'clark.kent@example.com' } }
+      let(:params) { { email: 'clark.kent@example.com', category: 'footer' } }
       let(:result) { true }
 
       it 'instantiates a subscriber' do
         expect(Core::NewsletterSubscriptionCreator).to receive(:new).with(email[:email]) { subscriber }
-
-        xhr :post, :create, locale: I18n.locale, subscription: email
+        xhr :post, :create, locale: I18n.locale, subscription: params
       end
 
       it 'assigns result to an instance variable' do
-        xhr :post, :create, locale: I18n.locale, subscription: email
+        xhr :post, :create, locale: I18n.locale, subscription: params
 
         expect(assigns(:success)).to eq(result)
       end
 
       it 'is successful' do
-        xhr :post, :create, locale: I18n.locale, subscription: email
+        xhr :post, :create, locale: I18n.locale, subscription: params
 
         expect(response).to be_ok
       end
 
       it 'renders the right template' do
-        xhr :post, :create, locale: I18n.locale, subscription: email
+        xhr :post, :create, locale: I18n.locale, subscription: params
 
-        expect(response).to render_template('show')
+        expect(response).to render_template('footer')
       end
 
       it 'sets session variable' do
-        xhr :post, :create, locale: I18n.locale, subscription: email
+        xhr :post, :create, locale: I18n.locale, subscription: params
 
         expect(session[:newsletter_subscribed]).to be_truthy
       end
@@ -48,22 +48,23 @@ RSpec.describe NewsletterSubscriptionsController, type: :controller do
 
     context 'with valid email' do
       let(:email) { { email: 'clark.kent@example.com' } }
+      let(:params) { { email: 'clark.kent@example.com', category: 'footer' } }
       let(:result) { true }
 
       it 'adds a flash success message' do
-        post :create, locale: I18n.locale, subscription: email
+        post :create, locale: I18n.locale, subscription: params
 
         expect(request.flash[:success]).to include I18n.t('newsletter_subscriptions.success')
       end
 
       it 'sets session variable' do
-        post :create, locale: I18n.locale, subscription: email
+        post :create, locale: I18n.locale, subscription: params
 
         expect(session[:newsletter_subscribed]).to be_truthy
       end
 
       it 'redirects back' do
-        post :create, locale: I18n.locale, subscription: email
+        post :create, locale: I18n.locale, subscription: params
 
         expect(response).to redirect_to('example.com')
       end
@@ -71,22 +72,23 @@ RSpec.describe NewsletterSubscriptionsController, type: :controller do
 
     context 'with invalid email' do
       let(:email) { { email: 'clark.kent@dailyplanet' } }
+      let(:params) { { email: 'clark.kent@example.com', category: 'footer' } }
       let(:result) { false }
 
       it 'adds a flash error message' do
-        post :create, locale: I18n.locale, subscription: email
+        post :create, locale: I18n.locale, subscription: params
 
         expect(request.flash[:error]).to include I18n.t('newsletter_subscriptions.error')
       end
 
       it 'does not set session variable' do
-        post :create, locale: I18n.locale, subscription: email
+        post :create, locale: I18n.locale, subscription: params
 
         expect(session[:newsletter_subscribed]).to be_falsey
       end
 
       it 'redirects back' do
-        post :create, locale: I18n.locale, subscription: email
+        post :create, locale: I18n.locale, subscription: params
 
         expect(response).to redirect_to('example.com')
       end

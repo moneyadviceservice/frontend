67ff270300d8700626069f6cbc59ee61f28ad81d
Phil Lee <asmega@ph-lee.com>
2015-09-22 11:50:32 +0100

    implement locale link for christmas planner

--- app/controllers/christmas_money_planner_controller.rb
+++ app/controllers/christmas_money_planner_controller.rb
@@ -1,14 +1,25 @@
 class ChristmasMoneyPlannerController < EmbeddedToolsController
-  
-  include SuppressMenuButton
-  
   def show
-    @breadcrumbs = BreadcrumbTrail.home
-    render layout: '_unconstrained'
+  end
+
+  protected
+
+  def breadcrumbs
+    BreadcrumbTrail.home
+  end
+
+  def alternate_url
+    alternate_locale = mount_point.alternate_locale(I18n.locale.to_s)
+    alternate_tool_id = mount_point.public_send("#{alternate_locale}_id")
+
+    "/#{alternate_locale}/tools/#{alternate_tool_id}"
   end
 
   def category_id
     'managing-money'
   end
 
+  def mount_point
+    @mount_point ||= ToolMountPoint::ChristmasMoneyPlanner.new
+  end
 end
---
+++ lib/tool_mount_point/christmas_money_planner.rb
@@ -0,0 +1,6 @@
+module ToolMountPoint
+  class ChristmasMoneyPlanner < Base
+    EN_ID = 'christmas-money-planner'
+    CY_ID = 'cynllunydd-ariannol-y-nadolig'
+  end
+end

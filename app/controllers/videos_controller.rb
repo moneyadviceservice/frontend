6cbb562f19213a483bb08bfe88828e358fa1034b
Phil Lee <asmega@ph-lee.com>
2015-09-25 14:42:32 +0100

    allow videos to be redirected

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -5,8 +5,12 @@ class VideosController < ApplicationController
   include Navigation
 
   def show
-    @video = interactor.call do
-      not_found
+    @video = interactor.call do |error|
+      if error.redirect?
+        return redirect_to error.location, status: error.status
+      else
+        not_found
+      end
     end
 
     set_related_content
--- lib/core/entity/video.rb
+++ lib/core/entity/video.rb
@@ -3,5 +3,9 @@ module Core
     attr_accessor :type, :title, :description, :body, :categories
 
     validates_presence_of :title, :body
+
+    def redirect?
+      false
+    end
   end
 end

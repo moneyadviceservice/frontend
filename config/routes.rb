ef714ce54fd46d23668b0fa424916b97a95496f6
John Player <john.player@moneyadvicservice.org.uk>
2015-10-01 14:49:11 +0100

    Adding frontend canonical tags

--- config/routes.rb
+++ config/routes.rb
@@ -178,6 +178,8 @@ Rails.application.routes.draw do
       get 'export-partners', on: :collection
     end
 
+    resources :corporate, only: [:show], as: 'corporate_articles'
+
     resources :campaigns,
               only: 'show',
               path: 'campaigns',

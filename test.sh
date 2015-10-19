62bad742cd531031d8f334039d3ecc4a5974c97b
pmcostello <pmcostello@hotmail.co.uk>
2015-10-19 18:21:06 +0100

    pipeline label change

--- test.sh
+++ test.sh
@@ -9,7 +9,7 @@ export BUNDLE_WITHOUT=development
 CORES=$(grep -c ^processor /proc/cpuinfo 2>/dev/null || sysctl -n hw.ncpu)
 BUNDLE_JOBS=$((CORES-1))
 
-CI_PIPELINE_COUNTER=${GO_PIPELINE_LABEL-0}
+CI_PIPELINE_COUNTER=${GO_PIPELINE_COUNT-0}
 CI_EXECUTOR_NUMBER=${EXECUTOR_NUMBER-0}
 
 # remove prior dirty packaged gems e.g. build.sh

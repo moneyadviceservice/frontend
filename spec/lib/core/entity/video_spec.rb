2e9f1c31623a82fac9f0fda3b2c398e04eb523e5
Phil Lee <asmega@ph-lee.com>
2015-11-16 16:52:33 +0000

    add basic spec for video entity

---
+++ spec/lib/core/entity/video_spec.rb
@@ -0,0 +1,27 @@
+RSpec.describe Core::Video do
+  let(:params) do
+    { type: 'no-idea-about-type',
+      title: 'awesome-title',
+      description: 'awesome-description',
+      body: 'awesome-body',
+      categories: [:foo, :bar] }
+  end
+
+  subject do
+    described_class.new('some-video', params)
+  end
+
+  describe 'attributes' do
+    it 'are set' do
+      %i{ type title description body categories }.each do |attr|
+        expect(subject.public_send(attr)).to eql(params[attr])
+      end
+    end
+  end
+
+  describe '#redirect?' do
+    it 'returns false' do
+      expect(subject.redirect?).to be_falsey
+    end
+  end
+end

bb40e47dd978c8841d7c66809cb93396b52c9144
Jones Agyemang <mightyj@hotmail.co.uk>
2016-01-07 15:19:10 +0000

    fix before go live date spec issue

--- spec/helpers/survive_january_spec.rb
+++ spec/helpers/survive_january_spec.rb
@@ -2,7 +2,11 @@ RSpec.describe SurviveJanuary, type: :helper do
   describe '#go_live?' do
     context 'before go live date' do
       it 'should be false' do
-        expect(display_campaign_promo?).to be false
+        before_go_live_date = Chronic.parse('26/12/2015')
+
+        Timecop.freeze(before_go_live_date) do
+          expect(display_campaign_promo?).to be false
+        end
       end
     end
 

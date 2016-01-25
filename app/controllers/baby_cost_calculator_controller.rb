5afc3b09202d46b3635fff3f68b642d98c531145
Rick Jones <rick.jones@playtimestudios.com>
2016-01-25 17:49:25 +0000

    6958 Remove Baby Cost Calc

--- app/controllers/baby_cost_calculator_controller.rb
+++ app/controllers/baby_cost_calculator_controller.rb
@@ -1,4 +1,10 @@
 class BabyCostCalculatorController < EmbeddedToolsController
+  before_action :deny_non_syndicates
+
+  def deny_non_syndicates
+    redirect_to main_app.article_path(I18n.t('controllers.baby_cost_calculator.deny_non_syndicates.article_id')), status: 301 unless syndicated_tool_request?
+  end
+
   protected
   def category_id
     'having-a-baby'

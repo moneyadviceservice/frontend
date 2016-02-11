68267af5bd9cf021b0c935ddc2c8daa8acc72e4d
Jones Agyemang <mightyj@hotmail.co.uk>
2016-02-11 15:20:48 +0000

    render promo banner text in markdown

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -4,4 +4,15 @@ module HomeHelper
       map["#{locale}-GB"] = root_url(locale: locale)
     end
   end
+
+  def markdown(text)
+    renderer = Redcarpet::Render::HTML.new
+    document = Redcarpet::Markdown.new(renderer).render(text)
+    remove_p_tags_from(document).html_safe
+  end
+
+  private
+  def remove_p_tags_from(document)
+    Regexp.new(/\A<p>(.*)<\/p>\Z/m).match(document)[1] rescue document
+  end
 end

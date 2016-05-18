aad79e768ea790845510a78f412e657c476d47c0
David Trussler <mail@davidtrussler.net>
2016-05-18 16:06:30 +0100

    TP: 7334, Comment: Removes the Sticky Newsletter Sign-up section

--- app/controllers/cookie_dismissal_controller.rb
+++
@@ -1,6 +0,0 @@
-class CookieDismissalController < ApplicationController
-  def create
-    cookies[COOKIE_DISMISS_NEWSLETTER_NAME] = { value: COOKIE_HIDE_NEWSLETTER_VALUE, expires: 1.month.from_now }
-    head :ok
-  end
-end

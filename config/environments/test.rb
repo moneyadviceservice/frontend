127454e4f28b023fc90dd90047abe747928b849b
Jon Gilbraith <jon@hobocode.io>
2016-10-03 17:29:26 +0100

    Address Rails 4.2 changes and deprecation warnings

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -21,7 +21,7 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :test
 
   # Configure static asset server for tests with Cache-Control for performance.
-  config.serve_static_assets  = true
+  config.serve_static_files  = true
   config.static_cache_control = 'public, max-age=3600'
 
   # Show full error reports and disable caching.

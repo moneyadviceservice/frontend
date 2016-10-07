4479210c5e53dc78761bf94f0035dd559e3742a6
Jones Agyemang <mightyj@hotmail.co.uk>
2016-10-07 10:10:28 +0100

    remove dynamic rendering of campaign paths

--- app/models/campaign_page/campaign.rb
+++ app/models/campaign_page/campaign.rb
@@ -2,6 +2,31 @@ module CampaignPage
   class Campaign
     include Enumerable
 
+    WHITELIST= %w(
+      cmo
+      coping-with-unexpected-bills
+      csa
+      borrowing-get-the-facts
+      budgeting-to-get-through-january
+      free-debt-advice
+      friends-life-lander
+      get-set-for-summer
+      interest-only-mortgages
+      life-and-critical-illness
+      paying-too-much-tax-on-savings
+      save-gbp3-a-day-for-emergencies
+      saving-for-a-holiday
+      start-living-your-life-free-of-debt
+      student-budgeting
+      sw-saving-and-debt
+      the-cost-of-caring
+      the-true-cost-of-affording-a-home
+      the-true-cost-of-borrowing
+      uk-money-habits-study
+      what-does-ma-think
+      young-peoples-money-regrets
+    ).freeze
+
     attr_accessor :name, :sections, :cost_calculator_link
     private :name=, :sections=, :cost_calculator_link=
 
--- spec/controllers/campaigns_controller_spec.rb
+++ spec/controllers/campaigns_controller_spec.rb
@@ -1,32 +1,60 @@
 RSpec.describe CampaignsController, type: :controller do
   describe 'GET show' do
-    let(:id) { 'revealed-the-true-cost-of-buying-a-car' }
     let(:template) { double }
 
-    context 'if there are no custom template' do
-      before { allow(Template).to receive(:new) { template } }
+    %w(
+       revealed-the-true-cost-of-buying-a-car
+       how-to-look-ahead-when-buying-a-car
+      ).each do |id|
+      context 'if there are no custom template' do
+        before { allow(Template).to receive(:new) { template } }
 
-      it 'builds a campaing' do
-        expect(template).to receive(:build_campaign).with(id)
+        it 'builds a campaign' do
+          expect(template).to receive(:build_campaign).with(id)
+          get :show, locale: I18n.locale, id: id
+        end
 
-        get :show, locale: I18n.locale, id: id
-      end
-
-      it 'is successful' do
-        allow(template).to receive(:build_campaign) { double }
-
-        get :show, locale: I18n.locale, id: id
-
-        expect(response).to be_ok
+        it 'is successful' do
+          allow(template).to receive(:build_campaign) { double }
+          get :show, locale: I18n.locale, id: id
+          expect(response).to be_ok
+        end
       end
     end
 
-    context 'if there are a custom template' do
-      let(:campaign_page_id) { 'life-and-critical-illness' }
-      subject { get :show, locale: I18n.locale, id: campaign_page_id }
+    %w(
+       borrowing-get-the-facts
+       budgeting-to-get-through-january
+       cmo
+       coping-with-unexpected-bills
+       csa
+       free-debt-advice
+       friends-life-lander
+       get-set-for-summer
+       interest-only-mortgages
+       life-and-critical-illness
+       paying-too-much-tax-on-savings
+       save-gbp3-a-day-for-emergencies
+       saving-for-a-holiday
+       start-living-your-life-free-of-debt
+       student-budgeting
+       sw-saving-and-debt
+       the-cost-of-caring
+       the-true-cost-of-affording-a-home
+       the-true-cost-of-borrowing
+       uk-money-habits-study
+       what-does-ma-think
+       young-peoples-money-regrets
+       ).each do |campaign_page_id|
+      context 'if there are custom templates' do
 
-      it 'renders the custom template' do
-        expect(subject).to render_template("campaigns/#{campaign_page_id.underscore}")
+        before :each do
+          get campaign_page_id.underscore.to_sym, locale: I18n.locale
+        end
+
+        it 'renders the custom template' do
+          expect(response).to render_template("campaigns/#{campaign_page_id.underscore}")
+        end
       end
     end
   end

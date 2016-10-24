0a82038513b3489f29eb712b30fb9e8197ffe228
John Player <johnplayer1982@msn.com>
2016-10-24 19:50:38 +0100

    7708 - Adding likes / dislikes count from AJAX response

--- app/assets/javascripts/components/OnPageFeedback.js
+++ app/assets/javascripts/components/OnPageFeedback.js
@@ -11,6 +11,8 @@ define(['jquery', 'DoughBaseComponent'], function($, DoughBaseComponent) {
     this.shareBtns = $el.find('[data-dough-feedback-share] .social-sharing__item__icon');
     this.submitBtn = $el.find('[data-dough-feedback-submit]');
     this.ajaxUrl = $el.attr('data-dough-on-page-feedback-post');
+    this.likeCount = $el.find('[data-dough-feedback-like-count]');
+    this.dislikeCount = $el.find('[data-dough-feedback-dislike-count]');
   };
 
   DoughBaseComponent.extend(OnPageFeedback);
@@ -20,25 +22,24 @@ define(['jquery', 'DoughBaseComponent'], function($, DoughBaseComponent) {
     var isLiked = (interaction === 'positive') ? true : false;
     
     var postObject = {
-        liked: isLiked
-      };
-      
-      $.post(this.ajaxUrl, postObject, function(data){
-        this._showPage(interaction);
-      }.bind(this))
-      .fail(function() {
-        console.log('SOMETHING WENT WRONG');
-      }.bind(this));
+      liked: isLiked
+    };
+
+    $.post(this.ajaxUrl, postObject, function(data){
+      this._showPage(interaction);
+      this._updateCount(data);
+    }.bind(this))
+    .fail(function() {
+      console.log('SOMETHING WENT WRONG');
+    }.bind(this));
 
   };
 
-  OnPageFeedback.prototype._share = function() {
-    // TODO: Do we need to track 'shares'? I can't remember.
-
+  OnPageFeedback.prototype._share = function(share) {
     var submitShare = $.ajax({
       url: this.ajaxUrl,
       type: 'PATCH',
-      data: {'shared_on': 'Twitter'}
+      data: {'shared_on': share}    
     });
 
     submitShare.fail(function(status){
@@ -49,23 +50,25 @@ define(['jquery', 'DoughBaseComponent'], function($, DoughBaseComponent) {
   };
 
   OnPageFeedback.prototype._submitComment = function() {
-    // TODO: another AJAX call here with the comment
-
-    var userComment = $('[data-dough-feedback-comment]').val();
-
-    var submitFeedback = $.ajax({
-      url: this.ajaxUrl,
-      type: 'PATCH',
-      data: {'comment': userComment}
-    });
-
-    console.log(this.ajaxUrl);
+    var userComment = $('[data-dough-feedback-comment]').val(),
+        submitFeedback = $.ajax({
+          url: this.ajaxUrl,
+          type: 'PATCH',
+          data: {'comment': userComment}
+        });
 
     submitFeedback.fail(function(status){
       console.log('Failed to send feedback: "' + userComment + '"');
     });
 
     this._showPage('results');
+  };
+
+  OnPageFeedback.prototype._updateCount = function(data) {
+    var countResponse = data;
+    
+    this.likeCount.text(countResponse.likes_count);
+    this.dislikeCount.text(countResponse.dislikes_count);
   };
 
   OnPageFeedback.prototype._showPage = function(pageName) {
@@ -79,7 +82,11 @@ define(['jquery', 'DoughBaseComponent'], function($, DoughBaseComponent) {
       e.preventDefault();
       self._submitInteraction($(this).attr('data-dough-feedback'));
     });
-    this.shareBtns.on('click', $.proxy(this._share, this));
+    // this.shareBtns.on('click', $.proxy(this._share, this));
+    this.shareBtns.on('click', function(e) {
+      e.preventDefault();
+      self._share($(this).attr('data-dough-shared-on'));
+    });
     this.submitBtn.on('click', $.proxy(this._submitComment, this));
   };
 

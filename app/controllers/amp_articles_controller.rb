31b85d2694c59cfbf72554c8550425de23398465
Stephen Haley <stephen.haley@cultivatehq.com>
2016-10-26 10:34:03 +0100

    Create AmpArticle Controller

---
+++ app/controllers/amp_articles_controller.rb
@@ -0,0 +1,22 @@
+class AmpArticlesController < ActionController::Base
+  decorates_assigned :article, with: ContentItemDecorator
+  before_action :retrieve_article
+
+  private
+
+  # This has come from Application controller.  We need to split this stuff out into a separate mixin
+  def not_found
+    fail ActionController::RoutingError.new('Not Found')
+  end
+
+  # Borrowed from ArticlesController.  Should be re-factored.
+  def retrieve_article
+    @article ||= Core::ArticleReader.new(params[:article_id]).call do |error|
+      if error.redirect?
+        return redirect_to error.location, status: error.status
+      else
+        not_found
+      end
+    end
+  end
+end

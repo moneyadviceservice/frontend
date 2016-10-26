1b97bf0d529eb2bb43ebcb1b1422c68c9bda0ad9
Stephen Haley <stephen.haley@cultivatehq.com>
2016-10-26 10:34:03 +0100

    Create AmpArticle Controller

--- config/routes.rb
+++ config/routes.rb
@@ -113,6 +113,7 @@ Rails.application.routes.draw do
 
     resources :action_plans, only: 'show'
     resources :articles, only: 'show' do
+      resource :amp, only: [:show], controller: :amp_articles
       resource :feedback, only: [:new, :create], controller: :article_feedbacks
     end
 

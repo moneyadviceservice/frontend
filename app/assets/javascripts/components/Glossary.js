d99d13bce6ab03a8ce433d121af339b92f1d5bee
John Player <johnplayer1982@msn.com>
2016-11-15 16:17:20 +0000

    Adding real glossary terms and regex excludes a tags and titles

--- app/assets/javascripts/components/Glossary.js
+++ app/assets/javascripts/components/Glossary.js
@@ -8,28 +8,40 @@ define(['jquery', 'DoughBaseComponent'], function($, DoughBaseComponent) {
     Glossary.baseConstructor.call(this, $el, config);
 
     var article = $('[data-dough-glossary-content]'),
-    articleContent = article.text();
+    articleContent = $(article).html();
 
     $.getJSON('/glossary.json', function(data){
       var glossaryTerms = data.glossary.map(function (item) {
+
         // If the article content contains any of the keywords in the JSON
         if(articleContent.indexOf(item.term) >= 1){
-          var updatedContent = $(article).html().replace(RegExp("\\b" + item.term + "\\b","gi"), "<button class='unstyled-button glossary__term' aria-label='" + item.term + " (see description)'>" + item.term + "</button><span class='glossary__description'><span class='glossary__description-title'>" + item.term + "</span>" + item.description + "<button class='glossary__description-close'>close</button></span>");
+          var updatedContent = articleContent.replace(RegExp('(?!<.*?)\\b(' + item.term + ')\\b(?![^<>]*?(<\/a>|>))(?![^<>]*?(<\/h2>|>))(?![^<>]*?(<\/h3>|>))',"gi"), "<button class='unstyled-button glossary__term' aria-label='" + item.term + " (see description)'>" + item.term + "</button><span class='glossary__description'><span class='glossary__description-title'>" + item.term + "</span>" + item.description + "<button class='glossary__description-close'>close</button></span>");
           $(article).html(updatedContent);
+          console.log(item.term + ' found in this article')
         } else {
           // For dev purposes
-          console.log(item.term + ' has not been found in this article')
+          // console.log(item.term + ' has not been found in this article')
         };
+
       });
 
       // Show descriptions
       $('.glossary__term').on('click', function(){
-        $(this).next('.glossary__description').toggle().focus();
+        event.stopPropagation();
+        $(this).next('.glossary__description').addClass('glossary__description--active');
       });
 
-      // Close descriptions
+      // Close descriptions using button
       $('.glossary__description-close').on('click', function(){
-        $(this).parents('.glossary__description').hide();
+        $(this).parents('.glossary__description').removeClass('glossary__description--active');
+        console.log('closed');
+      });
+
+      // Close descriptions by clicking outside description element
+      $('body').click(function() {
+        if(!$(this.target).is('.glossary__description')) {
+          $(".glossary__description").removeClass('glossary__description--active');
+        }
       });
 
     });

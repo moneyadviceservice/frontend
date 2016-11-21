2ff791ec506ecb6bc5a39febe8da174eac845b11
David Trussler <mail@davidtrussler.net>
2016-11-21 16:31:08 +0000

    TP: 7723, Comment: Updates SW to use assets from CDN

--- public/sw.js
+++ public/sw.js
@@ -1,3 +1,8 @@
+var urlsToCache = [
+  'en/offline_page',
+  'cy/offline_page'
+];
+
 // When the SW is installed, cache the assets required to display the offlinePage content
 self.addEventListener('install', function(event) {
   event.waitUntil(
@@ -7,42 +12,51 @@ self.addEventListener('install', function(event) {
 
 // Test for network and use response if available plus cache assets, otherwise use offlinePage content
 self.addEventListener('fetch', function(event) {
+  if (event.request.url.indexOf('logo-sprite-en') != -1) {
+    urlsToCache.push(event.request.url);
+    caches.open('mas-offline-page').then(function(cache) {
+      cache.add(event.request.url);
+    })
+  } else if (event.request.url.indexOf('logo-sprite-cy') != -1) {
+    urlsToCache.push(event.request.url);
+    caches.open('mas-offline-page').then(function(cache) {
+      cache.add(event.request.url);
+    })
+  } else if (event.request.url.indexOf('basic') != -1) {
+    urlsToCache.push(event.request.url);
+    caches.open('mas-offline-page').then(function(cache) {
+      cache.add(event.request.url);
+    })
+  } else if (event.request.url.indexOf('enhanced_responsive') != -1) {
+    urlsToCache.push(event.request.url);
+    caches.open('mas-offline-page').then(function(cache) {
+      cache.add(event.request.url);
+    })
+  }
+
   event.respondWith(
     fetch(event.request).then(function(response) {
-      caches.open('mas-offline-page').then(function(cache) {
-        return caches.match(event.request);
-      })
-
       return response
     }).catch(function() {
       // send response from the cache
       if (event.request.url.match(/\/en$/) || event.request.url.match(/\/en\//)) {
         return caches.match('/en/offline_page');
-      } else if (event.request.url.match(/\/assets\/logo-sprite-en.png/)) {
-        return caches.match('/assets/logo-sprite-en.png');
       } else if (event.request.url.match(/\/cy$/) || event.request.url.match(/\/cy\//)) {
         return caches.match('/cy/offline_page');
-      } else if (event.request.url.match(/\/assets\/logo-sprite-cy.png/)) {
-        return caches.match('/assets/logo-sprite-cy.png');
-      } else if (event.request.url.match(/\/assets\/dough\/assets\/stylesheets\/basic.css/)) {
-        return caches.match('/assets/dough/assets/stylesheets/basic.css');
-      } else if (event.request.url.match(/\/assets\/enhanced_responsive.css/)) {
-        return caches.match('/assets/enhanced_responsive.css');
+      } else if (event.request.url.match(/logo-sprite-en/)) {
+        return caches.match(event.request.url);
+      } else if (event.request.url.match(/logo-sprite-cy/)) {
+        return caches.match(event.request.url);
+      } else if (event.request.url.match(/basic/)) {
+        return caches.match(event.request.url);
+      } else if (event.request.url.match(/enhanced_responsive/)) {
+        return caches.match(event.request.url);
       }
     })
   );
 });
 
 function cacheAssets() {
-  var urlsToCache = [
-    '/assets/logo-sprite-en.png',
-    '/assets/logo-sprite-cy.png',
-    '/assets/dough/assets/stylesheets/basic.css',
-    '/assets/enhanced_responsive.css',
-    'en/offline_page',
-    'cy/offline_page'
-  ];
-
   // open a cache named 'mas-offline-page'
   caches.open('mas-offline-page').then(function(cache) {
     // add the urls in urlsToCache to opened cache

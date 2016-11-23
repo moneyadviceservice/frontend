8654050e0f3154cc4fd13efce6275eb44fdf8b9e
Jon Gilbraith <jon@hobocode.io>
2016-11-23 09:51:34 +0000

    Remove the article feedback email mechanism.

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -42,7 +42,6 @@ Rails.application.configure do
   # Custom configuration options for feedback settings
   config.feedback_delivery_method = :test
   config.raise_feedback_delivery_errors = false
-  config.article_feedback_email   = 'content.feedback@moneyadviceservice.org.uk'
   config.technical_feedback_email = 'matt.lucht@moneyadviceservice.org.uk'
 
   # Missing translations should raise exceptions

9d6afbb583d70bc2141dc02a8ce1acdaa50cf513
Jon Gilbraith <jon@hobocode.io>
2016-11-23 10:31:39 +0000

    Remove the cms_home_page feature toggle
    Permanently enable CMS based homepage, remove static one

--- spec/requests/home_spec.rb
+++ spec/requests/home_spec.rb
@@ -1,21 +1,23 @@
 RSpec.describe 'HomePage', type: :request do
   let(:category_repository)  { Core::Repository::Categories::Fake.new }
   let(:footer_repository)    { FakeFooterRepositoryDefinedInSpecHelper.new }
-  let(:home_page_repository) { Core::Repository::HomePages::Static.new }
+  let(:home_page_repository) { Core::Repository::HomePages::CMS.new }
 
   before do
     allow(Core::Registry::Repository).to receive(:[]).with(:home_page).and_return(home_page_repository)
     allow(Core::Registry::Repository).to receive(:[]).with(:footer).and_return(footer_repository)
     allow(Core::Registry::Repository).to receive(:[]).with(:category).and_return(category_repository)
+
+    VCR.use_cassette('/en/core/repository/home_pages/cms') do
+      get '/en'
+    end
   end
 
   it "successfully renders" do
-    get '/en'
     expect(response.status).to eq(200)
   end
 
   it 'displays footer content' do
-    get '/en'
     expect(response.body).to include('I am some content in a footer')
   end
 end

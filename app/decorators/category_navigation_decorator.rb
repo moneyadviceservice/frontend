bc13ab06f46609486b53f7b272db4b3f8d4d09b7
Jerome Riga <jerome.riga@moneyadviceservice.org.uk>
2016-12-08 13:12:27 +0000

    Cleaned spike code

--- app/decorators/category_navigation_decorator.rb
+++ app/decorators/category_navigation_decorator.rb
@@ -18,10 +18,10 @@ class CategoryNavigationDecorator < Draper::Decorator
   end
 
   def left_nav_items
-     data = object.content.contents.try(:object) || object.content.legacy_contents
-     Core::CategoryReader.new(1).build_contents(data.object).map! do |item|
-       CategoryContentDecorator.new(item)
-     end
+    data = category.contents || category.legacy_contents
+    Core::CategoryReader.new(1).build_contents(data.object).map! do |item|
+      CategoryContentDecorator.new(item)
+    end
   end
 
   private

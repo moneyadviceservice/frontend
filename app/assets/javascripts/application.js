cd670bc6ad31c1888625d12dc753d83505def260
David Trussler <mail@davidtrussler.net>
2016-12-12 09:59:06 +0000

    TP: 7772, Comment: Refines mobile layout

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -36,6 +36,8 @@ require(['common', 'jquery'], function(MAS, $) {
   require(['jquery', 'collapsable'], function($, Collapsable) {
     $(document).ready(function() {
 
+      // Temporary change to test Global Nav styling --
+      /*
       $('#primary-nav')
         .clone()
         .attr('id', 'js-primary-nav')
@@ -43,7 +45,39 @@ require(['common', 'jquery'], function(MAS, $) {
         .wrap('<div class="l-menu-nav"></div>');
 
       $('.mobile-nav__link--menu').attr('href', '#js-primary-nav');
-      
+
+      // Mobile Nav
+      new Collapsable({
+        name: 'mobileNav',
+        closeOffFocus: false,
+        accordion: true,
+        triggerEl: '.mobile-nav a',
+        targetType: 'href',
+        parentWrapper: '#js-primary-nav'
+      });
+      */
+
+      $('.mobile-nav__link--menu').click(function() {
+        if ($('.global-nav').hasClass('global-nav--active')) {
+          $('.global-nav').removeClass('global-nav--active');
+        } else {
+          $('.global-nav').addClass('global-nav--active');
+        }
+      });
+
+      $('.global-nav__clump__heading').click(function() {
+        $(this)
+          .parents('.global-nav__clump').addClass('global-nav__clump--active')
+          .parents('.global-nav__clumps').addClass('global-nav__clumps--active');
+      });
+
+      $('.global-nav__clump__content__heading').click(function() {
+        $(this)
+          .parents('.global-nav__clump').removeClass('global-nav__clump--active')
+          .parents('.global-nav__clumps').removeClass('global-nav__clumps--active');
+      })
+      // -- Temporary change to test Global Nav styling
+
       // Article Collapsables
       new Collapsable({
         name: 'articleCollapsables',

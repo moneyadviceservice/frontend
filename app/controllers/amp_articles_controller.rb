6d52116cb1eca0252a5e1ad4649eeb635707b452
Stephen Haley <stephen.haley@cultivatehq.com>
2016-12-14 14:52:11 +0000

    Add amp route and page
    This adds a `/amp` route to the end of each article url.  It also adds
    amp specific styling, images.
    
    If an article has been blacklisted the controller will redirect back to
    the main article view

---
+++ app/controllers/amp_articles_controller.rb
@@ -0,0 +1,24 @@
+class AmpArticlesController < ActionController::Base
+  protect_from_forgery with: :exception
+
+  include NotFound
+
+  decorates_assigned :article, with: AmpArticleDecorator
+  before_action :retrieve_article
+
+  def show
+    redirect_to url_for(action: 'show', controller: 'articles', id: @article.id, only_path: false) unless @article.supports_amp || params[:redirect]
+  end
+
+  private
+
+  def retrieve_article
+    @article ||= Core::ArticleReader.new(params[:article_id]).call do |error|
+      if error.redirect?
+        return redirect_to error.location, status: error.status
+      else
+        not_found
+      end
+    end
+  end
+end

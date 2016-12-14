bc3cec6f8fa0da104974512c7b1359c7c2f8a1d4
Stephen Haley <stephen.haley@cultivatehq.com>
2016-12-14 10:52:05 +0000

    Update Metatags gem to point to main release

--- config/initializers/meta_tags.rb
+++ config/initializers/meta_tags.rb
@@ -1,1 +1,6 @@
-MetaTags.truncate_description_at_length = 500
+MetaTags.configure do |c|
+  c.title_limit        = 500
+  c.description_limit  = 500
+  c.keywords_limit     = 255
+  c.keywords_separator = ', '
+end

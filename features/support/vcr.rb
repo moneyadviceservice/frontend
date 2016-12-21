918519fdbda1f5e9c57285c737faaae6b9ef93e9
Jon Gilbraith <jon@hobocode.io>
2016-12-21 14:52:38 +0000

    Refactor the strategy for VCR cassettes for cukes

--- features/support/vcr.rb
+++ features/support/vcr.rb
@@ -9,6 +9,31 @@ VCR.configure do |c|
     # Phantomjs to load (http://localhost:<random port>/__identify__)
     req.uri =~ /\/__identify__$|newrelic/
   end
+
+  c.around_http_request do |request|
+    uri = URI(request.uri)
+    cassette_name = if ENV['MAS_CMS_URL'] =~ /#{uri.host}/
+      "/CMS/#{request.method}#{uri.path}#{uri.query}"
+
+    elsif uri.host =~ /googleapis.com/
+      params = CGI.parse(uri.query)
+      params['key'] = ['GOOGLE_API_KEY']
+      params['cx'].first.gsub!(/#{ENV['GOOGLE_API_CX_EN']}/, 'GOOGLE_API_CX_EN')
+      params['cx'].first.gsub!(/#{ENV['GOOGLE_API_CX_CY']}/, 'GOOGLE_API_CX_CY')
+
+      "/GOOGLE_SEARCH/#{request.method}/#{params.to_query}"
+
+    else
+      "/#{uri.host}/#{request.method}#{uri.path}#{uri.query}"
+    end
+
+    if VCR.current_cassette && VCR.current_cassette.name == cassette_name
+      request.proceed
+    else
+      VCR.use_cassette(cassette_name, &request)
+    end
+  end
+
   c.filter_sensitive_data('<GOOGLE_API_KEY>') { ENV['GOOGLE_API_KEY'] }
   c.filter_sensitive_data('<GOOGLE_API_CX_EN>') { ENV['GOOGLE_API_CX_EN'] }
   c.filter_sensitive_data('<GOOGLE_API_CX_CY>') { ENV['GOOGLE_API_CX_CY'] }

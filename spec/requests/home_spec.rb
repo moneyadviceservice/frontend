b5e18afa04d1c62728eeded69210bb5da5598b62
Jon Gilbraith <jon@hobocode.io>
2016-12-22 17:28:09 +0000

    Stub the clumps repository in the home page request spec

--- spec/requests/home_spec.rb
+++ spec/requests/home_spec.rb
@@ -2,11 +2,13 @@ RSpec.describe 'HomePage', type: :request do
   let(:category_repository)  { Core::Repository::Categories::Fake.new }
   let(:footer_repository)    { FakeFooterRepositoryDefinedInSpecHelper.new }
   let(:home_page_repository) { Core::Repository::HomePages::CMS.new }
+  let(:clumps_repository)    { Core::Repository::Clumps::CMS.new }
 
   before do
     allow(Core::Registry::Repository).to receive(:[]).with(:home_page).and_return(home_page_repository)
     allow(Core::Registry::Repository).to receive(:[]).with(:footer).and_return(footer_repository)
     allow(Core::Registry::Repository).to receive(:[]).with(:category).and_return(category_repository)
+    allow(Core::Registry::Repository).to receive(:[]).with(:clump).and_return(clumps_repository)
 
     get '/en'
   end

53bdbfd100b256437a0e0f71f433adad4e40ab0c
David Trussler <mail@davidtrussler.net>
2017-01-09 16:21:37 +0000

    TP: 7773, Comment: Improved dropdowns - initial work

--- app/assets/javascripts/modules/global_nav.js
+++ app/assets/javascripts/modules/global_nav.js
@@ -16,14 +16,14 @@ define(['jquery'], function($) {
     });
 
     $('.global-nav__clump__heading').click(function() {
-      // Fixes an issue with Keyboard navigation on Desktop, 
+      // Fixes an issue with Keyboard navigation on Desktop,
       // if you open a Clump then other Clumps need to be closed
       if (window.innerWidth > 719) {
         $('.global-nav__clump').removeClass('is-active');
         $('.global-nav__clumps').removeClass('is-active');
       }
       // End Temporary fix
-      
+
       $(this)
         .parents('.global-nav__clump').addClass('is-active')
         .parents('.global-nav__clumps').addClass('is-active');
@@ -37,26 +37,38 @@ define(['jquery'], function($) {
   }
 
   GlobalNav.prototype._setUpDesktopInteraction = function() {
-    var delay = 250; // ms
-    var timeoutID;
+    var delay = 1000; // 250; // ms
+    var timeout_in, timeout_out;
 
+    // heading behaviour
     $('.global-nav__clump__heading').not('.global-nav__clump__blog-link')
       .mouseenter(function() {
         var self = this;
 
-        timeoutID = window.setTimeout(function() {
+        window.clearTimeout(timeout_out);
+
+        timeout_in = window.setTimeout(function() {
+          // clear active subnavs
+          $('.global-subnav').removeClass('is-active');
+
           $(self)
-            .parent('.global-nav__clump').addClass('is-active')
+            // .parent('.global-nav__clump').addClass('is-active')
+            // show current subnav
             .siblings('.global-subnav').addClass('is-active');
         }, delay);
       })
       .mouseleave(function() {
-        window.clearTimeout(timeoutID);
+        var self = this;
 
-        $(this)
-          .parent('.global-nav__clump').removeClass('is-active');
+        window.clearTimeout(timeout_in);
+
+        timeout_out = window.setTimeout(function() {
+          $(self)
+            .parent('.global-nav__clump').removeClass('is-active');
+        }, delay);
       });
 
+    /* subnav behaviour
     $('.global-subnav')
       .mouseenter(function() {
         $(this)
@@ -68,6 +80,7 @@ define(['jquery'], function($) {
           .removeClass('is-active')
           .siblings('.global-nav__clump__heading').parent('.global-nav__clump').removeClass('is-active');
       });
+    */
   }
 
   return GlobalNav;

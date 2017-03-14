03c77602033e1115f08a0992e3db550e854b1c22
Gabriel Smith <gabriel@itslikely.com>
2017-03-14 13:34:38 +0000

    updated javascript file to position tooltip above the clicked element and scroll tooltip to the top of the page

--- app/assets/javascripts/components/Glossary.js
+++ app/assets/javascripts/components/Glossary.js
@@ -21,9 +21,13 @@ define(['jquery', 'DoughBaseComponent'], function($, DoughBaseComponent) {
 
           $(item).each(function(){
             var updatedContent = articleContent.replace(RegExp("(?!<.*?)\\b(" + item.term + ")\\b(?![^<>]*?(<\/a>|<\/h2>|<\/h3>|>))","gi"),
-                                                               "<button class='unstyled-button glossary__term' aria-label='" + item.term + " (see description)'>" + item.term + "</button><span class='glossary__description'><span class='glossary__description-title'>" + item.term + "</span>" + item.description + "<button class='glossary__description-close'>close</button></span>");
+                                                               "<button class='unstyled-button glossary__term' aria-label='" + item.term + " (see description)'>" + item.term + "</a></button><span class='glossary__description'><span class='glossary__description-title'>" + item.term + "</a></span>" + item.description + "<button class='glossary__description-close'><svg xmlns='http://www.w3.org/2000/svg' \
+        class='svg-icon svg-icon--mobile-close-box'><use xmlns:xlink='http://www.w3.org/1999/xlink' xlink:href='#svg-icon--mobile-close-box'>\
+        </use></svg></button></span>");
+
             articleContent = updatedContent;
             $(article).html(updatedContent);
+
           });
           
         };
@@ -31,7 +35,24 @@ define(['jquery', 'DoughBaseComponent'], function($, DoughBaseComponent) {
 
       // Show descriptions
       $('.glossary__term').on('click', function(){
+        $(".glossary__description").removeClass('glossary__description--active');
         $(this).next('.glossary__description').addClass('glossary__description--active');
+
+        // Calculate parent container width and apply its width to the tooltip width
+        // Determine the clicked elements verticle position in the document and substract the height to position the tooltip
+        var container = $('html,body'),
+        scrollTo = $('.glossary__description--active'),
+        tooltipWidth = $('.l-article-3col-main').innerWidth() - 10,
+        top = $(this).position().top - scrollTo.outerHeight() - 10;
+
+        // apply top positioning and width to tooltip class using tooltipWidth and top variables
+        $('.glossary__description').css({top: top, width: tooltipWidth});
+
+        // scroll and animate tooltip to the top of the page
+        container.animate({
+            scrollTop: scrollTo.offset().top - container.offset().top - 70 + container.scrollTop()
+        });
+
         return false;
       });
 

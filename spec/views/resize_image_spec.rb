eeca4cc8c477f4e1d5366cbf80a444fad9d6eaad
Jones Agyemang <mightyj@hotmail.co.uk>
2017-03-15 09:47:03 +0000

    fixup! Adds support for serving image srcsets

--- spec/views/resize_image_spec.rb
+++ spec/views/resize_image_spec.rb
@@ -1,25 +1,11 @@
 RSpec.describe 'shared/_article_promos', type: :view do
   before do
 		allow(view).to receive(:items_with_image) do
-			[
-			 {
-				"heading"=>"Plug those money leaks",
-				 "image"=>"https://example.com/MoneyLeaks.jpg",
-				 "url"=>"https://example.com/blog",
-				 "label"=>"blog",
-				 "content"=>"Do you know the most common ways you waste money?"
-			 }
-			]
+      [{"heading"=>"Plug those money leaks", "image"=>"https://example.com/MoneyLeaks.jpg", "url"=>"https://example.com/blog", "label"=>"blog", "content"=>"Do you know the most common ways you waste money?" }]
 		end
 
 		allow(view).to receive(:items_without_image) do
-			[
-       {
-			  "heading"=>"Estimate the cost of buying a house and moving",
-			  "url"=>"https://example.com/blog",
-				"content"=>"There is more to it than just mortgage repayments"
-       }
-			]
+			[{"heading"=>"Estimate the cost of buying a house and moving", "url"=>"https://example.com/blog", "content"=>"There is more to it than just mortgage repayments"}]
 		end
 
     render

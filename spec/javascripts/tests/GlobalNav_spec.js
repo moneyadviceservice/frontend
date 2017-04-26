d87419f3a6163c6585908b03e7a91532d4881251
David Trussler <mail@davidtrussler.net>
2017-04-26 09:08:54 +0100

    TP: 7923, Comment: Test for mobile interaction - one test in this section not working, will need further work

--- spec/javascripts/tests/GlobalNav_spec.js
+++ spec/javascripts/tests/GlobalNav_spec.js
@@ -9,8 +9,13 @@ describe.only('GlobalNav', function() {
         function($, GlobalNav, mediaQueries, utilities, common) {
           self.$html = $(window.__html__['spec/javascripts/fixtures/GlobalNav.html']).appendTo('body');
           self.component = self.$html.find('[data-dough-component="GlobalNav"]');
-          self.GlobalNav = GlobalNav;
-          self.obj = new self.GlobalNav(self.component, self.component.data('dough-global-nav-config'));
+          self.globalNav = GlobalNav;
+          self.mobileNavButton = (self.$html).find('[data-dough-mobile-nav-button]');
+          self.clumpHeading = self.component.find('[data-dough-nav-clump-heading]');
+          self.subNavHeading = self.component.find('[data-dough-subnav-heading]');
+          self.mobileNavCloseButton = self.component.find('[data-dough-mobile-nav-close]');
+          self.mobileNavOverlay = self.component.find('[data-dough-mobile-nav-overlay]');
+          self.obj = new self.globalNav(self.component, self.component.data('dough-global-nav-config'));
 
           done();
         }, done);
@@ -20,19 +25,77 @@ describe.only('GlobalNav', function() {
     this.$html.remove();
   });
 
-  it('calls the right methods when initialising', function() {
-    var setUpMobileInteractionStub = sinon.stub(this.obj, "_setUpMobileInteraction"),
-        setUpDesktopInteractionStub = sinon.stub(this.obj, "_setUpDesktopInteraction"),
-        setUpMobileAnimationStub = sinon.stub(this.obj, "_setUpMobileAnimation"),
-        setUpKeyboardEventsStub = sinon.stub(this.obj, "_setUpKeyboardEvents"),
-        bindEventsStub = sinon.stub(this.obj, "_bindEvents");
+  describe('Initialise component', function() {
+    it('calls the right methods when initialising', function() {
+      var setUpMobileInteractionStub = sinon.stub(this.obj, "_setUpMobileInteraction"),
+          setUpDesktopInteractionStub = sinon.stub(this.obj, "_setUpDesktopInteraction"),
+          setUpMobileAnimationStub = sinon.stub(this.obj, "_setUpMobileAnimation"),
+          setUpKeyboardEventsStub = sinon.stub(this.obj, "_setUpKeyboardEvents"),
+          bindEventsStub = sinon.stub(this.obj, "_bindEvents");
 
-    this.obj.init();
+      this.obj.init();
 
-    expect(setUpMobileInteractionStub.callCount).to.be.equal(1);
-    expect(setUpDesktopInteractionStub.callCount).to.be.equal(1);
-    expect(setUpMobileAnimationStub.callCount).to.be.equal(1);
-    expect(setUpKeyboardEventsStub.callCount).to.be.equal(1);
-    expect(bindEventsStub.callCount).to.be.equal(1);
+      expect(setUpMobileInteractionStub.callCount).to.be.equal(1);
+      expect(setUpDesktopInteractionStub.callCount).to.be.equal(1);
+      expect(setUpMobileAnimationStub.callCount).to.be.equal(1);
+      expect(setUpKeyboardEventsStub.callCount).to.be.equal(1);
+      expect(bindEventsStub.callCount).to.be.equal(1);
+    });
+  });
+
+  describe('Mobile interaction', function() {
+    beforeEach(function() {
+      this.obj.init();
+    });
+
+    it('toggles nav visibility when menu button is clicked', function() {
+      this.mobileNavButton.trigger('click');
+      expect(this.component.hasClass('is-active')).to.be.true;
+
+      this.mobileNavButton.trigger('click');
+      expect(this.component.hasClass('is-active')).to.be.false;
+    });
+
+    /*
+     * this test is breaking because GlobalNav.js won't read the width set here
+     * return to it later!
+     *
+    it('toggles subnav visibility when clump heading is clicked', function() {
+      this.clumpHeading.click();
+
+      expect(this.clumpHeading.parents('[data-dough-nav-clump]').hasClass('is-active')).to.be.true;
+      expect(this.clumpHeading.parents('[data-dough-nav-clumps]').hasClass('is-active')).to.be.true;
+    });
+    */
+
+    it('toggles subnav visibility when subnav heading is clicked', function() {
+      // the subnav and nav need to be open before the test is run
+      this.subNavHeading.parents('[data-dough-nav-clump]').addClass('is-active');
+      this.subNavHeading.parents('[data-dough-nav-clumps]').addClass('is-active');
+
+      this.subNavHeading.trigger('click');
+      expect(this.clumpHeading.parents('[data-dough-nav-clump]').hasClass('is-active')).to.be.false;
+      expect(this.clumpHeading.parents('[data-dough-nav-clumps]').hasClass('is-active')).to.be.false;
+
+      this.subNavHeading.trigger('click');
+      expect(this.clumpHeading.parents('[data-dough-nav-clump]').hasClass('is-active')).to.be.true;
+      expect(this.clumpHeading.parents('[data-dough-nav-clumps]').hasClass('is-active')).to.be.true;
+    });
+
+    it('closes nav when close button is clicked', function() {
+      // the nav needs to be open before the test is run
+      this.component.addClass('is-active');
+
+      this.mobileNavCloseButton.trigger('click');
+      expect(this.component.hasClass('is-active')).to.be.false;
+    });
+
+    it('closes nav when overlay is clicked', function() {
+      // the nav needs to be open before the test is run
+      this.component.addClass('is-active');
+
+      this.mobileNavOverlay.trigger('click');
+      expect(this.component.hasClass('is-active')).to.be.false;
+    });
   });
 });

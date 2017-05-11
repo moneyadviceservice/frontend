827eccb6b211416e86ca62cca780c9589f18a322
David Trussler <mail@davidtrussler.net>
2017-05-11 12:32:19 +0100

    TP: 7923, Comment: Test for keyboard interactions -- adds tests for enter key -- adds tests for spacebar key -- adds tests for left arrow key at primary level -- adds tests for right arrow key at primary level -- adds tests for down arrow key at primary level -- adds tests for escape key -- adds tests for up arrow key -- adds tests for down arrow key at secondary level -- adds tests for left arrow key at secondry level -- adds tests for right arrow key at secondary level

--- spec/javascripts/tests/GlobalNav_spec.js
+++ spec/javascripts/tests/GlobalNav_spec.js
@@ -1,4 +1,4 @@
-describe.only('GlobalNav', function() {
+describe('GlobalNav', function() {
   'use strict';
 
   beforeEach(function(done) {
@@ -156,6 +156,170 @@ describe.only('GlobalNav', function() {
     it('toggles the no-transition class when viewport size changes', function() {
       // can't get this to work at the minute for the same reasons:
       // unable to reflect the size change via the mediaQueries component
+    });
+  });
+
+  describe.only('Keyboard Events', function() {
+    // var clock;
+    var triggerKeyUp = function(element, keyCode) {
+      var e = $.Event('keyup');
+      e.which = keyCode;
+      element.trigger(e);
+    };
+
+    beforeEach(function() {
+      // clock = sinon.useFakeTimers();
+      this.obj.init();
+    });
+
+    afterEach(function() {
+      // clock.restore();
+    });
+
+    it('when at top level the enter key opens dropdown and moves focus into this', function() {
+      var index = $('#clump-1').find('[data-dough-nav-clump-heading]');
+
+      triggerKeyUp(index, 13);
+
+      expect(index.parents('[data-dough-nav-clump]').hasClass('is-active')).to.be.true;
+      expect(index.attr('aria-expanded')).to.eq('true');
+
+      // not convinced this is actually doing what is required ...
+      // ... come back to it later ...
+      expect($(index.siblings('[data-dough-subnav]').find('[data-dough-subcategories]').find('a')[0]).filter(':focus')).to.exist;
+    });
+
+    it('when at top level the spacebar key opens dropdown', function() {
+      var index = $('#clump-1').find('[data-dough-nav-clump-heading]');
+
+      triggerKeyUp(index, 32);
+
+      expect(index.parents('[data-dough-nav-clump]').hasClass('is-active')).to.be.true;
+      expect(index.attr('aria-expanded')).to.eq('true');
+    });
+
+    it('when at top level the left arrow key moves focus to the previous element or wraps if on the first element', function() {
+      var index = $('#clump-2').find('[data-dough-nav-clump-heading]');
+
+      triggerKeyUp(index, 37);
+
+      // not convinced this is actually doing what is required either ...
+      // ... also come back to it later ...
+      expect($('#clump-1').find('[data-dough-nav-clump-heading]').filter(':focus')).to.exist;
+
+      // also haven't done the wrapping yet
+    });
+
+    it('when at top level the right arrow key moves focus to the next element or wraps if on the last element', function() {
+      var index = $('#clump-1').find('[data-dough-nav-clump-heading]');
+
+      triggerKeyUp(index, 39);
+
+      // not convinced this is actually doing what is required either ...
+      // ... also come back to it later ...
+      expect($('#clump-2').find('[data-dough-nav-clump-heading]').filter(':focus')).to.exist;
+
+      // also haven't done the wrapping yet
+    });
+
+    it('when at top level the down arrow key opens the dropdown', function() {
+      var index = $('#clump-1').find('[data-dough-nav-clump-heading]');
+
+      triggerKeyUp(index, 40);
+
+      expect(index.parents('[data-dough-nav-clump]').hasClass('is-active')).to.be.true;
+    });
+
+    it('when at secondary level the escape key closes dropdown and moves focus back to top level', function() {
+      var index = $('#clump-1').find('[data-dough-subcategories]');
+
+      triggerKeyUp(index, 27);
+
+      expect(index.parents('[data-dough-nav-clump]').hasClass('is-active')).to.be.false;
+
+      // not convinced this is actually doing what is required either ...
+      // ... also come back to it later ...
+      expect($('#clump-1').find('[data-dough-nav-clump-heading]').filter(':focus')).to.exist;
+    });
+
+    it('when at secondary level the up arrow key moves focus to previous element or wraps to the last element if on the first', function() {
+      var links = $('#clump-1').find('[data-dough-subcategories]').find('a');
+
+      triggerKeyUp(links.last(), 38);
+
+      // not convinced this is actually doing what is required either ...
+      // ... also come back to it later ...
+      expect(links.first().filter(':focus')).to.exist;
+
+      triggerKeyUp(links.first(), 38);
+
+      // not convinced this is actually doing what is required either ...
+      // ... also come back to it later ...
+      expect(links.last().filter(':focus')).to.exist;
+    });
+
+    it('when at secondary level the down arrow key moves focus to the next element or wraps to the first element if on the last', function() {
+      var links = $('#clump-1').find('[data-dough-subcategories]').find('a');
+
+      triggerKeyUp(links.first(), 40);
+
+      // not convinced this is actually doing what is required either ...
+      // ... also come back to it later ...
+      expect(links.last().filter(':focus')).to.exist;
+
+      triggerKeyUp(links.first(), 40);
+
+      // not convinced this is actually doing what is required either ...
+      // ... also come back to it later ...
+      expect(links.first().filter(':focus')).to.exist;
+    });
+
+    it('when at secondary level the left arrow key closes the dropdown and moves focus to the previous top level element or the last element if already on the first', function() {
+      var links;
+
+      // open dropdown before running test
+      $('#clump-2').addClass('is-active');
+
+      links = $('#clump-2').find('[data-dough-subcategories]').find('a');
+
+      triggerKeyUp(links.last(), 37);
+
+      expect($('#clump-2').hasClass('is-active')).to.be.false;
+      expect($('#clump-1').find('[data-dough-nav-clump-heading]').filter(':focus')).to.exist;
+
+      // open dropdown before running test
+      $('#clump-1').addClass('is-active');
+
+      links = $('#clump-1').find('[data-dough-subcategories]').find('a');
+
+      triggerKeyUp(links.last(), 37);
+
+      expect($('#clump-1').hasClass('is-active')).to.be.false;
+      expect($('#clump-2').find('[data-dough-nav-clump-heading]').filter(':focus')).to.exist;
+    });
+
+    it('when at secondary level the right arrow key closes the dropdown and moves focus to the next top level element or the first element if already on the last', function() {
+      var links;
+
+      // open dropdown before running test
+      $('#clump-1').addClass('is-active');
+
+      links = $('#clump-1').find('[data-dough-subcategories]').find('a');
+
+      triggerKeyUp(links.last(), 39);
+
+      expect($('#clump-1').hasClass('is-active')).to.be.false;
+      expect($('#clump-2').find('[data-dough-nav-clump-heading]').filter(':focus')).to.exist;
+
+      // open dropdown before running test
+      $('#clump-2').addClass('is-active');
+
+      links = $('#clump-2').find('[data-dough-subcategories]').find('a');
+
+      triggerKeyUp(links.last(), 39);
+
+      expect($('#clump-2').hasClass('is-active')).to.be.false;
+      expect($('#clump-1').find('[data-dough-nav-clump-heading]').filter(':focus')).to.exist;
     });
   });
 });

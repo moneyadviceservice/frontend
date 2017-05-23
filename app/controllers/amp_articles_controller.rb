a3c19531422fe06c651040d562366902214a81e0
pmcostello <pmcostello@hotmail.co.uk>
2017-05-23 15:07:48 +0100

    add newrelic browser agent

--- app/controllers/amp_articles_controller.rb
+++ app/controllers/amp_articles_controller.rb
@@ -6,6 +6,8 @@ class AmpArticlesController < ActionController::Base
   decorates_assigned :article, with: AmpArticleDecorator
   before_action :retrieve_article
 
+  newrelic_ignore_enduser
+
   def show
     redirect_to url_for(action: 'show', controller: 'articles', id: @article.id, only_path: false) unless @article.supports_amp || params[:no_redirect]
   end

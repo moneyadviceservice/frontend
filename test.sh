8708895541a925f2b57440614d4a974140f4a3d4
pmcostello <pmcostello@hotmail.co.uk>
2017-06-24 02:46:18 +0100

    fixup! update pa11y.json file

--- test.sh
+++ test.sh
@@ -23,4 +23,5 @@ bowndler update --production --config.interactive=false
 RAILS_ENV=development rake karma:install karma:run_once
 #bundle exec rspec spec --format html --out tmp/spec.html --format RspecJunitFormatter --profile --format progress --deprecation-out log/rspec_deprecations.txt
 #bundle exec cucumber
-bundle exec passenger start -d; pa11y -i "warning;notice" localhost:3000/ -r rainbows -s WCAG2AA; bundle exec passenger stop
+npm install pa11y pa11y-reporter-ci pa11y-reporter-rainbows
+bundle exec passenger start -d && pa11y -i "warning;notice" localhost:3000/ -r ci -s WCAG2AA && bundle exec passenger stop

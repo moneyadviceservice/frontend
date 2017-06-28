d8aa04a09341f1e3029958921b6d5d797ee89d1b
Fernando Briano <fernando@picandocodigo.net>
2017-06-28 12:46:22 -0300

    Remove Universal Credit session data on log out

---
+++ features/step_definitions/universal_credit_steps.rb
@@ -0,0 +1,19 @@
+Given(/^I have answered questions on Money Manager$/) do
+  visit('/en/tools/money-manager/')
+  find_field(id: 'questionnaire_received_first_payment_true').click
+  find_field(id: 'questionnaire_single_or_in_couple_single').click
+  click_button('Submit')
+end
+
+Given(/^I am at the To Read page$/) do
+  expect(page.current_path).to eql('/en/tools/money-manager/to-read')
+end
+
+Then(/^I should not see answered questions on Money Manager$/) do
+  expect(find_field(id: 'questionnaire_received_first_payment_true')).not_to be_checked
+  expect(find_field(id: 'questionnaire_received_first_payment_false')).not_to be_checked
+end
+
+Then(/^I should be at the Money Manager landing page$/) do
+  expect(page.current_path).to eql('/en/tools/money-manager/')
+end

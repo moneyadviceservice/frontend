62b80db2bf5165563d1689a2beb4d53800677576
Tomas D'Stefano <tomas_stefano@successoft.com>
2017-08-09 16:12:11 +0100

    The wpcc url should be secretive for now

--- spec/requests/tool_integration/wpcc_spec.rb
+++ spec/requests/tool_integration/wpcc_spec.rb
@@ -2,11 +2,27 @@ RSpec.describe 'Wpcc', type: :request do
   [ "/en/tools/#{ToolMountPoint::Wpcc::EN_ID}",
     "/cy/tools/#{ToolMountPoint::Wpcc::CY_ID}" ].each do |path|
     describe path do
-      before do
-        get path
+      context 'when credentials are valid' do
+        let(:credentials) do
+          ActionController::HttpAuthentication::Basic.encode_credentials(
+            ENV['AUTH_USERNAME'], ENV['AUTH_PASSWORD']
+          )
+        end
+
+        before do
+          get(path, {}, { 'HTTP_AUTHORIZATION' => credentials })
+        end
+
+        specify { expect(response).to be_ok }
       end
 
-      specify { expect(response).to be_ok }
+      context 'when credentials are invalid' do
+        before { get(path) }
+
+        it 'returns unauthorized status' do
+          expect(response.status).to be(401)
+        end
+      end
     end
   end
 end

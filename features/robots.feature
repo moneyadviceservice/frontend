cfd77294d9c993dcc8e77380f49c2b1a7c7e4d22
Tomas D'Stefano <tomas_stefano@successoft.com>
2017-08-09 15:56:28 +0100

    The wppc secret link should not be available to web crawlers

--- features/robots.feature
+++ features/robots.feature
@@ -3,3 +3,4 @@ Feature: discover robots instructions file
   Scenario: display robots text file
     When I visit the conventional robots link
     Then I should see that Atomz is blocked from scrawling
+    And I should see that wpcc secret url is blocked from crawlers
--- lib/tool_mount_point/wpcc.rb
+++ lib/tool_mount_point/wpcc.rb
@@ -1,8 +1,6 @@
 module ToolMountPoint
   class Wpcc < Base
-    # EN_ID = 'workplace-pension-contribution-calculator'
-    # TO BE SWITCHED TO ABOVE ONCE WE ARE DEPLOYING AND MOVING ON PRODUCTION
     EN_ID = 'wpcc'
-    CY_ID = 'cyfrifiannell-cyfraniadau-pensiwn-gweithle'
+    CY_ID = 'wpcc'
   end
 end

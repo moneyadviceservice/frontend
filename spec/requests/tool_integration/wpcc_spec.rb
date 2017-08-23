40f8135c1d3238125f01618abdea0f90ac21d701
Tomas D'Stefano <tomas_stefano@successoft.com>
2017-08-23 16:53:08 +0100

    Replacing old wpcc with the new one
    In order to replace the following was necessary:
    
    * The contribution_calculator (the old wpcc) gem should be removed.
    * Wpcc using git until we have the final version bumped.
    * Remove authentication form the wpcc in order to be open to the users.
    * Remove the robots reference because there is no /en/tools/wpcc link anymore.

--- spec/requests/tool_integration/wpcc_spec.rb
+++ spec/requests/tool_integration/wpcc_spec.rb
@@ -1,28 +1,12 @@
 RSpec.describe 'Wpcc', type: :request do
-  [ "/en/tools/#{ToolMountPoint::Wpcc::EN_ID}",
-    "/cy/tools/#{ToolMountPoint::Wpcc::CY_ID}" ].each do |path|
+  ["/en/tools/#{ToolMountPoint::Wpcc::EN_ID}",
+   "/cy/tools/#{ToolMountPoint::Wpcc::CY_ID}"].each do |path|
     describe path do
-      context 'when credentials are valid' do
-        let(:credentials) do
-          ActionController::HttpAuthentication::Basic.encode_credentials(
-            ENV['AUTH_USERNAME'], ENV['AUTH_PASSWORD']
-          )
-        end
-
-        before do
-          get(path, {}, { 'HTTP_AUTHORIZATION' => credentials })
-        end
-
-        specify { expect(response).to be_ok }
+      before do
+        get(path)
       end
 
-      context 'when credentials are invalid' do
-        before { get(path) }
-
-        it 'returns unauthorized status' do
-          expect(response.status).to be(401)
-        end
-      end
+      specify { expect(response).to be_ok }
     end
   end
 end

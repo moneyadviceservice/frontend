d39f3e27c024ea400551197fc6305f049299c4f4
Jones Agyemang <mightyj@hotmail.co.uk>
2017-08-30 10:29:16 +0100

    WelshID may still refer to the old tool so removing for now

--- lib/tool_mount_point/wpcc.rb
+++ lib/tool_mount_point/wpcc.rb
@@ -2,7 +2,8 @@ module ToolMountPoint
   class Wpcc < Base
     # EN_ID = 'workplace-pension-contribution-calculator'
     # TO BE SWITCHED TO ABOVE ONCE WE DEPLOY AND MOVE TO PROD
+    # CY_ID = 'cyfrifiannell-cyfraniadau-pensiwn-gweithle'
     EN_ID = 'wpcc'
-    CY_ID = 'cyfrifiannell-cyfraniadau-pensiwn-gweithle'
+    CY_ID = 'wpcc'
   end
 end

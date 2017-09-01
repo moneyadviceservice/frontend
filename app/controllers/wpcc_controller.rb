b28471f792858433876bc64fbeebf2b6a03bc7d3
Tomas D'Stefano <tomas_stefano@successoft.com>
2017-09-01 15:50:44 +0100

    Remove authentication from wpcc temporarily on UAT

--- app/controllers/wpcc_controller.rb
+++ app/controllers/wpcc_controller.rb
@@ -1,14 +1,5 @@
 class WpccController < EmbeddedToolsController
-  before_action :authenticate
-
   protected
-
-  def authenticate
-    authenticate_or_request_with_http_basic do |username, password|
-      Authenticable.authenticate(username, password)
-    end
-  end
-
   def category_id
     ''
   end

b20fc3e2837c85f4f4363aeda5dd10d7d44d5046
Gabriel Smith <gabriel@itslikely.com>
2017-09-15 14:01:31 +0100

    Fixes mobile nav scrolling by hiding subnav and adding no-scroll class to body tag

--- app/assets/javascripts/components/GlobalNav.js
+++ app/assets/javascripts/components/GlobalNav.js
@@ -233,7 +233,7 @@ define(['jquery', 'DoughBaseComponent', 'mediaQueries', 'utilities', 'common'], 
 
     this.$globalSubNavHeading.click(function(e) {
       e.preventDefault();
-      self._toggleMobileSubNav(this);
+      self._returnMobileNav(this);
     });
 
     this.$mobileNavClose.click(function(e){
@@ -304,6 +304,7 @@ define(['jquery', 'DoughBaseComponent', 'mediaQueries', 'utilities', 'common'], 
   GlobalNav.prototype._toggleMobileNav = function() {
     this.$globalNav.toggleClass('is-active');
     this.$mobileNavOverlay.toggleClass('is-active');
+    $('body').addClass('no-scroll');
 
     // If we just closed the nav, reset all subnavs
     if (!this.$globalNav.hasClass('is-active')) {
@@ -311,6 +312,7 @@ define(['jquery', 'DoughBaseComponent', 'mediaQueries', 'utilities', 'common'], 
       this.$mobileNavOverlay.removeClass('is-active');
       this.$globalNavClumps.removeClass('is-active');
       this.$globalNavClump.removeClass('is-active');
+      $('body').removeClass('no-scroll');
     }
   };
 
@@ -318,6 +320,15 @@ define(['jquery', 'DoughBaseComponent', 'mediaQueries', 'utilities', 'common'], 
     $(index)
       .parents('[data-dough-nav-clump]').toggleClass('is-active')
       .parents('[data-dough-nav-clumps]').toggleClass('is-active');
+      this.$globalSubNav.removeClass('is-hidden');
+
+  };
+
+  GlobalNav.prototype._returnMobileNav = function(index) {
+    $(index)
+      .parents('[data-dough-nav-clump]').toggleClass('is-active')
+      .parents('[data-dough-nav-clumps]').toggleClass('is-active');
+      this.$globalSubNav.addClass('is-hidden');
   };
 
   GlobalNav.prototype._openDesktopSubNav = function(index) {

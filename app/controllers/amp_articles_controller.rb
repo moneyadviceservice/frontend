8a04de8bb0a27ec3b40d787cfa5a941b6e397793
John Player <johnplayer1982@msn.com>
2017-10-19 14:25:27 +0100

    Initial SPIKE for AMP global nav

--- app/controllers/amp_articles_controller.rb
+++ app/controllers/amp_articles_controller.rb
@@ -12,6 +12,54 @@ class AmpArticlesController < ActionController::Base
     redirect_to url_for(action: 'show', controller: 'articles', id: @article.id, only_path: false) unless @article.supports_amp || params[:no_redirect]
   end
 
+  def clumps
+    Core::ClumpsReader.new.call
+  end
+  helper_method :clumps
+
+  def category_tree(categories = Core::Registry::Repository[:category].all)
+    Core::CategoryTreeReader.new.call(categories)
+  end
+
+  def category_tree_with_decorator(categories = Core::Registry::Repository[:category].all)
+    Core::CategoryTreeReaderWithDecorator.new.call(categories)
+  end
+
+  def navigation_categories
+    Core::Registry::Repository[:category].all
+  end
+
+  def corporate_categories
+    Core::Registry::Repository[:category].find('corporate-categories')['contents']
+  end
+
+  def corporate_category?(category, corporate = corporate_categories, categories = [])
+    categories << corporate.map {|c| c['id']}
+    return true if categories.flatten.include?(category)
+    unless corporate.first['contents']
+      corporate_category?(category, corporate.first['contents'], categories.flatten)
+    end
+  end
+  
+  helper_method :corporate_category?
+  
+  def category_navigation(corporate = false)
+    categories = corporate ? corporate_categories : navigation_categories
+    @category_navigation ||= CategoryNavigationDecorator.decorate_collection(category_tree_with_decorator(categories).children)
+  end
+
+  helper_method :category_navigation
+
+  def corporate_category_navigation
+    @corporate_category_navigation ||= CategoryNavigationDecorator.decorate_collection(category_tree_with_decorator(corporate_categories).children)
+  end
+
+  helper_method :corporate_category_navigation
+
+  def hide_elements_irrelevant_for_third_parties?
+    false
+  end
+  
   private
 
   def retrieve_article

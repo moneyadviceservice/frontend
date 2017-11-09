6078fb29e75363665e272b39b9755826e10cce80
Will Hall <willhall88@gmail.com>
2017-11-09 18:17:45 +0000

    Add reCAPTCHA to Sign Up

--- spec/controllers/registrations_controller_spec.rb
+++ spec/controllers/registrations_controller_spec.rb
@@ -3,8 +3,40 @@ RSpec.describe RegistrationsController, type: :controller do
     it 'sets user to have accepted terms and conditions' do
       allow(subject).to receive(:resource_class) { User }
       allow(subject).to receive(:resource_name) { 'user' }
-      subject.send :build_resource,  first_name: 'Phil'
+      subject.send :build_resource, first_name: 'Phil'
       expect(subject.send(:resource).accept_terms_conditions).to be_truthy
+    end
+  end
+
+  describe '#create' do
+    let(:user_params) do
+      {
+        email: 'user@mail.com',
+        first_name: 'peanut',
+        post_code: 'EC1 9JU',
+        password: 'Password123'
+      }
+    end
+
+    before do
+      @request.env['devise.mapping'] = Devise.mappings[:user]
+      allow(subject).to receive(:sign_up_params) { user_params }
+    end
+
+    context 'with valid reCAPTCHA' do
+      it 'saves a user' do
+        allow(subject).to receive(:verify_recaptcha) { true }
+        post :create, params: { locale: :en }
+        expect(User.all.count).to eq 1
+      end
+    end
+
+    context 'with invalid reCAPTCHA' do
+      it 'does not save a user' do
+        allow(subject).to receive(:verify_recaptcha) { false }
+        post :create, params: { locale: :en }
+        expect(User.all.count).to eq 0
+      end
     end
   end
 end

a9c13b59ebdd77e3ee3c0c732add678ff6985da8
David Trussler <mail@davidtrussler.net>
2018-01-09 12:10:20 +0000

    TP: 8675, Comment: Adds leader image to homepage

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -13,6 +13,12 @@ module HomeHelper
     remove_p_tags_from(document).html_safe
   end
 
+  def leader_text(name, size, x, y, rotation)
+    "<text text-anchor='middle' x=#{x} y=#{y} transform='rotate(#{rotation} #{x} #{y})'>
+      #{t('home.show.beta_leader.text_' + name + '.' + size + '_html')}
+    </text>"
+  end
+
   private
 
   def remove_p_tags_from(document)

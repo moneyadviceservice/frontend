2a13ca9046737a52bfc65763925e38e7f4d38f1f
John Player <johnplayer1982@msn.com>
2018-01-16 14:01:41 +0000

    Adds tests for seasonal spotlight component

--- features/step_definitions/home_beta_page_steps.rb
+++ features/step_definitions/home_beta_page_steps.rb
@@ -9,3 +9,11 @@ Then(/^I should be presented with popular tools and calculators$/) do
   expect(home_beta_page.popular_tools)
     .to have_content(I18n.t('home.show.tools_heading'))
 end
+
+Then(/^I should be presented with the seasonal spotlight$/) do
+  expected_en = "Easy ways to catch the saving habit"
+  expected_cy = "Ffyrdd hawdd i ddal y arferiad cynilo"
+
+  expect(home_beta_page.seasonal_spotlight)
+    .to have_content(eval("expected_#{I18n.locale}"))
+end

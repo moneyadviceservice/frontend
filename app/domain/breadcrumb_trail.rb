41d0e28922aab14ef3f1cb627f8e2345e8997928
Philip Hayton <p.j.hayton@hotmail.com>
2018-01-22 11:28:37 +0000

    Updated category controller to use mas cms gem and simplifed breadbrumbs for categories

--- app/domain/breadcrumb_trail.rb
+++ app/domain/breadcrumb_trail.rb
@@ -1,29 +1,38 @@
 class BreadcrumbTrail
-  def self.build(item, category_tree)
-    categories = case item
-      when Core::Category
-        if item.parent_id.present?
-          RootToNodePath.build(item, category_tree)[0...-1]
-        else
-          [HomeCategory.new]
-        end
-      when ToolCategory
-        RootToNodePath.build(item, category_tree)
-      when Core::StaticPage
-        [HomeCategory.new]
-      when Core::NewsArticle
-        [HomeCategory.new, NewsCategory.new]
+  def self.build(item, category_tree = nil)
+    case item
+    when Mas::Cms::Category
+      # recurse and find categories instead of passing in category_tree?
+      if item.parent_id.present?
+        build_trail(item.parent_id)
       else
-        if item.categories.empty?
-          [HomeCategory.new]
-        elsif item.categories.one?
-          RootToNodePath.build(item.categories.first, category_tree)
-        else
-          []
-        end
+        [HomeCategory.new]
+      end
+    when ToolCategory
+      RootToNodePath.build(item, category_tree)
+    when Core::StaticPage
+      [HomeCategory.new]
+    when Core::NewsArticle
+      [HomeCategory.new, NewsCategory.new]
+    else
+      if item.categories.empty?
+        [HomeCategory.new]
+      elsif item.categories.one?
+        RootToNodePath.build(item.categories.first, category_tree)
+      else
+        []
+      end
+    end.map do |element|
+      Breadcrumb.new(element)
     end
+  end
 
-    categories.map(&Breadcrumb.public_method(:new))
+  def self.build_trail(category_id, children = [])
+    category = Mas::Cms::Category.find(category_id, locale: I18n.locale)
+    children.unshift category
+    return [] if category.id == 'corporate-home'
+    return children.unshift(HomeCategory.new) if category.parent_id.blank?
+    build_trail(category.parent_id, children)
   end
 
   def self.home

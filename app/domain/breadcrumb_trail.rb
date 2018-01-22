07c4a009ebaf6aaf3a4d6fea314754026f38caae
Philip Hayton <p.j.hayton@hotmail.com>
2018-01-22 11:28:37 +0000

    86656 Updated category controller to use mas cms gem and simplifed breadbrumbs for categories
    Generalised Mas::Cms error handling and redirects, refactored articles to use category methods
    
    Updated corproate controllers to work with mas cms categories

--- app/domain/breadcrumb_trail.rb
+++ app/domain/breadcrumb_trail.rb
@@ -1,29 +1,45 @@
 class BreadcrumbTrail
-  def self.build(item, category_tree)
-    categories = case item
-      when Core::Category
-        if item.parent_id.present?
-          RootToNodePath.build(item, category_tree)[0...-1]
-        else
-          [HomeCategory.new]
-        end
-      when ToolCategory
-        RootToNodePath.build(item, category_tree)
-      when Core::StaticPage
-        [HomeCategory.new]
-      when Core::NewsArticle
-        [HomeCategory.new, NewsCategory.new]
-      else
-        if item.categories.empty?
-          [HomeCategory.new]
-        elsif item.categories.one?
-          RootToNodePath.build(item.categories.first, category_tree)
-        else
-          []
-        end
+  def self.build(item, category_tree = nil)
+    case item
+    when Mas::Cms::Category
+      build_category_trail(item)
+    when ToolCategory
+      RootToNodePath.build(item, category_tree)
+    when Core::StaticPage
+      [HomeCategory.new]
+    when Core::NewsArticle
+      [HomeCategory.new, NewsCategory.new]
+    else
+      build_default_trail(item, category_tree)
+    end.map do |element|
+      Breadcrumb.new(element)
     end
+  end
 
-    categories.map(&Breadcrumb.public_method(:new))
+  def self.build_category_trail(item)
+    if item.parent_id.present?
+      build_trail(item.parent_id)
+    else
+      [HomeCategory.new]
+    end
+  end
+
+  def self.build_default_trail(item, category_tree)
+    if item.categories.empty?
+      [HomeCategory.new]
+    elsif item.categories.one?
+      RootToNodePath.build(item.categories.first, category_tree)
+    else
+      []
+    end
+  end
+
+  def self.build_trail(category_id, children = [])
+    category = Mas::Cms::Category.find(category_id, locale: I18n.locale)
+    children.unshift category
+    return [] if category.id == 'corporate-home'
+    return children.unshift(HomeCategory.new) if category.parent_id.blank?
+    build_trail(category.parent_id, children)
   end
 
   def self.home
--- spec/controllers/corporate_controller_spec.rb
+++ spec/controllers/corporate_controller_spec.rb
@@ -1,36 +1,28 @@
 RSpec.describe CorporateController, type: :controller do
-  let(:corporate) { instance_double(Core::Article, id: 'test', categories: []) }
-  let(:corporate_reader) { instance_double(Core::CorporateReader, call: corporate) }
-  let(:category_tree) { double.as_null_object }
-  let(:corporate_category) { Core::Category.new('corporate-home', contents: []) }
-  let(:syndication_category) { Core::Category.new('syndication', contents: []) }
-  let(:corporate_category_reader) { instance_double(Core::CategoryReader, call: corporate_category) }
-  let(:syndication_category_reader) { instance_double(Core::CategoryReader, call: syndication_category) }
+  let(:syndication_category) { Mas::Cms::Category.new('syndication', contents: []) }
+  let(:corporate_category) { Mas::Cms::Category.new('corporate-home', contents: []) }
 
   before do
-    allow(Core::CategoryTreeReader).to receive(:new) do
-      instance_double(Core::CategoryTreeReader, call: category_tree)
-    end
+    allow(Mas::Cms::Category).to receive(:find)
+      .with('corporate-home', locale: I18n.locale)
+      .and_return(corporate_category)
+
+    allow(Mas::Cms::Category).to receive(:find)
+      .with('syndication', anything)
+      .and_return(syndication_category)
   end
 
-  describe "GET index" do
-    before do
-      allow(Core::CategoryReader).to receive(:new) do
-        instance_double(Core::CategoryReader, call: corporate_category)
-      end
-    end
-
+  describe 'GET index' do
     context 'when corporate-home category exists' do
       before do
-        expect(Core::CategoryReader).to receive(:new).with(corporate_category.id) { corporate_category_reader }
         get :index, locale: I18n.locale
       end
 
-      it "responds successfully" do
+      it 'responds successfully' do
         expect(response).to be_ok
       end
 
-      it "assigns category" do
+      it 'assigns category' do
         expect(assigns[:category]).to be(corporate_category)
       end
     end
@@ -38,13 +30,14 @@ RSpec.describe CorporateController, type: :controller do
 
   describe 'GET show' do
     context 'when corporate page exists' do
-      let(:corporate) { instance_double(Core::Article, id: 'test', categories: [corporate_category]) }
+      let(:corporate_article) { Mas::Cms::Article.new('test', categories: [corporate_category]) }
 
       before do
-        expect(Core::CategoryReader).to receive(:new).with(corporate_category.id) { corporate_category_reader }
-        expect(Core::CategoryReader).to receive(:new).with(syndication_category.id) { syndication_category_reader }
-        expect(Core::CorporateReader).to receive(:new).with(corporate.id) { corporate_reader }
-        get :show, locale: I18n.locale, id: corporate.id
+        allow(Mas::Cms::Article).to receive(:find)
+          .with(corporate_article.id, locale: I18n.locale)
+          .and_return(corporate_article)
+
+        get :show, locale: I18n.locale, id: corporate_article.id
       end
 
       it 'responds successfuly' do
@@ -52,28 +45,13 @@ RSpec.describe CorporateController, type: :controller do
       end
 
       it 'assigns corporate page' do
-        expect(assigns[:article]).to be(corporate)
-      end
-    end
-
-    context 'when corporate page does not exist' do
-      it 'raises an ActionController RoutingError' do
-        allow(Core::CorporateReader).to receive(:new) { ->(&block) { block.call(Core::Article.new('no-exist')) } }
-
-        expect { get :show, id: 'foo', locale: I18n.locale }.to raise_error(ActionController::RoutingError)
-      end
-    end
-
-    context 'when corporate page is redirected' do
-      it 'is redirected to specified location' do
-        get :show, id: 'standard-financial-statement-consultation', locale: I18n.locale
-        expect(response).to redirect_to('http://localhost:5000/en/corporate/press-release-standard-financial-statement')
+        expect(assigns[:article]).to be(corporate_article)
       end
     end
   end
 
   describe 'POST create' do
-    let(:tool) { instance_double(Core::Article, id: "#{valid_partner[:tool_name]}", categories: [syndication_category]) }
+    let(:tool) { instance_double(Core::Article, id: valid_partner[:tool_name].to_s, categories: [syndication_category]) }
     let(:valid_partner)   { FactoryGirl.attributes_for(:corporate_partner, tool_name: 'budget-planner-syndication') }
     let(:invalid_partner) { { name: 2323, width: 'sasd' } }
 
@@ -90,7 +68,6 @@ RSpec.describe CorporateController, type: :controller do
     end
 
     context 'with valid attributes and a partner record already exists' do
-
       before do
         post :create, locale: I18n.locale, corporate_partner: valid_partner, id: tool.id
       end
@@ -101,14 +78,13 @@ RSpec.describe CorporateController, type: :controller do
         end.to_not change(CorporatePartner, :count)
         expect(CorporatePartner.count).to eq(1)
       end
-
     end
 
     context 'with invalid attributes' do
       it 'does not save the new partner' do
         expect do
           post :create, locale: I18n.locale, corporate_partner: invalid_partner, id: tool.id
-        end.to_not change(CorporatePartner,:count)
+        end.to_not change(CorporatePartner, :count)
       end
 
       it 're-renders the corporate tool page' do
@@ -124,7 +100,7 @@ RSpec.describe CorporateController, type: :controller do
         { id: 'my-long-tool-name-syndication' }
       end
 
-      result = subject.send(:tool_friendly_name);
+      result = subject.send(:tool_friendly_name)
       expect(result).to eq('My long tool name')
     end
   end

1d3e4492a6db8e499f328a0a60ee3586bb179a63
Philip Hayton <p.j.hayton@hotmail.com>
2018-01-23 16:28:54 +0000

    8668 mas cms page feedback
    remove old interactor code
    
    update controller and tests
    
    remove old core files
    
    update tests and handle failures

--- app/controllers/page_feedbacks_controller.rb
+++ app/controllers/page_feedbacks_controller.rb
@@ -1,38 +1,37 @@
 class PageFeedbacksController < ApplicationController
   before_action :check_accepts_feedback
+  rescue_from Mas::Cms::Errors::Base, with: :unprocessable
 
   def create
-    page_feedback = Core::PageFeedbackCreator.new.call(page_feedback_params)
-
-    if page_feedback
-      render json: page_feedback, status: 201
-    else
-      render json: {}, status: 422
-    end
+    render json: Mas::Cms::PageFeedback.create(page_feedback_params),
+           status: 201
   end
 
   def update
-    page_feedback = Core::PageFeedbackUpdator.new.call(page_feedback_params)
-
-    if page_feedback
-      render json: page_feedback, status: 200
-    else
-      render json: {}, status: 422
-    end
+    render json: Mas::Cms::PageFeedback.update(page_feedback_params),
+           status: 200
   end
 
   private
 
   def check_accepts_feedback
-    if !Core::Article.new(params[:article_id]).accepts_feedback?
-      render json: {}, status: 403
-    end
+    return if article_resource.accepts_feedback?
+    render json: {}, status: 403
+  end
+
+  def article_resource
+    Mas::Cms::Article.find(params[:article_id], locale: I18n.locale)
   end
 
   def page_feedback_params
     params.permit(:liked, :shared_on, :comment, :article_id).merge(
       session_id: session.id,
-      locale: I18n.locale
+      locale: I18n.locale,
+      slug: params[:article_id]
     )
+  end
+
+  def unprocessable
+    render json: {}, status: 422
   end
 end

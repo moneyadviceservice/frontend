5f7236f5d5fad2902852ced2b6fafe59a94751ab
Scott Riley <scott.riley111@gmail.com>
2018-02-06 15:11:25 +0000

    Addresses rubocop issues

--- features/step_definitions/money_manager_steps.rb
+++ features/step_definitions/money_manager_steps.rb
@@ -35,8 +35,12 @@ end
 
 Then(/^I should not see any hint of my details when I re-visit the tool$/) do
   step 'I select a country'
-  expect(money_manager_questionnaire_page.received_first_payment_true).not_to be_checked
-  expect(money_manager_questionnaire_page.received_first_payment_false).not_to be_checked
+  expect(
+    money_manager_questionnaire_page.received_first_payment_true
+  ).not_to be_checked
+  expect(
+    money_manager_questionnaire_page.received_first_payment_false
+  ).not_to be_checked
 end
 
 Then(/^I should be at the Money Manager landing page$/) do
@@ -45,13 +49,18 @@ end
 
 Then(/^I have the latest answers$/) do
   money_manager_circumstances_changed_page.load
-  expect(money_manager_circumstances_changed_page.received_first_payment_false).to be_checked
-  expect(money_manager_circumstances_changed_page.single_or_in_couple_couple).to be_checked
+  expect(
+    money_manager_circumstances_changed_page.received_first_payment_false
+  ).to be_checked
+  expect(
+    money_manager_circumstances_changed_page.single_or_in_couple_couple
+  ).to be_checked
 end
 
-
 Then(/^I see all of the advice$/) do
-  all_advice_count = money_manager_show_all_advice_page.all_advice_list_items.length
-  visible_advice_count = money_manager_show_all_advice_page.active_advice_list_items.length
-  expect(all_advice_count).to eq(visible_advice_count)
+  expect(
+    money_manager_show_all_advice_page.all_advice_list_items.length
+  ).to eq(
+    money_manager_show_all_advice_page.active_advice_list_items.length
+  )
 end

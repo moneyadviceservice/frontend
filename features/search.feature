c9375605467a6725881f77ca525a6b5d47a452e5
Tomas D'Stefano <tomas_stefano@successoft.com>
2018-04-12 13:57:50 +0100

    Add feature tests for site search including pagination

--- features/search.feature
+++ features/search.feature
@@ -1,13 +1,51 @@
-Feature: Searching
+Feature: Searching on MAS site
   As a user visiting the website
   I want to search by keyword
   So that I can find the content I am looking for
 
-  Scenario: The search box is visible in the header
-    When I visit the home page
-    Then I should see the search box
-
-  Scenario: Search can be performed
+  Background:
     Given I am on the home page
-    When I search for something relevant
-    Then I should see search results
+
+  Scenario: Search on first result page
+    When I search the query "money"
+    Then I should see "10" results
+    And I should see the search results
+      | title                                             |
+      | Money Manager tool for Universal Credit claimants |
+      | Money Advice Toolkits                             |
+      | Our board                                         |
+      | Money lives: Paul                                 |
+    And I should be on page "1" of "36" of the search results
+    And I should not see the previous button
+
+  Scenario: Search and navigate to the next result page
+    When I search the query "money"
+    And I go to the next results page
+    Then I should see "10" results
+    And I should see the search results
+      | title                          |
+      | Debt advice evaluation toolkit |
+    And I should be on page "2" of "36" of the search results
+
+  Scenario: Search and navigate to last result page
+    When I search the query "money"
+    And I go to the last page of results
+    Then I should be on page "36" of "36" of the search results
+    And I should see the search results
+      | title                                                        |
+      | Council Tax: what it is, what it costs and how to save money |
+    And I should not see the next button
+
+  Scenario: Search and navigate to a previous result page
+    When I search the query "money"
+    And I go to the last page of results
+    And I go to the previous results page
+    Then I should see "10" results
+    And I should see the search results
+      | title                                                     |
+      | UK could gain £108 billion from improved money management |
+    And I should be on page "35" of "36" of the search results
+
+  Scenario: Search with no results
+    When I search the query "Norris"
+    Then I should see no search results for "Norris"
--- features/step_definitions/search_page_steps.rb
+++ features/step_definitions/search_page_steps.rb
@@ -1,6 +1,25 @@
-When(/^I search for something relevant$/) do
-  home_page.search_box.input.set 'health'
+When('I search the query {string}') do |query|
+  home_page.search_box.input.set(query)
   home_page.search_box.submit.click
+end
+
+When('I go to the next results page') do
+  search_results_page.pagination.next_button.click
+end
+
+When(
+  'I go to results page {string} of the {string} search query'
+) do |page, query|
+  visit("/en/search?query=#{query}&page=#{page}")
+end
+
+When('I should not see the next button') do
+  expect(search_results_page.pagination).to_not have_next_button
+end
+
+When('I go to the previous results page') do
+  expect(search_results_page.pagination).to have_previous_button
+  search_results_page.pagination.previous_button.click
 end
 
 When(/^I search for something irrelevant$/) do
@@ -8,108 +27,61 @@ When(/^I search for something irrelevant$/) do
   home_page.search_box.submit.click
 end
 
-When(/^I search for a query that returns two pages of results$/) do
-  home_page.search_box.input.set '"When to use an insurance broker"'
-  home_page.search_box.submit.click
-end
-
-When(/^I submit a search with no query$/) do
+When("I submit a search with no query") do
   home_page.search_box.input.set ''
   home_page.search_box.submit.click
 end
 
-When(/^I search for an incorrectly spelt term with results$/) do
-  home_page.search_box.input.set('buget')
-  home_page.search_box.submit.click
+When("I go to the last page of results") do
+  step %{I go to results page "35" of the "money" search query}
 end
 
-When(/^I search for an incorrectly spelt term with no results$/) do
-  home_page.search_box.input.set('mouney')
-  home_page.search_box.submit.click
+Then("the search results page should have a robots tag with value noindex") do
+  expect { search_results_page.robots_tag[:content] }.to become('noindex')
 end
 
-Then(/^I should see the search box$/) do
-  expect(home_page).to have_search_box
+Then("I should see the search results") do |table|
+  results = search_results_page.results.map { |result| result.text }.join("\n")
+
+  table.rows.each do |row|
+    expect(results).to include(row[0])
+  end
 end
 
-Then(/^I should see the search page$/) do
-  expected_heading = ''
-  expected_title   = '%s - %s' % [I18n.t('search_results.index.document_title'),
-                                  I18n.t('layouts.base.title')]
-
-  expect(search_results_page.title).to eq(expected_title)
-  expect(search_results_page.heading).to have_content(expected_heading)
+When("I search for something relevant") do
+  step %{I search the query "money"}
 end
 
-Then(/^I should see search results$/) do
-  expected_heading = strip_tags(I18n.t('search_results.index_with_results.page_title_html', query: 'health'))
-  expected_title   = '%s - %s' % [I18n.t('search_results.index_with_results.document_title', query: 'health'),
-                                  I18n.t('layouts.base.title')]
+Then('I should see no search results for {string}') do |query|
+  expected_heading = strip_tags(
+    I18n.t('search_results.index_no_results.page_title_html', query: query)
+  )
 
-  expect(search_results_page.title).to eq(expected_title)
-  expect(search_results_page.heading).to have_content(expected_heading)
-  expect(search_results_page).to have_results
-end
+  document_title = I18n.t(
+    'search_results.index_no_results.document_title', query: query
+  )
+  base_title = I18n.t('layouts.base.title')
 
-Then(/^I should see no search results$/) do
-  expected_heading = strip_tags(I18n.t('search_results.index_no_results.page_title_html', query: 'tiger'))
-  expected_title   = '%s - %s' % [I18n.t('search_results.index_no_results.document_title', query: 'tiger'),
-                                  I18n.t('layouts.base.title')]
+  expected_title = "#{document_title} - #{base_title}"
 
   expect(search_results_page.title).to eq(expected_title)
   expect(search_results_page.heading).to have_content(expected_heading)
   expect(search_results_page).to have_no_results
 end
 
-Then(/^I should have search results$/) do
-  expect(search_results_page).to have_results
+Then(
+  'I should be on page {string} of {string} of the search results'
+) do |page, number_of_pages|
+  expect(
+    search_results_page.pagination.text
+  ).to include("Page #{page} of #{number_of_pages}")
 end
 
-Then(/^I should have corrected search results$/) do
-  expect(search_results_page.corrected_query).to have_content('money')
-  expect(search_results_page).to have_results
-end
-
-Then(/^I should prompted to try another search term$/) do
-  expect(search_results_page).to have_content(I18n.t('search_results.index_no_results.body'))
-end
-
-Then(/^the search results page should have a robots tag with value noindex$/) do
-  expect { search_results_page.robots_tag[:content] }.to become('noindex')
-end
-
-Then(/^I should see what page of results I am on$/) do
-  expect(search_results_page.pagination).to have_page_info
-end
-
-Then(/^I should see the "Next" button$/) do
-  expect(search_results_page.pagination).to have_next_button
-end
-
-Then(/^I should not see the "Next" button$/) do
-  expect(search_results_page.pagination).to_not have_next_button
-end
-
-Then(/^I should see the "Prev" button$/) do
-  expect(search_results_page.pagination).to have_previous_button
-end
-
-Then(/^I should not see the "Prev" button$/) do
+Then('I should not see the previous button') do
   expect(search_results_page.pagination).to_not have_previous_button
 end
 
-Then(/^I should see a spelling suggestion$/) do
-  expect(search_results_page).to have_spelling_suggestion
+Then('I should see {string} results') do |results_size|
+  expect(search_results_page.results.size).to be(results_size.to_i)
 end
 
-When(/^I go to the next page of results$/) do
-  search_results_page.pagination.next_button.click
-end
-
-When(/^I go to the sixth page of a query that returns five pages of results$/) do
-  visit(search_results_path(locale: I18n.locale, query: 'Health insurance', page: 6))
-end
-
-Then(/^I should be on page (\d+) of (\d+) of the search results$/) do |page, number_of_pages|
-  expect(search_results_page.pagination).to have_content "Page #{page} of #{number_of_pages}"
-end

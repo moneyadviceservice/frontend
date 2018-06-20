5cdc8f7e427f1e181d97fa9ee5e8e56f7c43a6fc
pmcostello <pmcostello@hotmail.co.uk>
2018-06-20 23:30:58 +0100

    add conditionals to data migrations

--- db/migrate/20180619090944_encrypt_dob.rb
+++ db/migrate/20180619090944_encrypt_dob.rb
@@ -1,9 +1,14 @@
 class EncryptDob < ActiveRecord::Migration
   def change
-    add_column :users, :encrypted_date_of_birth, :string
-    add_column :users, :encrypted_date_of_birth_iv, :string
-    rename_column :users, :date_of_birth, :date_of_birth_old
-
+    unless column_exists? :users, :encrypted_date_of_birth
+      add_column :users, :encrypted_date_of_birth, :string
+    end
+    unless column_exists? :users, :encrypted_date_of_birth_iv
+      add_column :users, :encrypted_date_of_birth_iv, :string
+    end
+    if column_exists? :users, :date_of_birth
+      rename_column :users, :date_of_birth, :date_of_birth_old
+    end
     User.reset_column_information
      User.find_each do |instance|
       #this will set the encrypted_reply based on attr_encrypted

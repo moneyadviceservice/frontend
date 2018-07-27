a3af6071bbb00418a99e7e152715dcd457c26a08
Margo Urey <margo@margonline.co.uk>
2018-07-27 16:32:08 +0100

    Add pacs engine to frontend
    - set the parent controller
    - mount the engine
    - create the route

---
+++ config/initializers/pacs.rb
@@ -0,0 +1,1 @@
+Pacs.parent_controller = '::PacsController'
---
+++ lib/tool_mount_point/pacs.rb
@@ -0,0 +1,6 @@
+module ToolMountPoint
+  class Pacs < Base
+    EN_ID = 'compare-bank-account-fees-and-charges'.freeze
+    CY_ID = 'welsh-for-compare-bank-account-fees-and-charges'.freeze
+  end
+end

dd57475ddb0fffb0056e38a5104d46404dce53eb
Tomas D'Stefano <tomas_stefano@successoft.com>
2018-10-18 13:26:15 +0100

    Add rake feed store to whenever

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,5 +1,3 @@
 every 1.day, at: '4:30 am' do
-  job_type :rake, "cd /srv/frontend/ && source /etc/mas/environment && RAILS_ENV=production bundle exec rake :task --silent :output"
-
-  rake 'feed:store' # from the PACs engine
+  command "cd /srv/frontend/ && source /etc/mas/environment && RAILS_ENV=production bundle exec rake feed:store"
 end

ec0bab9711d5ba7acc18dd0fd7e25b18a6b2580f
David Trussler <mail@davidtrussler.net>
2018-12-05 17:06:58 +0000

    Adds support for cucumber tests

---
+++ features/support/ui/sections/whatsapp.rb
@@ -0,0 +1,11 @@
+require_relative '../section'
+
+module UI::Sections
+  class Whatsapp < UI::Section
+    # element :button, '.t-chat-button'
+    # element :description, '.t-chat-description'
+    element :javascript_warning, '.t-whatsapp-javascript'
+    # element :opening_times, '.t-chat-opening-times'
+    # element :smallprint, '.t-welsh-smallprint'
+  end
+end

952998626e0e30d48d76713f58e06ab145d7362c
Jason Cowlam <jasoncowlam@gmail.com>
2019-01-15 11:51:23 +0000

    Adds final tests to the cucumbers

--- features/step_definitions/whatsapp_steps.rb
+++ features/step_definitions/whatsapp_steps.rb
@@ -1,21 +1,3 @@
-Then(/^I should be able to start a chat with an advisor via WhatsApp$/) do
-  expect(home_page.whatsapp.button).to have_content(I18n.t('contact_panels.chat.available.call_to_action'))
-  expect(home_page.whatsapp.description).to have_content(I18n.t('contact_panels.chat.available.description'))
-end
-
-Then(/^I should see a message informing me that I need JavaScript in order chat with an advisor via WhatsApp$/) do
-  expect(home_page.whatsapp).to have_javascript_warning
-end
-
-Then(/^I should not be able to start a chat with an advisor via WhatsApp$/) do
-  expect(home_page.whatsapp.button['class']).to include('is-disabled')
-end
-
-Then(/^I should see a message informing me that WhatsApp chat is currently busy$/) do
-  expect(home_page.whatsapp.button).to have_content(I18n.t('contact_panels.chat.busy.call_to_action'))
-  expect(home_page.whatsapp.description).to have_content(I18n.t('contact_panels.chat.busy.description'))
-end
-
 Then(/^I should see a message informing me that WhatsApp chat will be online between today's opening hours$/) do
   expect(home_page.whatsapp.description).to have_content(I18n.t('contact_panels.chat.offline.description', hours: '8am to 8pm'))
 end
@@ -28,15 +10,20 @@ Then(/^I should see a message informing me that WhatsApp chat is only available 
   expect(home_page.whatsapp).to have_smallprint
 end
 
-# May not include this one if too complex
-# When (/^I visit the website on a desktop device$/) do
-#   pending # Write code here that turns the phrase above into concrete actions
-#   expect(home_page.whatsapp).to have_javascript_warning
-#   print home_page.whatsapp
-#   home_page.load()
-#   save_and_open_page
-# end
+Then(/^I should see an unavailable message$/) do
+  expect(home_page.whatsapp.unavailable).to have_content(I18n.t('contact_panels.chat.unavailable.call_to_action'))
+end
 
-# Then(/^I should not see the WhatsApp chat$/) do
-#   pending # Write code here that turns the phrase above into concrete actions
-# end
+Then(/^I should see a whats app link$/) do
+  expect(home_page.whatsapp.link).to be_truthy
+end
+
+When(/^I visit the website on a desktop device$/) do
+  page.driver.resize_window 1200, 768
+end
+
+Then(/^I should not see the WhatsApp chat$/) do
+  expect(page).to_not have_css('.contact-panel__whatsapp')
+end
+
+

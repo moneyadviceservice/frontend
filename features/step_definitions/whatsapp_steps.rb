0133c23a57a9985661d2b94dffa73ac40229d468
Marc Sardon <pringaohowto@gmail.com>
2019-01-18 15:56:18 +0000

    Restrict Whatsapp integration to specific content
    Whatsapp footer on mobile devices should only appear on:
    - Debt and Borrowing content.
    - Pension and Retirement content.

--- features/step_definitions/whatsapp_steps.rb
+++ features/step_definitions/whatsapp_steps.rb
@@ -1,21 +1,29 @@
+When(/^I visit the category ([\w\-]+) in (English|Welsh)$/) do |category, language|
+  locale = language == 'Welsh' ? 'cy' : 'en'
+  category_page.load(locale: locale, id: category)
+end
+
+When(/^I visit a category with WhatsApp integration in (English|Welsh)$/) do |language|
+  step "I visit the category debt-and-borrowing in #{language}"
+end
+
 Then(/^I should see a message informing me that WhatsApp chat is only available in English$/) do
-  expect(home_page.whatsapp).to have_smallprint
+  expect(category_page.whatsapp).to have_smallprint
 end
 
 Then(/^I should see an unavailable message$/) do
-  expect(home_page.whatsapp.unavailable).to have_content(I18n.t('contact_panels.chat.unavailable.call_to_action'))
+  expect(category_page.whatsapp.unavailable).to have_content(I18n.t('contact_panels.chat.unavailable.call_to_action'))
 end
 
-Then(/^I should see a whats app link$/) do
-  expect(home_page.whatsapp.link).to be_truthy
+Then(/^I should see a WhatsApp link$/) do
+  expect(current_page.whatsapp.link).to be_truthy
 end
 
-When(/^I visit the website on a desktop device$/) do
+When(/^I visit a category with WhatsApp integration on a desktop device$/) do
   page.driver.resize_window 1200, 768
+  step "I visit a category with WhatsApp integration in English"
 end
 
 Then(/^I should not see the WhatsApp chat$/) do
-  expect(page).to_not have_css('.contact-panel__whatsapp')
+  expect(current_page).to_not have_css('.contact-panel__whatsapp')
 end
-
-

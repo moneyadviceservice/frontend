d2cdb812ed29d816c1b02989590aa731d1e61d28
andreferreiradlw <ferreirandre25@gmail.com>
2019-09-17 09:30:16 +0100

    Addressed PR Comments

--- app/controllers/concerns/chat.rb
+++ app/controllers/concerns/chat.rb
@@ -6,6 +6,7 @@ module Chat
       @chat_opening_hours ||=
         ChatOpeningHoursDecorator.decorate(Rails.application.config.chat_opening_hours)
     end
+    
     helper_method def pensions_opening_hours
       @pensions_opening_hours ||=
         ChatOpeningHoursDecorator.decorate(Rails.application.config.pensions_opening_hours)

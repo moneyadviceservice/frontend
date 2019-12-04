0ee1c13825a223676cdde200391f1b84fd3c9961
David Trussler <mail@davidtrussler.net>
2019-12-04 12:29:08 +0000

    Adds MAPS banner to MAS core site

--- spec/views/header_search_box_spec.rb
+++ spec/views/header_search_box_spec.rb
@@ -6,6 +6,7 @@ RSpec.describe 'shared/_header', type: :view do
     allow(view).to receive(:display_search_box_in_header?) { display }
     allow(view).to receive(:alternate_locales) { [] }
     allow(view).to receive(:hide_elements_irrelevant_for_third_parties?) { false }
+    allow(view).to receive(:show_home_banner?) { false }
   end
 
   context 'when the search box should be displayed in the header' do

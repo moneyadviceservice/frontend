71c80ad90e0e428d6667dce35934f20eda87e2cf
David Trussler <mail@davidtrussler.net>
2020-04-30 20:50:38 +0100

    Adds js and svg tags to html on load

--- app/assets/javascripts/application_empty.js
+++ app/assets/javascripts/application_empty.js
@@ -3,6 +3,11 @@
 require(['common', 'jquery'], function(MAS, $) {
   'use strict';
 
+	var html = document.getElementsByTagName('html')[0]; 
+
+	html.className = html.className.replace('no-js', 'js'); 
+	html.className = html.className + ' svg'; 
+
   require(['componentLoader'], function(componentLoader) {
     componentLoader.init($('body'));
   });

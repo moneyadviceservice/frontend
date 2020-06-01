3e4b84e373c5a677259ffae3360f7c2976715d45
David Trussler <mail@davidtrussler.net>
2020-06-01 22:16:51 +0100

    updateDOM adds buttons and active class to questions

--- app/assets/javascripts/components/MoneyNavigatorQuestions.js
+++ app/assets/javascripts/components/MoneyNavigatorQuestions.js
@@ -7,6 +7,7 @@ define(['jquery', 'DoughBaseComponent'], function($, DoughBaseComponent) {
     MoneyNavigatorQuestions.baseConstructor.call(this, $el, config);
 
     this.$submitBtn = this.$el.find('[data-submit]'); 
+    this.$questions = this.$el.find('[data-question]'); 
   };
 
   DoughBaseComponent.extend(MoneyNavigatorQuestions);
@@ -19,7 +20,24 @@ define(['jquery', 'DoughBaseComponent'], function($, DoughBaseComponent) {
   };
 
   MoneyNavigatorQuestions.prototype._updateDOM = function() {
+    // Removes submit button
     this.$submitBtn.remove(); 
+
+    for (var i = 0; i < this.$questions.length; i++) {
+      var question = this.$questions[i]; 
+
+      if (i === 0) {
+        // Adds get-started button to first question
+        $(question).find('.question__content').append('<button class="button button--start" data-get-started="true">Get started</button>'); 
+         // Adds active class to first question
+        $(question).addClass('question--active'); 
+     } else {
+        // Adds continue and back buttons to all other questions
+        $(question).find('.question__content')
+          .append('<button data-continue="true" class="button button--continue">Continue</button>')
+          .append('<button data-back="true" class="button button--back">Back</button>');
+      }
+    }; 
   }
 
   return MoneyNavigatorQuestions; 

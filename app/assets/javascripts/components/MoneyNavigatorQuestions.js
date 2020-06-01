ddb313dd0f095154495a2d4630bfd9fc8044b105
David Trussler <mail@davidtrussler.net>
2020-06-01 09:54:06 +0100

    Adds JS Component

---
+++ app/assets/javascripts/components/MoneyNavigatorQuestions.js
@@ -0,0 +1,19 @@
+define(['jquery', 'DoughBaseComponent'], function($, DoughBaseComponent) {
+  'use strict';
+
+  var MoneyNavigatorQuestions;
+  
+  MoneyNavigatorQuestions = function($el, config) {
+    MoneyNavigatorQuestions.baseConstructor.call(this, $el, config);
+  };
+
+  DoughBaseComponent.extend(MoneyNavigatorQuestions);
+
+  MoneyNavigatorQuestions.componentName = 'MoneyNavigatorQuestions';
+
+  MoneyNavigatorQuestions.prototype.init = function(initialised) {
+    this._initialisedSuccess(initialised);
+  };
+
+  return MoneyNavigatorQuestions; 
+}); 

cebc29abaa5227ac6f088fb56b72c858e81754f0
David Trussler <mail@davidtrussler.net>
2020-06-03 21:29:19 +0100

    Adds conditional logic for multiple-type questions

--- spec/javascripts/tests/MoneyNavigatorQuestions_spec.js
+++ spec/javascripts/tests/MoneyNavigatorQuestions_spec.js
@@ -26,12 +26,15 @@ describe.only('MoneyNavigatorQuestions', function() {
   describe('Initialisation', function() {
     it('Calls the correct methods when the component is initialised', function() {
       var updateDOMStub = sinon.stub(this.obj, '_updateDOM'); 
+      var setUpMultipleQestionsStub = sinon.stub(this.obj, '_setUpMultipleQuestions'); 
       
       this.obj.init();
 
       expect(updateDOMStub.calledOnce).to.be.true;
+      expect(setUpMultipleQestionsStub.calledOnce).to.be.true; 
 
       updateDOMStub.restore(); 
+      setUpMultipleQestionsStub.restore(); 
     });
   });
 
@@ -116,6 +119,60 @@ describe.only('MoneyNavigatorQuestions', function() {
 
       expect($(this.questions[0]).hasClass(this.activeClass)).to.be.true; 
       expect($(this.questions[1]).hasClass(this.activeClass)).to.be.false; 
+    }); 
+  }); 
+
+  describe('setUpMultipleQuestions method', function() {
+    beforeEach(function() {
+      var multipleQuestion = this.questions[1]; 
+
+      this.responses = $(multipleQuestion).find('input');
+      this.obj._setUpMultipleQuestions(); 
+    }); 
+
+    it('Adds the expected states when the method is called', function() {
+      expect(this.responses[0].checked).to.be.true; 
+      expect(this.responses[1].checked).to.be.false; 
+      expect(this.responses[2].checked).to.be.false; 
+
+      expect(this.responses[0].disabled).to.be.false; 
+      expect(this.responses[1].disabled).to.be.true; 
+      expect(this.responses[2].disabled).to.be.true; 
+    });
+
+    it('Calls the correct methods when the first checkbox state is changed', function() {
+      var updateMultipleQuestionsSpy = sinon.spy(this.obj, '_updateMultipleQuestions'); 
+      $(this.responses[0]).trigger('change'); 
+      expect(updateMultipleQuestionsSpy.calledWith(this.responses[0])).to.be.true; 
+      updateMultipleQuestionsSpy.restore(); 
+
+      var updateMultipleQuestionsSpy = sinon.spy(this.obj, '_updateMultipleQuestions'); 
+      $(this.responses[1]).trigger('change'); 
+      expect(updateMultipleQuestionsSpy.calledWith(this.responses[0])).to.be.false; 
+      updateMultipleQuestionsSpy.restore(); 
+    })
+  }); 
+
+  describe('updateMultipleQuestions method', function() {
+    it('Updates the state of inputs correctly when called', function() {
+      var multipleQuestion = this.questions[1]; 
+      var responses = $(multipleQuestion).find('input[type="checkbox"]');
+
+      this.obj._setUpMultipleQuestions(); 
+
+      responses[0].checked = false; 
+
+      this.obj._updateMultipleQuestions(responses[0]); 
+
+      expect(responses[1].disabled).to.be.false; 
+      expect(responses[2].disabled).to.be.false; 
+
+      responses[0].checked = true; 
+
+      this.obj._updateMultipleQuestions(responses[0]); 
+
+      expect(responses[1].disabled).to.be.true; 
+      expect(responses[2].disabled).to.be.true; 
     }); 
   }); 
 });

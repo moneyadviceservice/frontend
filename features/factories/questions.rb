cf9678e2eeb3dfc2f1d16ffd1a7a6f763e8ef834
Abraham Kiggundu <abraham@thekiggys.com>
2020-06-03 15:22:01 +0100

    [TP-11471] Completed entropy rich urgent action tests plus minor cleanup

--- features/factories/questions.rb
+++ features/factories/questions.rb
@@ -1,42 +1,67 @@
+include Symbols
+
+#TODO consider moving these methods into a module if needed for other fixtures. 
+#Also to prevent polution of the global namespace (though not sure that's important given the way Factorybot is coded)
+def randomn_answers(answers, allow_empty)
+  choice = answers.sample(rand(1..answers.length)) if  !allow_empty
+  choice = answers.sample(rand(0..answers.length)) if  allow_empty
+  choice
+end
+
+
+def answers_with_entropy(question_code, mandatory_set)
+  mandated = randomn_answers(mandatory_set, false) unless mandatory_set.empty?
+  mandated = [] if mandatory_set.empty?
+  optional = randomn_answers((0..QUESTIONS_HASH[question_code].length).to_a.map{|index| "a#{index}"}, true) 
+  answers = mandated | optional
+  answers = answers[0] if answers.length == 1
+  answers
+end
+
 FactoryBot.define do
   factory :questions, class: Questions do
     
     factory :answers_requiring_urgent_scotland_action, traits: [:scotland_urgent_action]
     factory :answers_requiring_urgent_england_action, traits: [:england_urgent_action]
+    factory :answers_requiring_urgent_ni_action, traits: [:ni_urgent_action]
+    factory :answers_requiring_urgent_wales_action, traits: [:wales_urgent_action]
 
     trait :scotland_urgent_action do
+      urgent_action
       q0 { 'a3' }
-      q1 { 'a1' }
-      q2 { 'a1' }
-      q3 { 'a1' }
-      q4 { 'a1' }
-      q5 { 'a1' }
-      q6 { ['a4', 'a5', 'a6'] }
-      q7 { ['a1', 'a2', 'a3', 'a4', 'a5', 'a6', 'a7', 'a8', 'a9'] }
-      q8 { ['a1'] }
-      q9 { ['a1'] }
-      q10 { 'a3' }
-      q11 { 'a1' }
-      q12 { 'a1' }
-      q13 { ['a1'] }
-      q14 { 'a1' }
     end
+
     trait :england_urgent_action do
+      urgent_action
       q0 { 'a1' }
-      q1 { 'a1' }
-      q2 { 'a2' }
-      q3 { 'a1' }
-      q4 { 'a1' }
-      q5 { 'a1' }
-      q6 { ['a4', 'a5', 'a6'] }
-      q7 { ['a1', 'a2', 'a3', 'a4', 'a5', 'a6', 'a7', 'a8', 'a9'] }
-      q8 { ['a1', 'a2'] }
-      q9 { ['a1'] }
-      q10 { 'a3' }
-      q11 { 'a1' }
-      q12 { ['a1', 'a3'] }
-      q13 { 'a1' }
-      q14 { 'a1' }
     end
+    
+    trait :wales_urgent_action do
+      urgent_action
+      q0 { 'a4' }
+    end
+
+    trait :northerv_ireland_urgent_action do
+      urgent_action
+      q0 { 'a2' }
+    end
+
+    trait :urgent_action do
+      q1 { answers_with_entropy('q1', [])  }
+      q2 { answers_with_entropy('q2', [])  }
+      q3 { answers_with_entropy('q3', [])  }
+      q4 { answers_with_entropy('q4', ['a1'])  }
+      q5 { answers_with_entropy('q5', [])  }
+      q6 { answers_with_entropy('q6', (4..6).to_a.map{ |index| "a#{index}"  }) }
+      q7 { answers_with_entropy('q7', (1..9).to_a.map{ |index| "a#{index}"  }) }
+      q8 { answers_with_entropy('q8', [])  }
+      q9 { answers_with_entropy('q9', [])  }
+      q10 { answers_with_entropy('q10', ['a3'] ) }
+      q11 { answers_with_entropy('q11', [])  }
+      q12 { answers_with_entropy('q12', [])  }
+      q13 { answers_with_entropy('q13', [])  }
+      q14 { answers_with_entropy('q14', [])  }
+    end
+
   end
 end
--- spec/models/questions_spec.rb
+++ spec/models/questions_spec.rb
@@ -1,25 +1,84 @@
 RSpec.describe Questions, type: :model do
-  subject { build(:answers_requiring_urgent_england_action) }
+  include Symbols
 
-  it '(returns results prompting for urgent england action' do 
-    expect(subject.results).to eq 'H1'
-  end
+  context 'Urgent action' do
+    let(:urgent_action_section_code) { 'S1' }
+    let(:urgent_action_heading_code) { 'H1' }
 
-  def england_seek_advice_answers
-      {"q0"=>"a1",
-   "q1"=>"a1",
-   "q2"=>"a2",
-   "q3"=>"a1",
-   "q4"=>"a1",
-   "q5"=>"a1",
-   "q6"=>["a1, a6"],
-   "q7"=>["a4"],
-   "q8"=>["a2"],
-   "q9"=>["a10"],
-   "q10"=>"a3",
-   "q11"=>"a2",
-   "q12"=>["a1"],
-   "q13"=>"a2",
-   "q14"=>"a1"}
+    describe 'submissions from england' do
+      let(:model) { build(:answers_requiring_urgent_england_action) }
+
+      it 'should only display the one urgent action section containing only England content' do
+        results = model.results
+        expect(results.length).to be 1
+        expect(results[0])
+          .to include(
+                      {
+                        "section_code"=>"S1",
+                        "headings"=>[
+                          ["coronavirus-debt-advice-england"]
+                        ]
+                      }
+                     )
+      end
+
+    end
+
+    describe 'submissions from Wales' do
+      let(:model) { build(:answers_requiring_urgent_wales_action) }
+
+      it 'should only display the one urgent action section containing only Wales content' do
+        results = model.results
+        expect(results.length).to be 1
+        expect(results[0])
+          .to include(
+                      {
+                        "section_code"=>"S1",
+                        "headings"=>[
+                          ["coronavirus-debt-advice-wales"]
+                        ]
+                      }
+                     )
+      end
+
+    end
+
+    describe 'submissions from Northern Ireland' do
+      let(:model) { build(:answers_requiring_urgent_ni_action) }
+
+      it 'should only display the one urgent action section containing only Northern Ireland content' do
+        results = model.results
+        expect(results.length).to be 1
+        expect(results[0])
+          .to include(
+                      {
+                        "section_code"=>"S1",
+                        "headings"=>[
+                          ["coronavirus-debt-advice-ni"]
+                        ]
+                      }
+                     )
+      end
+
+    end
+
+    describe 'submissions from Scotland' do
+      let(:model) { build(:answers_requiring_urgent_scotland_action) }
+
+      it 'should only display the one urgent action section containing only Scotland content' do
+        results = model.results
+        expect(results.length).to be 1
+        expect(results[0])
+          .to include(
+                      {
+                        "section_code"=>"S1",
+                        "headings"=>[
+                          ["coronavirus-debt-advice-scotland"]
+                        ]
+                      }
+                     )
+      end
+
+    end
   end
 end

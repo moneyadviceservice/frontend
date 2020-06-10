1befee99cb24a2cb8038bf0343023891a582f0ea
David Trussler <mail@davidtrussler.net>
2020-06-10 17:04:33 +0100

    Adds behaviour/styles for headings - positions content for hidden state - adds overlay for heading content - adds overlay hide icon to section content
    - adds show/hide overlay

--- app/assets/javascripts/components/MoneyNavigatorResults.js
+++ app/assets/javascripts/components/MoneyNavigatorResults.js
@@ -10,7 +10,9 @@ define(['jquery', 'DoughBaseComponent'], function($, DoughBaseComponent) {
     this.$sections = this.$el.find('[data-section]'); 
     this.$sectionTitles = this.$el.find('[data-section-title]'); 
     this.$headingTitles = this.$el.find('[data-heading-title]'); 
-    this.hiddenClass = 'is-hidden'; 
+    this.$overlay = this.$el.find('[data-overlay]'); 
+    this.hiddenSuffix = '--hidden'; 
+    this.hiddenClass = 'is-hidden';
     this.collapsedClass = 'is-collapsed'; 
     this.doneClass = 'is-done'; 
   };
@@ -29,12 +31,18 @@ define(['jquery', 'DoughBaseComponent'], function($, DoughBaseComponent) {
     var _this = this; 
     var sectionIcon = document.createElement('span'); 
     var headingTitleIcon = document.createElement('span'); 
+    var closeIcon = document.createElement('span'); 
 
     $(sectionIcon).addClass('section__title__icon'); 
     $(headingTitleIcon).addClass('heading__title__icon'); 
+    $(closeIcon)
+      .attr('data-overlay-hide', true)
+      .addClass('overlay__hide'); 
 
     // Adds hidden classes to headings content
-    this.$headingContent.addClass(this.hiddenClass); 
+    this.$headingContent
+      .addClass('heading__content' + this.hiddenSuffix)
+      .append(closeIcon); 
 
     // Adds collapsed classes to sections
     this.$sections.addClass(this.collapsedClass); 
@@ -54,7 +62,11 @@ define(['jquery', 'DoughBaseComponent'], function($, DoughBaseComponent) {
     }); 
 
     this.$headingTitles.find('button').on('click', function(e) {
-      _this._toggleHeading(e.target); 
+      _this._showHeading(e.target); 
+    }); 
+
+    this.$headingContent.find('[data-overlay-hide]').on('click', function(e) {
+      _this._hideHeading(e.target); 
     }); 
   };
 
@@ -68,20 +80,39 @@ define(['jquery', 'DoughBaseComponent'], function($, DoughBaseComponent) {
     }
   }; 
 
-  MoneyNavigatorResults.prototype._toggleHeading = function(btn) {
+  MoneyNavigatorResults.prototype._showHeading = function(btn) {
     var $heading = $(btn).parents('[data-heading]'); 
     var $headingContent = $heading.find('[data-heading-content]'); 
+    var hiddenClass = 'heading__content' + this.hiddenSuffix; 
 
     if (!$heading.hasClass(this.doneClass)) {
       $heading.addClass(this.doneClass); 
     }
 
-    if ($headingContent.hasClass(this.hiddenClass)) {
-      $headingContent.removeClass(this.hiddenClass); 
-    } else {
-      $headingContent.addClass(this.hiddenClass); 
+    if ($headingContent.hasClass(hiddenClass)) {
+      $headingContent.removeClass(hiddenClass); 
+    }
+
+    if (this.$overlay.hasClass(this.hiddenClass)) {
+      this.$overlay.removeClass(this.hiddenClass); 
     }
   }; 
+
+  MoneyNavigatorResults.prototype._hideHeading = function(btn) {
+    console.log('_hideHeading!'); 
+    console.log('btn: ', btn); 
+
+    var $headingContent = $(btn).parents('[data-heading-content]'); 
+    var hiddenClass = 'heading__content' + this.hiddenSuffix; 
+
+    if (!$headingContent.hasClass(hiddenClass)) {
+      $headingContent.addClass(hiddenClass); 
+    }
+
+    if (!this.$overlay.hasClass(this.hiddenClass)) {
+      this.$overlay.addClass(this.hiddenClass); 
+    }
+  }
 
   return MoneyNavigatorResults; 
 }); 

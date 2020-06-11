3cdafa23b56c30a1310158c8f5ee0498dc0ee8c4
Abraham Kiggundu <abraham@thekiggys.com>
2020-06-11 15:21:08 +0100

    [TP-11471] Section 8 completed. No Tests yet.

--- app/models/concerns/symbols.rb
+++ app/models/concerns/symbols.rb
@@ -1949,6 +1949,51 @@ module Symbols
       ]
     },
 
+    {
+      #'Borrowing money' section
+      section_code: 'S8',
+      heading_rules: [
+        {
+          #'If you're thinking of borrowing money' Rules
+          heading_code: 'H8.1',
+          content_rules: [
+            #Q11A1, Q4A1
+            {
+              triggers: [
+                {q4: 'a1', q11: 'a1' },
+              ],
+              mask: MASK_ALL,
+              article: "coronavirus-thinking-of-borrowing-severe"
+            },
+            #Q11A1, Q4A2
+            {
+              triggers: [
+                {q4: 'a2', q11: 'a1' },
+              ],
+              mask: MASK_ALL,
+              article: "coronavirus-thinking-of-borrowing-temp-worried"
+            },
+            #Q11A1, Q4A3
+            {
+              triggers: [
+                {q4: 'a3', q11: 'a1' },
+              ],
+              mask: MASK_ALL,
+              article: "coronavirus-thinking-of-borrowing-temp-normal"
+            },
+            #Q11A1, Q4A4
+            {
+              triggers: [
+                {q4: 'a4', q11: 'a1' },
+              ],
+              mask: MASK_ALL,
+              article: "coronavirus-thinking-of-borrowing-no-change"
+            },
+          ]
+        },
+      ]
+    },
+
   ]
 end
 

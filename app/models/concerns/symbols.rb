fcdfa28a04177e323e5c36ed162ad814ffe3e305
Abraham Kiggundu <abraham@thekiggys.com>
2020-06-11 15:32:47 +0100

    [TP-11471] Section 9 completed. No Tests yet

--- app/models/concerns/symbols.rb
+++ app/models/concerns/symbols.rb
@@ -1994,6 +1994,57 @@ module Symbols
       ]
     },
 
+    {
+      #'Using pensions, equity and savings' section
+      section_code: 'S9',
+      heading_rules: [
+        {
+          #' If you’re thinking about using your pension pot to pay off debts ' Rules
+          heading_code: 'H9.1',
+          content_rules: [
+            #Q12A2
+            {
+              triggers: [
+                {q12: 'a2'},
+              ],
+              mask: MASK_ALL,
+              article: "coronavirus-pensions-debt"
+            },
+          ]
+        },
+
+        {
+          #' If you’re thinking about using equity in your home to pay off debts ' Rules
+          heading_code: 'H9.2',
+          content_rules: [
+            #Q12A1 or Q12A3
+            {
+              triggers: [
+                {q12: 'a1', q12: 'a3'},
+              ],
+              mask: MASK_SOME,
+              article: "coronavirus-equity-mortage-debt"
+            },
+          ]
+        },
+
+        {
+          #'If you're thinking of using your savings to pay off debts' Rules
+          heading_code: 'H9.3',
+          content_rules: [
+            #Q12A4
+            {
+              triggers: [
+                {q12: 'a4'},
+              ],
+              mask: MASK_ALL,
+              article: "coronavirus-savings-debt"
+            },
+          ]
+        },
+      ]
+    },
+
   ]
 end
 

7eb8c1bf8a57371ab9485e8a3defbc6efdc91062
Abraham Kiggundu <abraham@thekiggys.com>
2020-08-04 10:23:03 +0100

    [TP-11529] Updates to complete stepchange redirect test

--- features/factories/questions.rb
+++ features/factories/questions.rb
@@ -39,10 +39,11 @@ end
 FactoryBot.define do
   factory :questions, class: Questions do
 
-    factory :urgent_action_debt_advice, traits: [:country, :urgent_debt_advice_action]
-    factory :urgent_action_stepchange_debt, traits: [:country, :urgent_stepchange_action]
-    factory :urgent_action_self_employed_debt_advice, traits: [:country, :urgent_debtline_action]
-    factory :urgent_action_urgent_pension_advice, traits: [:country, :pension]
+    factory :S1_H1_debt_advice, traits: [:country, :S1_H1_debt_advice_answers]
+    factory :S1_H2_stepchange_redirect_to_self_employed_debt_advice, traits: [:country, :S1_H2_stepchange_redirect_to_self_employed_debt_advice_answers ]
+    factory :S1_H2_stepchange_debt, traits: [:country, :urgent_stepchange_action]
+    factory :S1_H3_self_employed_debt_advice, traits: [:country, :urgent_debtline_action]
+    factory :S1_H4_urgent_pension_advice, traits: [:country, :pension]
 
     country_answer_codes = HashWithIndifferentAccess.new(england: 'a1', ni: 'a2', scotland: 'a3', wales: 'a4')
 
@@ -55,7 +56,7 @@ FactoryBot.define do
     end
 
     #Any of these Q4A1, Q6A6, Q7A1-A9, Q10A3 PLUS the regional variation
-    trait :urgent_debt_advice_action do
+    trait :S1_H1_debt_advice_answers do
       q1 { answers_with_entropy('q1', [], ['a1', 'a3', 'a4'])  }
       q2 { answers_with_entropy('q2', [], nil)  }
       q3 { answers_with_entropy('q3', [], nil)  }
@@ -66,6 +67,23 @@ FactoryBot.define do
       q8 { answers_with_entropy('q8', [], nil)  }
       q9 { answers_with_entropy('q9', [], nil)  }
       q10 { answers_with_entropy('q10', ['a3'], nil ) }
+      q11 { answers_with_entropy('q11', [], nil)  }
+      q12 { answers_with_entropy('q12', [], nil)  }
+      q13 { answers_with_entropy('q13', [], nil)  }
+      q14 { answers_with_entropy('q14', [], nil)  }
+    end
+
+    trait :S1_H2_stepchange_redirect_to_self_employed_debt_advice_answers do
+      q1 { answers_with_entropy('q1', ['a2'], [])  }
+      q2 { answers_with_entropy('q2', [], nil)  }
+      q3 { answers_with_entropy('q3', [], nil)  }
+      q4 { answers_with_entropy('q4', ['a2', 'a3'], nil )}
+      q5 { answers_with_entropy('q5', [], nil)  }
+      q6 { answers_with_entropy('q6', ['a4', 'a5'], ['a1', 'a2', 'a3', 'a7'])}
+      q7 { answers_with_entropy('q7', [], ['a10'] )}
+      q8 { answers_with_entropy('q8', [], nil) }
+      q9 { answers_with_entropy('q9', ['a1', 'a2', 'a3', 'a4', 'a5', 'a6', 'a7', 'a8', 'a9', 'a10', 'a11'], nil)  }
+      q10 { answers_with_entropy('q10', ['a1'], ['a2'] ) }
       q11 { answers_with_entropy('q11', [], nil)  }
       q12 { answers_with_entropy('q12', [], nil)  }
       q13 { answers_with_entropy('q13', [], nil)  }

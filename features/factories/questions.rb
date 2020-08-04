85e9ba568d55ef281c74eea1551098ea76f7cae9
Abraham Kiggundu <abraham@thekiggys.com>
2020-08-04 17:23:10 +0100

    [TP-11529] Added stepchange england tests and adjusted test data to better reflect the form and avoid the issue where  fails if all are selected

--- features/factories/questions.rb
+++ features/factories/questions.rb
@@ -42,6 +42,7 @@ FactoryBot.define do
     factory :S1_H1_debt_advice, traits: [:country, :S1_H1_debt_advice_answers]
     factory :S1_H2_stepchange_redirect_to_self_employed_debt_advice, traits: [:country, :S1_H2_stepchange_redirect_to_self_employed_debt_advice_answers ]
     factory :S1_H2_stepchange_redirect_to_self_employed_debt_advice_ni, traits: [:country, :S1_H2_stepchange_redirect_to_self_employed_debt_advice_answers_ni ]
+    factory :S1_H2_stepchange_debt_england, traits: [:country, :S1_H2_stepchange_debt_england_answers ]
     factory :S1_H2_stepchange_debt, traits: [:country, :urgent_stepchange_action]
     factory :S1_H3_self_employed_debt_advice, traits: [:country, :urgent_debtline_action]
     factory :S1_H4_urgent_pension_advice, traits: [:country, :pension]
@@ -79,8 +80,8 @@ FactoryBot.define do
       q1 { answers_with_entropy('q1', ['a2'], [])  }
       q2 { answers_with_entropy('q2', [], nil)  }
       q3 { answers_with_entropy('q3', [], ['a1', 'a3'])  }
-      q4 { answers_with_entropy('q4', ['a2', 'a3'], ['a4'] )}
-      q5 { answers_with_entropy('q5', [], ['a1', 'a2'])  }
+      q4 { answers_with_entropy('q4', ['a2', 'a3'], [] )}
+      q5 { answers_with_entropy('q5', [], nil ) }
       q6 { answers_with_entropy('q6', ['a4', 'a5'], ['a1', 'a2', 'a3', 'a7'])}
       q7 { answers_with_entropy('q7', [], ['a10'] )}
       q8 { answers_with_entropy('q8', [], nil) }
@@ -109,6 +110,22 @@ FactoryBot.define do
       q14 { answers_with_entropy('q14', [], nil)  }
     end
 
+    trait :S1_H2_stepchange_debt_england_answers do
+      q1 { answers_with_entropy('q1', ['a1', 'a3', 'a4'], [])  }
+      q2 { answers_with_entropy('q2', [], nil)  }
+      q3 { answers_with_entropy('q3', ['a1'], [])  }
+      q4 { answers_with_entropy('q4', ['a2'], [] )}
+      q5 { answers_with_entropy('q5', [], nil)  }
+      q6 { answers_with_entropy('q6', ['a4'], [])}
+      q7 { answers_with_entropy('q7', [], ['a10'] )}
+      q8 { answers_with_entropy('q8', [], nil) }
+      q9 { answers_with_entropy('q9', ['a1', 'a2', 'a3', 'a4', 'a5', 'a6', 'a7', 'a8', 'a9', 'a10', 'a11'], nil)  }
+      q10 { answers_with_entropy('q10', ['a1'], ['a2'] ) }
+      q11 { answers_with_entropy('q11', [], nil)  }
+      q12 { answers_with_entropy('q12', [], nil)  }
+      q13 { answers_with_entropy('q13', [], nil)  }
+      q14 { answers_with_entropy('q14', [], nil)  }
+    end
     #Any of these Q3A1, Q4A2, Q4A3, Q6A4, Q6A5, Q9A1-A11, Q10A1  BUT NOT IF HAVE ALSO SELECTED Q4A1, Q6A6, Q7A1-A9, Q10A3
     trait :urgent_stepchange_action do
       q1 { answers_with_entropy('q1', [], nil)  }

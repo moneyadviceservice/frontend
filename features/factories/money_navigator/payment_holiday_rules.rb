cee9f01c58158905131c0a8e06e45fba8ec347e6
Abraham Kiggundu <abraham@thekiggys.com>
2020-08-05 16:41:50 +0100

    [TP-11529] Added first holiday payments test with associated DRY refactor

---
+++ features/factories/money_navigator/payment_holiday_rules.rb
@@ -0,0 +1,37 @@
+include Symbols
+require_relative 'rules_factory_common'
+
+FactoryBot.define do
+  factory :payment_holiday_rule_answers, class: Questions do
+
+    factory :S2_H1_holiday_mortgage_payment_severe, traits: [:country, :S2_H1_holiday_mortgage_payment_severe_answers]
+
+    country_answer_codes = HashWithIndifferentAccess.new(england: 'a1', ni: 'a2', scotland: 'a3', wales: 'a4')
+
+    transient do
+      target_region {[ 'england' ]}
+    end
+
+    trait :country do
+      q0 { answers_with_entropy('q0', target_region.map {|country| country_answer_codes[country]}, []) }
+    end
+
+    trait :S2_H1_holiday_mortgage_payment_severe_answers do
+      q1 { answers_with_entropy('q1', [], nil)  }
+      q2 { answers_with_entropy('q2', [], nil)  }
+      q3 { answers_with_entropy('q3', [], nil)  }
+      q4 { answers_with_entropy('q4', ['a1'], [])}
+      q5 { answers_with_entropy('q5', [], nil)  }
+      q6 { answers_with_entropy('q6', [], nil)}
+      q7 { answers_with_entropy('q7', [], nil )}
+      q8 { answers_with_entropy('q8', ['a1'], [])  }
+      q9 { answers_with_entropy('q9', [], nil) }
+      q10 { answers_with_entropy('q10', [], nil) }
+      q11 { answers_with_entropy('q11', [], nil)  }
+      q12 { answers_with_entropy('q12', [], nil)  }
+      q13 { answers_with_entropy('q13', [], nil)  }
+      q14 { answers_with_entropy('q14', [], nil)  }
+    end
+
+  end
+end

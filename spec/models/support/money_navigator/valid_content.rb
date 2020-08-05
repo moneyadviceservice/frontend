451d04a05d8fea840278ea9eb2980592440e2d6c
Abraham Kiggundu <abraham@thekiggys.com>
2020-08-05 12:38:30 +0100

    [TP-11529] Refactor to avoid placing all tests in one long file

---
+++ spec/models/support/money_navigator/valid_content.rb
@@ -0,0 +1,18 @@
+module MoneyNavigator::ValidContent
+
+  shared_examples 'valid content' do
+    it 'displays the appropriate heading and content' do
+      allow_any_instance_of(Questions).to receive(:cms_content) {|me, slug| "#{ slug }"}
+      results =  model.results
+      expect(results).to include({
+        "section_code" => section_code,
+        "headings" => array_including(
+          hash_including({
+            "heading_code"=>heading_code,
+            "content"=>hash_including({url: "#{url}"})
+          })
+        )
+      })
+    end
+  end
+end

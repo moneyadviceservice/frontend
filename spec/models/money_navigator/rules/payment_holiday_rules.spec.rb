99873b5ea320c68bfd47f4db5dec47bf652e802f
Abraham Kiggundu <abraham@thekiggys.com>
2020-08-05 17:27:51 +0100

    [TP-11529] Added Mortgage payment holidays rule tests

--- spec/models/money_navigator/rules/payment_holiday_rules.spec.rb
+++ spec/models/money_navigator/rules/payment_holiday_rules.spec.rb
@@ -1,26 +1,56 @@
 
-  RSpec.describe Questions, type: :model do
-    include ::Symbols
+RSpec.shared_examples 'payments common' do 
+          let(:model) { build("#{section_code}_#{heading_code}_#{content_prefix}".gsub('-', '_').to_sym, target_region: region ) }
+          let(:url) { "coronavirus-#{content_prefix}" }
+end
 
-    #TODO: These are all positive tests. need to add negative tests
-    #(i.e. test that checks content is not displayed under a condition that shoud result in it not displaying)
+RSpec.describe Questions, type: :model do
+  include ::Symbols
 
-    context 'Rule test: ' do
-      context 'Payment Holidays' do
-        let(:section_code) { 'S2' }
+  #TODO: These are all positive tests. need to add negative tests
+  #(i.e. test that checks content is not displayed under a condition that shoud result in it not displaying)
 
-        [[ 'england', 'wales', 'scotland', 'ni'] ].each do |region|
-          describe  "free debt advice #{region}" do
-            let(:heading_code) { 'H1' }
-            let(:content_prefix) {"holiday-mortgage-payment-severe"}
-            let(:model) { build("#{section_code}_#{heading_code}_#{content_prefix}".gsub('-', '_').to_sym, target_region: region ) }
-            let(:url) { "coronavirus-#{content_prefix}" }
+  context 'Rule test: ' do
+    context 'Payment Holidays' do
+      let(:section_code) { 'S2' }
 
-            include_examples 'valid content'
-          end
+      [[ 'england', 'wales', 'scotland', 'ni'] ].each do |rgn|
+        let(:heading_code) { 'H1' }
+
+        describe  "Mortgage severe #{rgn}" do
+          let(:region) { rgn }
+          let(:content_prefix) {"holiday-mortgage-payment-severe"}
+
+          include_examples 'payments common'
+          include_examples 'valid content'
         end
 
+        describe  "Mortgage temp worried #{rgn}" do
+          let(:region) { rgn }
+          let(:content_prefix) {"holiday-mortgage-payment-temp-worried"}
+
+          include_examples 'payments common'
+          include_examples 'valid content'
+        end
+
+        describe  "Mortgage temp normal #{rgn}" do
+          let(:region) { rgn }
+          let(:content_prefix) {"holiday-mortgage-payment-temp-normal"}
+
+          include_examples 'payments common'
+          include_examples 'valid content'
+        end
+
+        describe  "Mortgage temp no change #{rgn}" do
+          let(:region) { rgn }
+          let(:content_prefix) {"holiday-mortgage-payment-no-change"}
+
+          include_examples 'payments common'
+          include_examples 'valid content'
+        end
       end
 
     end
+
   end
+end

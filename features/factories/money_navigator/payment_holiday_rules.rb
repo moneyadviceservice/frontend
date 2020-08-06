b3fe2e3f294950463bbadfc00c0a20f07eb22b40
Abraham Kiggundu <abraham@thekiggys.com>
2020-08-06 15:38:54 +0100

    [TP-11529] Added Payday loan tests

--- features/factories/money_navigator/payment_holiday_rules.rb
+++ features/factories/money_navigator/payment_holiday_rules.rb
@@ -39,6 +39,11 @@ FactoryBot.define do
     factory :S2_H7_holiday_rent_to_own_temp_normal, traits: [:country, :S2_H7_holiday_rent_to_own_temp_normal_answers]
     factory :S2_H7_holiday_rent_to_own_no_change, traits: [:country, :S2_H7_holiday_rent_to_own_no_change_answers]
 
+    factory :S2_H8_holiday_payday_loan_severe, traits: [:country, :S2_H8_holiday_payday_loan_severe_answers]
+    factory :S2_H8_holiday_payday_loan_temp_worried, traits: [:country, :S2_H8_holiday_payday_loan_temp_worried_answers]
+    factory :S2_H8_holiday_payday_loan_temp_normal, traits: [:country, :S2_H8_holiday_payday_loan_temp_normal_answers]
+    factory :S2_H8_holiday_payday_loan_no_change, traits: [:country, :S2_H8_holiday_payday_loan_no_change_answers]
+
     trait :S2_H1_holiday_mortgage_payment_severe_answers do
       q1 { answers_with_entropy('q1', [], nil)  }
       q2 { answers_with_entropy('q2', [], nil)  }
@@ -507,6 +512,74 @@ FactoryBot.define do
       q6 { answers_with_entropy('q6', [], nil)}
       q7 { answers_with_entropy('q7', [], nil )}
       q8 { answers_with_entropy('q8', ['a7'], [])  }
+      q9 { answers_with_entropy('q9', [], nil) }
+      q10 { answers_with_entropy('q10', [], nil) }
+      q11 { answers_with_entropy('q11', [], nil)  }
+      q12 { answers_with_entropy('q12', [], nil)  }
+      q13 { answers_with_entropy('q13', [], nil)  }
+      q14 { answers_with_entropy('q14', [], nil)  }
+    end
+
+    trait :S2_H8_holiday_payday_loan_severe_answers do
+      q1 { answers_with_entropy('q1', [], nil)  }
+      q2 { answers_with_entropy('q2', [], nil)  }
+      q3 { answers_with_entropy('q3', [], nil)  }
+      q4 { answers_with_entropy('q4', ['a1'], [])}
+      q5 { answers_with_entropy('q5', [], nil)  }
+      q6 { answers_with_entropy('q6', [], nil)}
+      q7 { answers_with_entropy('q7', [], nil )}
+      q8 { answers_with_entropy('q8', ['a8'], [])  }
+      q9 { answers_with_entropy('q9', [], nil) }
+      q10 { answers_with_entropy('q10', [], nil) }
+      q11 { answers_with_entropy('q11', [], nil)  }
+      q12 { answers_with_entropy('q12', [], nil)  }
+      q13 { answers_with_entropy('q13', [], nil)  }
+      q14 { answers_with_entropy('q14', [], nil)  }
+    end
+
+    trait :S2_H8_holiday_payday_loan_temp_worried_answers do
+      q1 { answers_with_entropy('q1', [], nil)  }
+      q2 { answers_with_entropy('q2', [], nil)  }
+      q3 { answers_with_entropy('q3', [], nil)  }
+      q4 { answers_with_entropy('q4', ['a2'], [])}
+      q5 { answers_with_entropy('q5', [], nil)  }
+      q6 { answers_with_entropy('q6', [], nil)}
+      q7 { answers_with_entropy('q7', [], nil )}
+      q8 { answers_with_entropy('q8', ['a8'], [])  }
+      q9 { answers_with_entropy('q9', [], nil) }
+      q10 { answers_with_entropy('q10', [], nil) }
+      q11 { answers_with_entropy('q11', [], nil)  }
+      q12 { answers_with_entropy('q12', [], nil)  }
+      q13 { answers_with_entropy('q13', [], nil)  }
+      q14 { answers_with_entropy('q14', [], nil)  }
+    end
+
+    trait :S2_H8_holiday_payday_loan_temp_normal_answers do
+      q1 { answers_with_entropy('q1', [], nil)  }
+      q2 { answers_with_entropy('q2', [], nil)  }
+      q3 { answers_with_entropy('q3', [], nil)  }
+      q4 { answers_with_entropy('q4', ['a3'], [])}
+      q5 { answers_with_entropy('q5', [], nil)  }
+      q6 { answers_with_entropy('q6', [], nil)}
+      q7 { answers_with_entropy('q7', [], nil )}
+      q8 { answers_with_entropy('q8', ['a8'], [])  }
+      q9 { answers_with_entropy('q9', [], nil) }
+      q10 { answers_with_entropy('q10', [], nil) }
+      q11 { answers_with_entropy('q11', [], nil)  }
+      q12 { answers_with_entropy('q12', [], nil)  }
+      q13 { answers_with_entropy('q13', [], nil)  }
+      q14 { answers_with_entropy('q14', [], nil)  }
+    end
+
+    trait :S2_H8_holiday_payday_loan_no_change_answers do
+      q1 { answers_with_entropy('q1', [], nil)  }
+      q2 { answers_with_entropy('q2', [], nil)  }
+      q3 { answers_with_entropy('q3', [], nil)  }
+      q4 { answers_with_entropy('q4', ['a4'], [])}
+      q5 { answers_with_entropy('q5', [], nil)  }
+      q6 { answers_with_entropy('q6', [], nil)}
+      q7 { answers_with_entropy('q7', [], nil )}
+      q8 { answers_with_entropy('q8', ['a8'], [])  }
       q9 { answers_with_entropy('q9', [], nil) }
       q10 { answers_with_entropy('q10', [], nil) }
       q11 { answers_with_entropy('q11', [], nil)  }

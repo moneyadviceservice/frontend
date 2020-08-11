7b4a092b52125f49c23144b05939391f8f2e043c
Abraham Kiggundu <abraham@thekiggys.com>
2020-08-11 17:27:29 +0100

    [TP-11529] Added the 4 Borrowing money section rule tests

---
+++ spec/models/money_navigator/rules/borrowing_money_rules.spec.rb
@@ -0,0 +1,41 @@
+
+RSpec.describe Questions, type: :model do
+  #TODO: Using global smbols is bad.... do not use. fix this
+  include ::Symbols
+
+  #TODO: These are all positive tests. need to add negative tests
+  #(i.e. test that checks content is not displayed under a condition that shoud result in it not displaying)
+
+  context 'Rule test: ' do
+    context 'Borrowing money ' do
+      let(:section_code) { 'S8' }
+      let(:heading_code) { 'H1' }
+      let(:region) { [ 'ni', 'england', 'wales', 'scotland'] }
+
+      describe  "(severe) " do
+        let(:content_prefix) {'thinking-of-borrowing-severe'}
+
+        include_examples 'regionally valid content'
+      end
+
+      describe  "(temp worried) " do
+        let(:content_prefix) {'thinking-of-borrowing-temp-worried'}
+
+        include_examples 'regionally valid content'
+      end
+
+      describe  "(temp normal) " do
+        let(:content_prefix) {'thinking-of-borrowing-temp-normal'}
+
+        include_examples 'regionally valid content'
+      end
+
+      describe  "(no change) " do
+        let(:content_prefix) {'thinking-of-borrowing-no-change'}
+
+        include_examples 'regionally valid content'
+      end
+    end
+
+  end
+end

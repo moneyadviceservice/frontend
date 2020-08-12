d75380aded4acc6b4b534a2c7ff58d1790816d03
Abraham Kiggundu <abraham@thekiggys.com>
2020-08-12 12:29:37 +0100

    [TP-11529] Added all section 11 rule tests

---
+++ spec/models/money_navigator/rules/mental_health_rules.spec.rb
@@ -0,0 +1,21 @@
+
+RSpec.describe Questions, type: :model do
+  #TODO: Using global smbols is bad.... do not use. fix this
+  include ::Symbols
+
+  #TODO: These are all positive tests. need to add negative tests
+  #(i.e. test that checks content is not displayed under a condition that shoud result in it not displaying)
+
+  context 'Rule test: ' do
+    describe 'Mental health ' do
+      let(:section_code) { 'S11' }
+      let(:region) { [ 'ni', 'england', 'wales', 'scotland'] }
+      let(:heading_code) { 'H1' }
+      let(:content_prefix) {'mental-health'}
+
+      include_examples 'regionally valid content'
+
+    end
+
+  end
+end

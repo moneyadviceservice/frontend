53433d8042c9486ed91942fdf2e19d23fb5b61a4
Abraham Kiggundu <abraham@thekiggys.com>
2020-08-12 12:15:16 +0100

    [TP-11529] Added all section 10 rule tests

---
+++ spec/models/money_navigator/rules/protecting_your_futures_rules.spec.rb
@@ -0,0 +1,24 @@
+
+RSpec.describe Questions, type: :model do
+  #TODO: Using global smbols is bad.... do not use. fix this
+  include ::Symbols
+
+  #TODO: These are all positive tests. need to add negative tests
+  #(i.e. test that checks content is not displayed under a condition that shoud result in it not displaying)
+
+  context 'Rule test: ' do
+    context 'Protecting your future ' do
+      let(:section_code) { 'S10' }
+      let(:region) { [ 'ni', 'england', 'wales', 'scotland'] }
+
+      describe  "cancelling insurance" do
+        let(:heading_code) { 'H1' }
+        let(:content_prefix) {'cancelling-insurance'}
+
+        include_examples 'regionally valid content'
+      end
+
+    end
+
+  end
+end

2ec3230534b6997052cffd9eea955b20eaa890d2
thenapking <tom.giles@gmail.com>
2021-01-25 13:38:41 +0000

    Set a cookie instead

--- app/controllers/pace_controller.rb
+++ app/controllers/pace_controller.rb
@@ -1,9 +1,11 @@
 class PaceController < ApplicationController
   layout 'pace'
-  EXTERNAL_URLS = ['https://www.one.com/', 'https://www.two.com/'].freeze
+  EXTERNAL_URLS = ['https://tools.nationaldebtline.org/dat-reg?utm_source=mas&utm_medium=landing-page&utm_campaign=start_demo'.html_safe,
+  'https://www.stepchange.org/mas-money-advice.aspx?utm_source=mas&utm_medium=landing-page&utm_campaign=start'.html_safe].freeze
 
   def show
-  	@random_url= EXTERNAL_URLS.sample
+  	cookies[:_pace_url_id] = rand(EXTERNAL_URLS.count) if cookies[:_pace_url_id].nil?
+  	cookies[:_pace_url] = EXTERNAL_URLS[cookies[:_pace_url_id].to_i]
   end
 
   def privacy; end

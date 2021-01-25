b61fd842be4ec90b7942ce9e812a29ce61d83451
thenapking <tom.giles@gmail.com>
2021-01-25 13:56:38 +0000

    Handle Redirect Through Meta Tag

--- app/controllers/pace_controller.rb
+++ app/controllers/pace_controller.rb
@@ -1,14 +1,18 @@
 class PaceController < ApplicationController
   layout 'pace'
-  EXTERNAL_URLS = ['https://tools.nationaldebtline.org/dat-reg?utm_source=mas&utm_medium=landing-page&utm_campaign=start_demo'.html_safe,
-  'https://www.stepchange.org/mas-money-advice.aspx?utm_source=mas&utm_medium=landing-page&utm_campaign=start'.html_safe].freeze
+  
+  EXTERNAL_URLS = ['https://tools.nationaldebtline.org/dat-reg?utm_source=mas&utm_medium=landing-page&utm_campaign=start_demo',
+  'https://www.stepchange.org/mas-money-advice.aspx?utm_source=mas&utm_medium=landing-page&utm_campaign=start'].freeze
 
-  def show
-  	cookies[:_pace_url_id] = rand(EXTERNAL_URLS.count) if cookies[:_pace_url_id].nil?
-  	cookies[:_pace_url] = EXTERNAL_URLS[cookies[:_pace_url_id].to_i]
-  end
+  def show; end
 
   def privacy; end
 
-  def online; end
+  def online
+  	cookies[:_pace_url_id] = rand(EXTERNAL_URLS.count) if cookies[:_pace_url_id].nil?
+  	redirect_url = EXTERNAL_URLS[cookies[:_pace_url_id].to_i].html_safe
+  	redirect_time = 5
+  	@redirect_string = "#{redirect_time};url=#{redirect_url}"
+  end
+
 end

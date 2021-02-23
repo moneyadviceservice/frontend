d0276a3d3ec18c2345fa00d8fa282926cd2d44bf
David Trussler <mail@davidtrussler.net>
2021-02-23 13:03:31 +0000

    Adds optional object

--- app/assets/javascripts/translations/cy.js
+++ app/assets/javascripts/translations/cy.js
@@ -2,6 +2,14 @@ define([], function() {
   'use strict';
 
   return {
-    'hide': 'hide'
+    'hide': 'hide', 
+		'cookieController': {
+			'optionalCookies': {
+				'analytics': {
+				'label': 'Cwcis dadansoddi',
+				'description': 'Mae&#8217;r cwcis hyn yn caniatáu i ni gasglu data dienw am sut mae ein gwefan yn cael ei defnyddio, gan ein helpu i wneud gwelliannau i&#8217;r gwasanaethau rydym yn eu darparu i chi.'
+				}
+			}
+		}
   };
 });

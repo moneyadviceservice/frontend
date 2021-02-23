5899810ef87c9c88aa1120a26a0a89f22773368b
David Trussler <mail@davidtrussler.net>
2021-02-23 13:03:31 +0000

    Adds optional cookies

--- app/assets/javascripts/translations/cy.js
+++ app/assets/javascripts/translations/cy.js
@@ -2,6 +2,14 @@ define([], function() {
   'use strict';
 
   return {
-    'hide': 'hide'
+    'hide': 'hide', 
+		'cookieController': {
+			'optionalCookies': {
+				'analytics': {
+				'label': 'Analytics Cookies [cy]',
+				'description': 'These cookies allow us to collect anonymised data about how our website is being used, helping us to make improvements to the services we provide to you. [cy]'
+				}
+			}
+		}
   };
 });

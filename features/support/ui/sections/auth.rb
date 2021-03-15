b6d1d675b1c0c92c3c95493bae5c166644c5dc7d
Tom Giles <tom.giles@gmail.com>
2021-03-15 10:16:35 +0000

    TP-12185 Fix Tests for new Sign Out Button (#2320)
    * Version bumps for UC and Budget_Planner
    
    * Update Gemfile.lock
    
    * Gemfile update for DALT/WPCC
    
    * Click new sign out button to test sign out

--- features/support/ui/sections/auth.rb
+++ features/support/ui/sections/auth.rb
@@ -2,6 +2,6 @@ require_relative '../section'
 
 module UI::Sections
   class Auth < UI::Section
-    element :sign_out, "input[value='Sign out']"
+    element :sign_out, "li.authentication__item.authentication__item--last > a"
   end
 end

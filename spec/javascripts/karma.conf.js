74788ea2cd0bb4f9096a4a3cc13dad23a1dfa311
Ben Lovell <benjamin.lovell@gmail.com>
2021-10-08 11:59:29 +0100

    Debug

--- spec/javascripts/karma.conf.js
+++ spec/javascripts/karma.conf.js
@@ -4,7 +4,7 @@
 module.exports = function(config) {
   config.set({
     // base path, that will be used to resolve files and exclude
-    basePath: '../..',
+    basePath: '../../',
 
     // testing framework to use (jasmine/mocha/qunit/...)
     frameworks: ['requirejs', 'mocha', 'chai-jquery', 'chai', 'sinon','sinon-chai', 'fixture', 'jquery-1.12.4'],
@@ -45,7 +45,7 @@ module.exports = function(config) {
 
     // level of logging
     // possible values: LOG_DISABLE || LOG_ERROR || LOG_WARN || LOG_INFO || LOG_DEBUG
-    logLevel: config.LOG_INFO,
+    logLevel: config.LOG_DEBUG,
 
 
     // enable / disable watching file and executing tests whenever any file changes

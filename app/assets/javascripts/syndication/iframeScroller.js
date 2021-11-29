629a63ea803e2b7667abb95fa7bc1bcd79b461a6
Andrew Vos <andrew.vos@gmail.com>
2021-11-29 10:02:57 +0000

    Scroll to iframe top when clicking "Back to top" links

--- app/assets/javascripts/syndication/iframeScroller.js
+++ app/assets/javascripts/syndication/iframeScroller.js
@@ -1,7 +1,15 @@
 document.addEventListener("DOMContentLoaded", function(event) {
-  // this only works because the parent in AEM receives the message
-  // and has code to act on it
-  if (window.parent && window.parent.postMessage) {
-    window.parent.postMessage({scrollToTop: true}, '*');
+  const sendScrollToTopMessage = () => {
+    if (window.parent && window.parent.postMessage) {
+      // this only works because the parent in AEM receives the message
+      // and has code to act on it
+      window.parent.postMessage({scrollToTop: true}, '*');
+    }
   }
+
+  sendScrollToTopMessage()
+
+  document.getElementsByClassName("back_to_top__link").forEach((element) => {
+    element.addEventListener("click", () => { sendScrollToTopMessage() })
+  })
 });

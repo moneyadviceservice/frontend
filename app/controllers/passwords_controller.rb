1a09e419eb354444defde8ca8556d9ce51ee77b6
Ben Lovell <benjamin.lovell@gmail.com>
2022-01-19 08:38:52 +0000

    WIP: Replace responder with redirect

--- app/controllers/passwords_controller.rb
+++ app/controllers/passwords_controller.rb
@@ -1,9 +1,35 @@
 class PasswordsController < Devise::PasswordsController
   skip_before_action :store_location
 
+  # PUT /resource/password
+  # We have to override this with the default devise implementation but
+  # instead of responding with the resource and location we have to
+  # redirect to an external URL to ensure the end user isn't left
+  # outside of the embedded environment
+  def update
+    self.resource = resource_class.reset_password_by_token(resource_params)
+    yield resource if block_given?
+
+    if resource.errors.empty?
+      resource.unlock_access! if unlockable?(resource)
+      if Devise.sign_in_after_reset_password
+        flash_message = resource.active_for_authentication? ? :updated : :updated_not_active
+        set_flash_message!(:notice, flash_message)
+        resource.after_database_authentication
+        sign_in(resource_name, resource)
+      else
+        set_flash_message!(:notice, :updated_not_active)
+      end
+      redirect_to after_resetting_password_url, status: :see_other
+    else
+      set_minimum_password_length
+      respond_with resource
+    end
+  end
+
   private
 
-  def after_resetting_password_path_for(*)
+  def after_resetting_password_url
     case session[:user_return_to]
     when /money-manager/
       money_helper_url_for('/en/benefits/universal-credit/money-manager-my-profile')

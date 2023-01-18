3cb86be0fd51952c1f6d42446e1df46f89a04823
Ben Lovell <benjamin.lovell@gmail.com>
2023-01-18 20:00:00 +0000

    Ensure setting cookies is done properly
    The tools are always 'syndicated' now so ensure we configure them
    correctly.

--- spec/middleware/partner_tools_cookies_spec.rb
+++ spec/middleware/partner_tools_cookies_spec.rb
@@ -5,21 +5,8 @@ RSpec.describe PartnerToolsCookies do
     described_class.new(->(env) { [200, env, 'Hello'] })
   end
 
-  describe "with no `X-Syndicated-Tool' header" do
+  context "with a cookie being set" do
     before do
-      header 'Set-Cookie', 'v=1'
-      allow_any_instance_of(Rack::Request).to receive(:ssl?) { true }
-    end
-
-    it 'does not change any headers' do
-      get '/'
-      expect(last_response.headers['Set-Cookie']).to be_nil
-    end
-  end
-
-  describe "with a `X-Syndicated-Tool' has been set to `true'" do
-    before do
-      header 'X-Syndicated-Tool', 'true'
       header 'Set-Cookie', 'v=1'
       allow_any_instance_of(Rack::Request).to receive(:ssl?) { true }
     end

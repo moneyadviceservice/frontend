d77450fc5a32c5be69c4105afb0ef8b53a44da45
Ben Lovell <benjamin.lovell@gmail.com>
2023-01-22 09:53:17 +0000

    Add legacy blog redirects

--- lib/legacy_redirects.rb
+++ lib/legacy_redirects.rb
@@ -1,4 +1,6 @@
 r301 %r{^/assets/syndication/(.*)$}, '/a/syndication/$1'
+r301 %r{^/blog/?(.*)$}, 'https://www.moneyhelper.org.uk/en/blog'
+r301 %r{^/en/blog/?(.*)$}, 'https://www.moneyhelper.org.uk/en/blog'
 r301 %r{^/en/articles/how-to-talk-to-your-children-about-money-age-5-6/?$}, 'https://www.moneyhelper.org.uk/en/family-and-care/talk-money/how-to-talk-to-five-and-six-year-olds-about-money?source=mas'
 r301 %r{^/en/articles/individual-and-fixed-protection-2014-schemes-for-pension-savings/?$}, 'https://www.moneyhelper.org.uk/en/pensions-and-retirement/pension-problems/the-pension-protection-fund?source=mas'
 r301 %r{^/cy/articles/cronnwch-eich-cynilion-ymddeoliad-wedi-ysgariad-neu-ddiddymu/?$}, 'https://www.moneyhelper.org.uk/cy/family-and-care/divorce-and-separation/build-up-your-retirement-savings?source=mas'

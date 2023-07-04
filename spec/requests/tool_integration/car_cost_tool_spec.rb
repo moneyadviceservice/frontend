587b9510b3ecabc31492a7a922a5093c09fb76f6
Ben Lovell <benjamin.lovell@gmail.com>
2023-07-04 11:14:53 +0100

    Display a friendly message for car tool errors (#2459)

--- spec/requests/tool_integration/car_cost_tool_spec.rb
+++ spec/requests/tool_integration/car_cost_tool_spec.rb
@@ -1,3 +1,5 @@
+require 'car_cost_tool/wrappers/cap'
+
 RSpec.describe 'Car Cost Tool', type: :request do
   %W[
     /en/tools/#{ToolMountPoint::CarCostTool::EN_ID}?checked=true
@@ -9,6 +11,18 @@ RSpec.describe 'Car Cost Tool', type: :request do
       end
 
       specify { expect(response).to be_ok }
+    end
+  end
+
+  context 'when the CAP HPI API is flapping' do
+    it 'shows a friendly error message' do
+      allow_any_instance_of(CarCostTool::Wrappers::Cap)
+        .to receive(:get_cap_derivatives_as_array).and_raise(RestClient::InternalServerError)
+
+      get '/en/tools/car-costs-calculator/api/derivatives?model=96743'
+
+      expect(response).to be_ok
+      expect(response.body).to include("temporarily unavailable")
     end
   end
 end

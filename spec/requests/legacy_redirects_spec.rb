c49421e552383a28ead139204c30735886dc40e3
Ben Lovell <benjamin.lovell@gmail.com>
2023-08-02 12:21:53 +0100

    Ensure www is canonicalised for redirects
    These should act the same.

--- spec/requests/legacy_redirects_spec.rb
+++ spec/requests/legacy_redirects_spec.rb
@@ -1,14 +1,16 @@
 RSpec.describe 'Legacy redirects', type: :request do
-  context 'when requested from the legacy www.moneyadviceservice.org.uk host' do
-    it 'redirects tools to the correct landing page' do
-      host! 'www.moneyadviceservice.org.uk'
+  ['www.moneyadviceservice.org.uk', 'moneyadviceservice.org.uk'].each do |host|
+    context "when requested from the legacy #{host} host" do
+      it 'redirects tools to the correct landing page' do
+        host! host
 
-      get '/en/tools/budget-planner'
-      expect(request).to redirect_to('https://www.moneyhelper.org.uk/en/everyday-money/budgeting/use-our-budget-planner')
+        get '/en/tools/budget-planner'
+        expect(request).to redirect_to('https://www.moneyhelper.org.uk/en/everyday-money/budgeting/use-our-budget-planner')
 
-      # weird path from legacy campaigns
-      get '///en/tools/money-navigator-tool'
-      expect(request).to redirect_to('https://www.moneyhelper.org.uk/en/money-troubles/coronavirus/use-our-money-navigator-tool')
+        # weird path from legacy campaigns
+        get '///en/tools/money-navigator-tool'
+        expect(request).to redirect_to('https://www.moneyhelper.org.uk/en/money-troubles/coronavirus/use-our-money-navigator-tool')
+      end
     end
   end
 end

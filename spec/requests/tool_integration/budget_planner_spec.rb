e6f8277a229ae26be68b08906e191e3b9537d19e
Ben Lovell <benjamin.lovell@gmail.com>
2025-03-29 09:21:36 +0000

    Redirect from legacy tool to new tool syndication
    This will allow existing partners to continue syndication of the new
    tool in place of the legacy tool, with zero configuration on their side.

--- spec/requests/tool_integration/budget_planner_spec.rb
+++ spec/requests/tool_integration/budget_planner_spec.rb
@@ -8,33 +8,4 @@ RSpec.describe 'Budget Planner', type: :request do
       specify { expect(response).to be_ok }
     end
   end
-
-  context 'when syndicated with `noresize` but prior to cookie checks' do
-    it 'does not include the frame resizer' do
-      get '/en/tools/budget-planner', { noresize: true }
-
-      expect(response).to be_redirect
-      cookie_check_path = response.location
-      get cookie_check_path
-
-      expect(response).to be_redirect
-      expect(response.location).to end_with('/en/tools/budget-planner/?checked=true&noresize=true')
-    end
-  end
-
-  context 'when syndicated with `noresize`' do
-    it 'does not include the frame resizer' do
-      get '/en/tools/budget-planner', { checked: true, noresize: true }
-
-      expect(response.body).not_to include('window.iframeResizer')
-    end
-  end
-
-  context 'when syndicated normally' do
-    it 'includes the frame resizer' do
-      get '/en/tools/budget-planner?checked=true', {}
-
-      expect(response.body).to include('window.iframeResizer')
-    end
-  end
 end

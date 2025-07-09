713c136eb18d0fc9df31aca591ef561bfcfa55e9
Ben Lovell <benjamin.lovell@gmail.com>
2025-07-09 21:16:45 +0100

    Remove `mortgage_calculator`
    Here for a good time not a long time.

--- config/initializers/mortgage_calculator.rb
+++
@@ -1,5 +0,0 @@
-MortgageCalculator.parent_controller = '::EmbeddedToolsController'
-
-MortgageCalculator.affordability_enabled = true
-
-MortgageCalculator.bug_snag_key = ENV["MORTGAGE_CALC_BUG_SNAG_KEY"]
